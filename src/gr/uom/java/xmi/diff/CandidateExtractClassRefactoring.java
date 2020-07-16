01953c46217011c1c313f3a500927ff377bf7c6b
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-16 10:25:23 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,CandidateExtractClassRefactoring,innerClassExtract#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/CandidateExtractClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/CandidateExtractClassRefactoring.java
@@ -2,17 +2,13 @@ package gr.uom.java.xmi.diff;
 
 public class CandidateExtractClassRefactoring {
 	private UMLClassBaseDiff classDiff;
-	private ExtractClassRefactoring refactoring;
+	ExtractClassRefactoring refactoring;
 	
 	public CandidateExtractClassRefactoring(UMLClassBaseDiff classDiff, ExtractClassRefactoring refactoring) {
 		this.classDiff = classDiff;
 		this.refactoring = refactoring;
 	}
 	
-	public boolean innerClassExtract() {
-		return refactoring.getExtractedClass().getName().startsWith(refactoring.getOriginalClass().getName() + ".");
-	}
-
 	public UMLClassBaseDiff getClassDiff() {
 		return classDiff;
 	}
