87db53bc483b19eef8102ab7fe30437b70a53491
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-16 09:45:41 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,getOriginalClassName#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLModelDiff.java
+++ src/gr/uom/java/xmi/diff/UMLModelDiff.java
@@ -1345,7 +1345,7 @@ public class UMLModelDiff {
     		  UMLAttribute a2 = diff.findAttributeInNextClass(merge.getAfter());
     		  Set<CandidateMergeVariableRefactoring> set = mergeMap.get(merge);
     		  if(mergedVariables.size() > 1 && mergedVariables.size() == merge.getMergedVariables().size() && a2 != null) {
-    			  MergeAttributeRefactoring ref = new MergeAttributeRefactoring(mergedVariables, a2.getVariableDeclaration(), diff.getOriginalClassName(), diff.getNextClassName(), set);
+    			  MergeAttributeRefactoring ref = new MergeAttributeRefactoring(mergedVariables, a2.getVariableDeclaration(), diff.getModelDiff().getOriginalClassName(this), diff.getNextClassName(), set);
     			  if(!refactorings.contains(ref)) {
     				  refactorings.add(ref);
     				  Refactoring conflictingRefactoring = attributeRenamed(mergedVariables, a2.getVariableDeclaration(), refactorings);
@@ -2524,6 +2524,10 @@ public class UMLModelDiff {
       if(classDiff != null)
     	  classDiff.getAddedOperations().remove(operation);
    }
+
+	public String getOriginalClassName(UMLClassBaseDiff umlClassBaseDiff) {
+	return umlClassBaseDiff.originalClass.getName();
+}
 
 	private static boolean isNumeric(String str) {
 		for(char c : str.toCharArray()) {
