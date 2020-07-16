9c1e6f896310ec134581fa57793d3312be7d5bcf
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-16 08:57:50 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLAttribute,setFinal#Z,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/UMLModelASTReader.java
+++ src/gr/uom/java/xmi/UMLModelASTReader.java
@@ -502,7 +502,7 @@ public class UMLModelASTReader {
 				umlAttribute.setVisibility("package");
 			
 			if((fieldModifiers & Modifier.FINAL) != 0)
-				umlAttribute.setFinal(true);
+				umlAttribute.getJavadoc().setFinal(this, true);
 			
 			if((fieldModifiers & Modifier.STATIC) != 0)
 				umlAttribute.setStatic(true);
