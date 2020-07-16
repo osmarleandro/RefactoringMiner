dcb81aea473aefc5ebd2781d44ee1bea708e18ad
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-16 09:42:10 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,OperationBody,getAllVariableDeclarations#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/UMLOperation.java
+++ src/gr/uom/java/xmi/UMLOperation.java
@@ -160,7 +160,7 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
 
 	public List<VariableDeclaration> getAllVariableDeclarations() {
 		if(operationBody != null)
-			return operationBody.getAllVariableDeclarations();
+			return operationBody.getCompositeStatement().getAllVariableDeclarations();
 		return new ArrayList<VariableDeclaration>();
 	}
 
