7c9c2cd1fca92d2e553ae18c8abadb41f412cdbd
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-16 09:19:52 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,StatementObject,codeRange#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/StatementObject.java
+++ src/gr/uom/java/xmi/decomposition/StatementObject.java
@@ -22,7 +22,7 @@ import gr.uom.java.xmi.diff.CodeRange;
 public class StatementObject extends AbstractStatement {
 	
 	private String statement;
-	private LocationInfo locationInfo;
+	public LocationInfo locationInfo;
 	private List<String> variables;
 	private List<String> types;
 	private List<VariableDeclaration> variableDeclarations;
