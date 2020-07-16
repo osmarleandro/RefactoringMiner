66db8bec618152dcc3fbcefafbabbf602a0678fa
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-16 09:02:20 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLOperation,getTypeParameters#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/UMLJavadoc.java
+++ src/gr/uom/java/xmi/UMLJavadoc.java
@@ -35,4 +35,8 @@ public class UMLJavadoc {
 		}
 		return false;
 	}
+
+	public List<UMLTypeParameter> getTypeParameters(UMLOperation umlOperation) {
+		return umlOperation.typeParameters;
+	}
 }
--- src/gr/uom/java/xmi/UMLOperation.java
+++ src/gr/uom/java/xmi/UMLOperation.java
@@ -34,7 +34,7 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
 	private boolean emptyBody;
 	private OperationBody operationBody;
 	private List<UMLAnonymousClass> anonymousClassList;
-	private List<UMLTypeParameter> typeParameters;
+	List<UMLTypeParameter> typeParameters;
 	private UMLJavadoc javadoc;
 	private List<UMLAnnotation> annotations;
 	
@@ -47,10 +47,6 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
         this.annotations = new ArrayList<UMLAnnotation>();
     }
 
-	public List<UMLTypeParameter> getTypeParameters() {
-		return typeParameters;
-	}
-	
 	public void addTypeParameter(UMLTypeParameter typeParameter) {
 		typeParameters.add(typeParameter);
 	}
