cd12a292fdb645953b01a38c840eeada3338dc92
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-16 08:58:35 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLAttribute,getAnnotations#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLAttributeDiff.java
+++ src/gr/uom/java/xmi/diff/UMLAttributeDiff.java
@@ -44,7 +44,7 @@ public class UMLAttributeDiff {
 			staticChanged = true;
 		if(removedAttribute.isFinal() != addedAttribute.isFinal())
 			finalChanged = true;
-		this.annotationListDiff = new UMLAnnotationListDiff(removedAttribute.getAnnotations(), addedAttribute.getAnnotations());
+		this.annotationListDiff = new UMLAnnotationListDiff(removedAttribute.getJavadoc().getAnnotations(this), addedAttribute.getJavadoc().getAnnotations(this));
 	}
 
 	public UMLAttribute getRemovedAttribute() {
