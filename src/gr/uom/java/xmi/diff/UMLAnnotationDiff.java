39ef9c333960aa96c3b8a4461f14a94d70e49435
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-16 09:12:57 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLAnnotation,getValue#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLAnnotationDiff.java
+++ src/gr/uom/java/xmi/diff/UMLAnnotationDiff.java
@@ -31,8 +31,8 @@ public class UMLAnnotationDiff {
 		if(!removedAnnotation.getTypeName().equals(addedAnnotation.getTypeName())) {
 			typeNameChanged = true;
 		}
-		AbstractExpression value1 = removedAnnotation.getValue();
-		AbstractExpression value2 = addedAnnotation.getValue();
+		AbstractExpression value1 = removedAnnotation.getLocationInfo().getValue(this);
+		AbstractExpression value2 = addedAnnotation.getLocationInfo().getValue(this);
 		if(value1 != null && value2 != null) {
 			if(!value1.getExpression().equals(value2.getExpression())) {
 				valueChanged = true;
