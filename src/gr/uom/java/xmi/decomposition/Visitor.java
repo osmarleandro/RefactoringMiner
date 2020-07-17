ecb286fd620f23f7543cdb66b6a8fd36f96ff508
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:10:10 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,Visitor,visit#QTypeLiteral,RenameMethod,OK

--- src/gr/uom/java/xmi/decomposition/Visitor.java
+++ src/gr/uom/java/xmi/decomposition/Visitor.java
@@ -365,7 +365,7 @@ public class Visitor extends ASTVisitor {
 		return super.visit(node);
 	}
 
-	public boolean visit(TypeLiteral node) {
+	public boolean visit_RENAMED(TypeLiteral node) {
 		typeLiterals.add(node.toString());
 		if(current.getUserObject() != null) {
 			AnonymousClassDeclarationObject anonymous = (AnonymousClassDeclarationObject)current.getUserObject();
