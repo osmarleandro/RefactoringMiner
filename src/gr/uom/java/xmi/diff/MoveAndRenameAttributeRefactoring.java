9c2dfe9712b110ae81cbd9d7df6352062135708e
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:25:10 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveAndRenameAttributeRefactoring,getAttributeRenames#,RenameMethod,OK

--- src/gr/uom/java/xmi/diff/MoveAndRenameAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveAndRenameAttributeRefactoring.java
@@ -17,7 +17,7 @@ public class MoveAndRenameAttributeRefactoring extends MoveAttributeRefactoring 
 		this.attributeRenames = attributeRenames;
 	}
 
-	public Set<CandidateAttributeRefactoring> getAttributeRenames() {
+	public Set<CandidateAttributeRefactoring> getAttributeRenames_RENAMED() {
 		return attributeRenames;
 	}
 
