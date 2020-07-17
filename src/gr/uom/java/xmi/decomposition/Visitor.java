54303279fe1e1b065382d247cecf53eaa07c135b
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:09:52 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,Visitor,insertNode#QAnonymousClassDeclaration,RenameMethod,OK

--- src/gr/uom/java/xmi/decomposition/Visitor.java
+++ src/gr/uom/java/xmi/decomposition/Visitor.java
@@ -218,7 +218,7 @@ public class Visitor extends ASTVisitor {
 	}
 
 	public boolean visit(AnonymousClassDeclaration node) {
-		DefaultMutableTreeNode childNode = insertNode(node);
+		DefaultMutableTreeNode childNode = insertNode_RENAMED(node);
 		AnonymousClassDeclarationObject childAnonymous = (AnonymousClassDeclarationObject)childNode.getUserObject();
 		if(current.getUserObject() != null) {
 			AnonymousClassDeclarationObject currentAnonymous = (AnonymousClassDeclarationObject)current.getUserObject();
@@ -288,7 +288,7 @@ public class Visitor extends ASTVisitor {
 		return parentNode;
 	}
 
-	private DefaultMutableTreeNode insertNode(AnonymousClassDeclaration childAnonymous) {
+	private DefaultMutableTreeNode insertNode_RENAMED(AnonymousClassDeclaration childAnonymous) {
 		Enumeration enumeration = root.postorderEnumeration();
 		AnonymousClassDeclarationObject anonymousObject = new AnonymousClassDeclarationObject(cu, filePath, childAnonymous);
 		DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(anonymousObject);
