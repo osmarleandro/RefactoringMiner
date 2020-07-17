5df7d9a3b0741d53e33c5d493258d2c9dedcb284
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:43:51 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MergeVariableRefactoring,getInvolvedClassesBeforeRefactoring#,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MergeVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/MergeVariableRefactoring.java
@@ -68,7 +68,7 @@ public class MergeVariableRefactoring implements Refactoring {
 		return this.getRefactoringType().getDisplayName();
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
+	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring_RENAMED() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getOperationBefore().getLocationInfo().getFilePath(), getOperationBefore().getClassName()));
 		return pairs;
