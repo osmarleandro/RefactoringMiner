6328421ebd2e1260872e5178b65d8a20f6adfb01
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:09:11 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ModifyMethodAnnotationRefactoring,getAnnotationAfter#,RenameMethod,OK

--- src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
@@ -30,7 +30,7 @@ public class ModifyMethodAnnotationRefactoring implements Refactoring {
 		return annotationBefore;
 	}
 
-	public UMLAnnotation getAnnotationAfter() {
+	public UMLAnnotation getAnnotationAfter_RENAMED() {
 		return annotationAfter;
 	}
 
