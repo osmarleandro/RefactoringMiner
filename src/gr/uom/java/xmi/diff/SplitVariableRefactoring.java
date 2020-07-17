8bd1519be36ee95ef9c6694bc7cf83c9b8707c67
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:23:38 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,SplitVariableRefactoring,getOperationBefore#,RenameMethod,OK

--- src/gr/uom/java/xmi/diff/SplitVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/SplitVariableRefactoring.java
@@ -37,7 +37,7 @@ public class SplitVariableRefactoring implements Refactoring {
 		return oldVariable;
 	}
 
-	public UMLOperation getOperationBefore() {
+	public UMLOperation getOperationBefore_RENAMED() {
 		return operationBefore;
 	}
 
@@ -73,7 +73,7 @@ public class SplitVariableRefactoring implements Refactoring {
 	@Override
 	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOperationBefore().getLocationInfo().getFilePath(), getOperationBefore().getClassName()));
+		pairs.add(new ImmutablePair<String, String>(getOperationBefore_RENAMED().getLocationInfo().getFilePath(), getOperationBefore_RENAMED().getClassName()));
 		return pairs;
 	}
 
