166ce4dee029635562eb654f2d7902fe434e1c90
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 01:44:19 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractStatement,searchVariableDeclaration#QString,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractStatement.java
+++ src/gr/uom/java/xmi/decomposition/AbstractStatement.java
@@ -17,13 +17,13 @@ public abstract class AbstractStatement extends AbstractCodeFragment {
     	return toString();
     }
 
-    public VariableDeclaration searchVariableDeclaration(String variableName) {
+    public VariableDeclaration searchVariableDeclaration_RENAMED(String variableName) {
     	VariableDeclaration variableDeclaration = this.getVariableDeclaration(variableName);
     	if(variableDeclaration != null) {
     		return variableDeclaration;
     	}
     	else if(parent != null) {
-    		return parent.searchVariableDeclaration(variableName);
+    		return parent.searchVariableDeclaration_RENAMED(variableName);
     	}
     	return null;
     }
