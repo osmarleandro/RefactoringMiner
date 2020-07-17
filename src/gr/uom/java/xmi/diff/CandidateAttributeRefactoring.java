e6a47faec5381d3f49f3ee42d480f0b378b22253
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:08:04 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,CandidateAttributeRefactoring,equals#QObject,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/CandidateAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/CandidateAttributeRefactoring.java
@@ -111,7 +111,7 @@ public class CandidateAttributeRefactoring {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals_RENAMED(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -122,12 +122,12 @@ public class CandidateAttributeRefactoring {
 		if (operationAfter == null) {
 			if (other.operationAfter != null)
 				return false;
-		} else if (!operationAfter.equals(other.operationAfter))
+		} else if (!operationAfter.equals_RENAMED(other.operationAfter))
 			return false;
 		if (operationBefore == null) {
 			if (other.operationBefore != null)
 				return false;
-		} else if (!operationBefore.equals(other.operationBefore))
+		} else if (!operationBefore.equals_RENAMED(other.operationBefore))
 			return false;
 		if (originalVariableName == null) {
 			if (other.originalVariableName != null)
