fb8a00a7b64882bb53013b98c9c5d6413777e318
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:41:04 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RemoveMethodAnnotationRefactoring,hashCode#,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
@@ -106,10 +106,10 @@ public class ModifyMethodAnnotationRefactoring implements Refactoring {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((annotationAfter == null) ? 0 : annotationAfter.hashCode());
-		result = prime * result + ((annotationBefore == null) ? 0 : annotationBefore.hashCode());
-		result = prime * result + ((operationAfter == null) ? 0 : operationAfter.hashCode());
-		result = prime * result + ((operationBefore == null) ? 0 : operationBefore.hashCode());
+		result = prime * result + ((annotationAfter == null) ? 0 : annotationAfter.hashCode_RENAMED());
+		result = prime * result + ((annotationBefore == null) ? 0 : annotationBefore.hashCode_RENAMED());
+		result = prime * result + ((operationAfter == null) ? 0 : operationAfter.hashCode_RENAMED());
+		result = prime * result + ((operationBefore == null) ? 0 : operationBefore.hashCode_RENAMED());
 		return result;
 	}
 
