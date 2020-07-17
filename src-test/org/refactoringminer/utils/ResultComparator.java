38bf99d611942bf0cd665a67b0c37809d8b91cb0
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:34:49 -0300

    RefactoringMiner,org.refactoringminer.utils,ResultComparator,getPrecision#I;I;I,RenameMethod,OK

--- src-test/org/refactoringminer/utils/ResultComparator.java
+++ src-test/org/refactoringminer/utils/ResultComparator.java
@@ -122,14 +122,14 @@ public class ResultComparator {
     }
 
     private String getResultLine(int tp, int fp, int fn) {
-        double precision = getPrecision(tp, fp, fn);
+        double precision = getPrecision_RENAMED(tp, fp, fn);
         double recall = getRecall(tp, fp, fn);
         double f1 = getF1(tp, fp, fn);
         //return String.format("& %3d & %3d & %3d & %3d & %.3f & %.3f \\", tp + fn, tp, fp, fn, precision, recall);
         return String.format("#: %3d  TP: %3d  FP: %3d  FN: %3d  Prec.: %.3f  Recall: %.3f  F1: %.3f", tp + fn, tp, fp, fn, precision, recall, f1);
     }
 
-    private static double getPrecision(int tp, int fp, int fn) {
+    private static double getPrecision_RENAMED(int tp, int fp, int fn) {
         return tp == 0 ? 0.0 : ((double) tp / (tp + fp));
     }
 
@@ -138,7 +138,7 @@ public class ResultComparator {
     }
 
     private static double getF1(int tp, int fp, int fn) {
-        double precision = ResultComparator.getPrecision(tp, fp, fn);
+        double precision = ResultComparator.getPrecision_RENAMED(tp, fp, fn);
         double recall = ResultComparator.getRecall(tp, fp, fn);
         return tp == 0 ? 0.0 : 2.0 * precision * recall / (precision + recall);
     }
@@ -295,7 +295,7 @@ public class ResultComparator {
             int tp = this.truePositives.size();
             int fp = this.falsePositives.size();
             int fn = this.falseNegatives.size();
-            return ResultComparator.getPrecision(tp, fp, fn);
+            return ResultComparator.getPrecision_RENAMED(tp, fp, fn);
         }
         
         public double getRecall() {
