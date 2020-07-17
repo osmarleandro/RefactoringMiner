5944cb6925913cfd6c0533ed3eb952503e9c9c40
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:19:20 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenameVariableRefactoring,getVariableReferences#,RenameMethod,OK

--- src/gr/uom/java/xmi/diff/RenameVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenameVariableRefactoring.java
@@ -63,7 +63,7 @@ public class RenameVariableRefactoring implements Refactoring {
 		return operationAfter;
 	}
 
-	public Set<AbstractCodeMapping> getVariableReferences() {
+	public Set<AbstractCodeMapping> getVariableReferences_RENAMED() {
 		return variableReferences;
 	}
 
