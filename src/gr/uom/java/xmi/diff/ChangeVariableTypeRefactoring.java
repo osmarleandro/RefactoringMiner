25f686afa79cf33900da453df9e1c1913eecd42a
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:11:03 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeVariableTypeRefactoring,getOperationAfter#,RenameMethod,OK

--- src/gr/uom/java/xmi/diff/ChangeVariableTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeVariableTypeRefactoring.java
@@ -61,7 +61,7 @@ public class ChangeVariableTypeRefactoring implements Refactoring {
 		return operationBefore;
 	}
 
-	public UMLOperation getOperationAfter() {
+	public UMLOperation getOperationAfter_RENAMED() {
 		return operationAfter;
 	}
 
@@ -133,7 +133,7 @@ public class ChangeVariableTypeRefactoring implements Refactoring {
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOperationAfter().getLocationInfo().getFilePath(), getOperationAfter().getClassName()));
+		pairs.add(new ImmutablePair<String, String>(getOperationAfter_RENAMED().getLocationInfo().getFilePath(), getOperationAfter_RENAMED().getClassName()));
 		return pairs;
 	}
 
