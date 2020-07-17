7258b24a31c471fc8110f8a6ad7707b68013e6fd
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:15:13 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractAttributeRefactoring,getOriginalClass#,RenameMethod,OK

--- src/gr/uom/java/xmi/diff/ExtractAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractAttributeRefactoring.java
@@ -62,7 +62,7 @@ public class ExtractAttributeRefactoring implements Refactoring {
 		return attributeDeclaration.codeRange();
 	}
 
-	public UMLClass getOriginalClass() {
+	public UMLClass getOriginalClass_RENAMED() {
 		return originalClass;
 	}
 
@@ -97,7 +97,7 @@ public class ExtractAttributeRefactoring implements Refactoring {
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOriginalClass().getLocationInfo().getFilePath(), getOriginalClass().getName()));
+		pairs.add(new ImmutablePair<String, String>(getOriginalClass_RENAMED().getLocationInfo().getFilePath(), getOriginalClass_RENAMED().getName()));
 		return pairs;
 	}
 
