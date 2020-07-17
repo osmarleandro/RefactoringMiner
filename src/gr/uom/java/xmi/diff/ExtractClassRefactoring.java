d8601597d14489d718f47c9baba443ca9f57615c
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:00:32 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveAttributeRefactoring,getName#,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/ExtractClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractClassRefactoring.java
@@ -31,7 +31,7 @@ public class ExtractClassRefactoring implements Refactoring {
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(getName()).append("\t");
+		sb.append(getName_RENAMED()).append("\t");
 		sb.append(extractedClass);
 		sb.append(" from class ");
 		sb.append(classDiff.getOriginalClass());
