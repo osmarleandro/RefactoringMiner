98023ea431ba10204a6fdf9f3ecc682ed8f9b18a
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:22:25 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveAndRenameClassRefactoring,getInvolvedClassesBeforeRefactoring#,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MoveAndRenameClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveAndRenameClassRefactoring.java
@@ -54,7 +54,7 @@ public class MoveAndRenameClassRefactoring implements Refactoring {
 		return renamedClass;
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
+	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring_RENAMED() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getOriginalClass().getLocationInfo().getFilePath(), getOriginalClass().getName()));
 		return pairs;
