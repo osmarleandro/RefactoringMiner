8c0f91c4c64dcc2ffb1ac7e6190f78fe9395c9a5
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:52:55 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeAttributeTypeRefactoring,hashCode#,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/ChangeAttributeTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeAttributeTypeRefactoring.java
@@ -80,13 +80,13 @@ public class ChangeAttributeTypeRefactoring implements Refactoring {
 	}
 
 	@Override
-	public int hashCode() {
+	public int hashCode_RENAMED() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((changedTypeAttribute == null) ? 0 : changedTypeAttribute.hashCode());
+		result = prime * result + ((changedTypeAttribute == null) ? 0 : changedTypeAttribute.hashCode_RENAMED());
 		result = prime * result + ((classNameAfter == null) ? 0 : classNameAfter.hashCode());
 		result = prime * result + ((classNameBefore == null) ? 0 : classNameBefore.hashCode());
-		result = prime * result + ((originalAttribute == null) ? 0 : originalAttribute.hashCode());
+		result = prime * result + ((originalAttribute == null) ? 0 : originalAttribute.hashCode_RENAMED());
 		return result;
 	}
 
