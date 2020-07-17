c082799b85ccd8657a6a3c4d0e6658e482ce3d5c
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:37:08 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeReturnTypeRefactoring,getOperationAfter#,RenameMethod,OK

--- src/gr/uom/java/xmi/diff/ChangeReturnTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeReturnTypeRefactoring.java
@@ -49,7 +49,7 @@ public class ChangeReturnTypeRefactoring implements Refactoring {
 		return operationBefore;
 	}
 
-	public UMLOperation getOperationAfter() {
+	public UMLOperation getOperationAfter_RENAMED() {
 		return operationAfter;
 	}
 
@@ -121,7 +121,7 @@ public class ChangeReturnTypeRefactoring implements Refactoring {
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOperationAfter().getLocationInfo().getFilePath(), getOperationAfter().getClassName()));
+		pairs.add(new ImmutablePair<String, String>(getOperationAfter_RENAMED().getLocationInfo().getFilePath(), getOperationAfter_RENAMED().getClassName()));
 		return pairs;
 	}
 
