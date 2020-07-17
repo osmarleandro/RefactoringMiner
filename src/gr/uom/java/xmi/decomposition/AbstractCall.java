c74088ddf4840b001ce310c6ea92f344c86bcadb
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 01:43:28 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCall,onlyArgumentsChanged#QAbstractCall;QSetQReplacement,RenameMethod,OK

--- src/gr/uom/java/xmi/decomposition/AbstractCall.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCall.java
@@ -252,7 +252,7 @@ public abstract class AbstractCall implements LocationInfoProvider {
 				getArguments().size() != call.getArguments().size();
 	}
 
-	private boolean onlyArgumentsChanged(AbstractCall call, Set<Replacement> replacements) {
+	private boolean onlyArgumentsChanged_RENAMED(AbstractCall call, Set<Replacement> replacements) {
 		return identicalExpression(call, replacements) &&
 				identicalName(call) &&
 				!equalArguments(call) &&
@@ -260,7 +260,7 @@ public abstract class AbstractCall implements LocationInfoProvider {
 	}
 
 	public boolean identicalWithMergedArguments(AbstractCall call, Set<Replacement> replacements) {
-		if(onlyArgumentsChanged(call, replacements)) {
+		if(onlyArgumentsChanged_RENAMED(call, replacements)) {
 			List<String> updatedArguments1 = new ArrayList<String>(this.arguments);
 			Map<String, Set<Replacement>> commonVariableReplacementMap = new LinkedHashMap<String, Set<Replacement>>();
 			for(Replacement replacement : replacements) {
@@ -305,7 +305,7 @@ public abstract class AbstractCall implements LocationInfoProvider {
 	}
 
 	public boolean identicalWithDifferentNumberOfArguments(AbstractCall call, Set<Replacement> replacements, Map<String, String> parameterToArgumentMap) {
-		if(onlyArgumentsChanged(call, replacements)) {
+		if(onlyArgumentsChanged_RENAMED(call, replacements)) {
 			int argumentIntersectionSize = argumentIntersectionSize(call, parameterToArgumentMap);
 			if(argumentIntersectionSize > 0 || getArguments().size() == 0 || call.getArguments().size() == 0) {
 				return true;
