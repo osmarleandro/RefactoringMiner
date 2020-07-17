24be41a6034690c4626989828c6aeb83df5dba1c
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:25:02 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractOperationRefactoring,getInvolvedClassesAfterRefactoring#,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/ExtractOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractOperationRefactoring.java
@@ -177,7 +177,7 @@ public class ExtractOperationRefactoring implements Refactoring {
 		return pairs;
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
+	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring_RENAMED() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getSourceOperationAfterExtraction().getLocationInfo().getFilePath(), getSourceOperationAfterExtraction().getClassName()));
 		pairs.add(new ImmutablePair<String, String>(getExtractedOperation().getLocationInfo().getFilePath(), getExtractedOperation().getClassName()));
