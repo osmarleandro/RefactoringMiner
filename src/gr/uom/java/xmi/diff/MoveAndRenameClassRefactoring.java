7aedb2fca4b4fac6aa636270d42b6465bcbf1616
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:22:11 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveAndRenameClassRefactoring,getRefactoringType#,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MoveAndRenameClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveAndRenameClassRefactoring.java
@@ -31,10 +31,10 @@ public class MoveAndRenameClassRefactoring implements Refactoring {
 	}
 
 	public String getName() {
-		return this.getRefactoringType().getDisplayName();
+		return this.getRefactoringType_RENAMED().getDisplayName();
 	}
 
-	public RefactoringType getRefactoringType() {
+	public RefactoringType getRefactoringType_RENAMED() {
 		return RefactoringType.MOVE_RENAME_CLASS;
 	}
 
