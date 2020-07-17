a68935168c84531c3390186f07808fb86f8a7203
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:41:49 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ClassRenameComparator,compare#QUMLClassRenameDiff;QUMLClassRenameDiff,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/ClassRenameComparator.java
+++ src/gr/uom/java/xmi/diff/ClassRenameComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class ClassRenameComparator implements Comparator<UMLClassRenameDiff> {
 
 	@Override
-	public int compare(UMLClassRenameDiff o1, UMLClassRenameDiff o2) {
+	public int compare_RENAMED(UMLClassRenameDiff o1, UMLClassRenameDiff o2) {
 		double nameDistance1 = o1.getRenamedClass().normalizedNameDistance(o1.getOriginalClass());
 		double nameDistance2 = o2.getRenamedClass().normalizedNameDistance(o2.getOriginalClass());
 		
