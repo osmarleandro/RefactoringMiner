f0f609b1417e18ba2eac5806ea2d3d7e79929f2b
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 02:22:31 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveClassRefactoring,leftSide#,RenameMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MoveClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveClassRefactoring.java
@@ -77,7 +77,7 @@ public class MoveClassRefactoring implements Refactoring {
 	}
 
 	@Override
-	public List<CodeRange> leftSide() {
+	public List<CodeRange> leftSide_RENAMED() {
 		List<CodeRange> ranges = new ArrayList<CodeRange>();
 		ranges.add(originalClass.codeRange()
 				.setDescription("original type declaration")
