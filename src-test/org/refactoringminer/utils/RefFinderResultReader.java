5d659a0d3124554851ff4c307298fb29d3354139
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 03:32:59 -0300

    RefactoringMiner,org.refactoringminer.utils,RefFinderResultReader,member#I,RenameMethod,OK

--- src-test/org/refactoringminer/utils/RefFinderResultReader.java
+++ src-test/org/refactoringminer/utils/RefFinderResultReader.java
@@ -70,12 +70,12 @@ public class RefFinderResultReader {
         Map<String, Function<List<String>, RefactoringRelationship>> mappers = new HashMap<>();
         mappers.put("extract_superclass", args -> parse(args, RefactoringType.EXTRACT_SUPERCLASS, type(1), type(2)));
         mappers.put("extract_interface", args -> parse(args, RefactoringType.EXTRACT_INTERFACE, type(2), type(1)));
-        mappers.put("rename_method", args -> parse(args, RefactoringType.RENAME_METHOD, member(1), member(2)));
+        mappers.put("rename_method", args -> parse(args, RefactoringType.RENAME_METHOD, member_RENAMED(1), member_RENAMED(2)));
         mappers.put("move_method", args -> parse(args, RefactoringType.MOVE_OPERATION, member(2, 1), member(3, 1)));
         mappers.put("push_down_method", args -> parse(args, RefactoringType.PUSH_DOWN_OPERATION, member(2, 1), member(3, 1)));
         mappers.put("pull_up_method", args -> parse(args, RefactoringType.PULL_UP_OPERATION, member(2, 1), member(3, 1)));
-        mappers.put("extract_method", args -> parse(args, RefactoringType.EXTRACT_OPERATION, member(1), member(2)));
-        mappers.put("inline_method", args -> parse(args, RefactoringType.INLINE_OPERATION, member(2), member(1)));
+        mappers.put("extract_method", args -> parse(args, RefactoringType.EXTRACT_OPERATION, member_RENAMED(1), member_RENAMED(2)));
+        mappers.put("inline_method", args -> parse(args, RefactoringType.INLINE_OPERATION, member_RENAMED(2), member_RENAMED(1)));
         mappers.put("move_field", args -> parse(args, RefactoringType.MOVE_ATTRIBUTE, member(2, 1), member(3, 1)));
         mappers.put("push_down_field", args -> parse(args, RefactoringType.PUSH_DOWN_ATTRIBUTE, member(2, 1), member(3, 1)));
         mappers.put("pull_up_field", args -> parse(args, RefactoringType.PULL_UP_ATTRIBUTE, member(2, 1), member(3, 1)));
@@ -105,7 +105,7 @@ public class RefFinderResultReader {
         };
     }
     
-    private static EntityParser member(final int i) {
+    private static EntityParser member_RENAMED(final int i) {
         return new EntityParser() {
             @Override
             String parse(List<String> args) {
