18675bbec762a2019697d02d37655ea5960aeeac
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-17 01:44:45 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeMapping,isExactAfterAbstraction#,RenameMethod,OK

--- src/gr/uom/java/xmi/decomposition/AbstractCodeMapping.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeMapping.java
@@ -63,7 +63,7 @@ public abstract class AbstractCodeMapping {
 
 	public boolean isExact() {
 		return (fragment1.getArgumentizedString().equals(fragment2.getArgumentizedString()) ||
-				fragment1.getString().equals(fragment2.getString()) || isExactAfterAbstraction() || containsIdenticalOrCompositeReplacement()) && !isKeyword();
+				fragment1.getString().equals(fragment2.getString()) || isExactAfterAbstraction_RENAMED() || containsIdenticalOrCompositeReplacement()) && !isKeyword();
 	}
 
 	private boolean isKeyword() {
@@ -72,7 +72,7 @@ public abstract class AbstractCodeMapping {
 				fragment1.getString().startsWith("continue;");
 	}
 
-	private boolean isExactAfterAbstraction() {
+	private boolean isExactAfterAbstraction_RENAMED() {
 		OperationInvocation invocation1 = fragment1.invocationCoveringEntireFragment();
 		OperationInvocation invocation2 = fragment2.invocationCoveringEntireFragment();
 		if(invocation1 != null && invocation2 != null) {
