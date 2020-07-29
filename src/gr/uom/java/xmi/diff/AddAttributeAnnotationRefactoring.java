e9575ddefd257bfcd4be5736d1306ea2e8ee460f
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:10:12 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,AddAttributeAnnotationRefactoring,getName#,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/AddAttributeAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/AddAttributeAnnotationRefactoring.java
@@ -64,7 +64,7 @@ public class AddAttributeAnnotationRefactoring implements Refactoring {
 
 	@Override
 	public String getName() {
-		return this.getRefactoringType().getDisplayName();
+		return annotation.getName(this);
 	}
 
 	@Override
