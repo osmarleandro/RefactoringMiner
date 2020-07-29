7e92a94a8c0d61e1e283118af7a7f665ca9de296
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:07:12 -0300

    RefactoringMiner,gr.uom.java.xmi,WildcardType,toString#,MoveMethod,OK

--- src/gr/uom/java/xmi/WildcardType.java
+++ src/gr/uom/java/xmi/WildcardType.java
@@ -1,8 +1,8 @@
 package gr.uom.java.xmi;
 
 public class WildcardType extends UMLType {
-	private UMLType bound;
-	private boolean upperBound;
+	UMLType bound;
+	boolean upperBound;
 	
 	public WildcardType(UMLType bound, boolean upperBound) {
 		this.bound = bound;
@@ -39,16 +39,7 @@ public class WildcardType extends UMLType {
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("?");
-		if(bound != null) {
-			if(upperBound)
-				sb.append(" extends ");
-			else
-				sb.append(" super ");
-			sb.append(bound.toString());
-		}
-		return sb.toString();
+		return bound.toString(this);
 	}
 
 	@Override
