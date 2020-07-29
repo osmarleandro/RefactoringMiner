5bfb8fae4ac95d06d1f855553925321b9f74e407
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:51:04 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,processMapperRefactorings#QUMLOperationBodyMapper;QListQRefactoring,MoveMethod,OK

--- src/gr/uom/java/xmi/decomposition/UMLOperationBodyMapper.java
+++ src/gr/uom/java/xmi/decomposition/UMLOperationBodyMapper.java
@@ -4161,4 +4161,59 @@ public class UMLOperationBodyMapper implements Comparable<UMLOperationBodyMapper
 		}
 		return false;
 	}
+
+	public void processMapperRefactorings(UMLClassBaseDiff umlClassBaseDiff, List<Refactoring> refactorings) {
+		for(Refactoring refactoring : getRefactorings()) {
+			if(refactorings.contains(refactoring)) {
+				//special handling for replacing rename variable refactorings having statement mapping information
+				int index = refactorings.indexOf(refactoring);
+				refactorings.remove(index);
+				refactorings.add(index, refactoring);
+			}
+			else {
+				refactorings.add(refactoring);
+			}
+		}
+		for(CandidateAttributeRefactoring candidate : getCandidateAttributeRenames()) {
+			if(!umlClassBaseDiff.multipleExtractedMethodInvocationsWithDifferentAttributesAsArguments(candidate, refactorings)) {
+				String before = PrefixSuffixUtils.normalize(candidate.getOriginalVariableName());
+				String after = PrefixSuffixUtils.normalize(candidate.getRenamedVariableName());
+				if(before.contains(".") && after.contains(".")) {
+					String prefix1 = before.substring(0, before.lastIndexOf(".") + 1);
+					String prefix2 = after.substring(0, after.lastIndexOf(".") + 1);
+					if(prefix1.equals(prefix2)) {
+						before = before.substring(prefix1.length(), before.length());
+						after = after.substring(prefix2.length(), after.length());
+					}
+				}
+				Replacement renamePattern = new Replacement(before, after, ReplacementType.VARIABLE_NAME);
+				if(umlClassBaseDiff.renameMap.containsKey(renamePattern)) {
+					umlClassBaseDiff.renameMap.get(renamePattern).add(candidate);
+				}
+				else {
+					Set<CandidateAttributeRefactoring> set = new LinkedHashSet<CandidateAttributeRefactoring>();
+					set.add(candidate);
+					umlClassBaseDiff.renameMap.put(renamePattern, set);
+				}
+			}
+		}
+		for(CandidateMergeVariableRefactoring candidate : getCandidateAttributeMerges()) {
+			Set<String> before = new LinkedHashSet<String>();
+			for(String mergedVariable : candidate.getMergedVariables()) {
+				before.add(PrefixSuffixUtils.normalize(mergedVariable));
+			}
+			String after = PrefixSuffixUtils.normalize(candidate.getNewVariable());
+			MergeVariableReplacement merge = new MergeVariableReplacement(before, after);
+			umlClassBaseDiff.processMerge(umlClassBaseDiff.mergeMap, merge, candidate);
+		}
+		for(CandidateSplitVariableRefactoring candidate : getCandidateAttributeSplits()) {
+			Set<String> after = new LinkedHashSet<String>();
+			for(String splitVariable : candidate.getSplitVariables()) {
+				after.add(PrefixSuffixUtils.normalize(splitVariable));
+			}
+			String before = PrefixSuffixUtils.normalize(candidate.getOldVariable());
+			SplitVariableReplacement split = new SplitVariableReplacement(before, after);
+			umlClassBaseDiff.processSplit(umlClassBaseDiff.splitMap, split, candidate);
+		}
+	}
 }
