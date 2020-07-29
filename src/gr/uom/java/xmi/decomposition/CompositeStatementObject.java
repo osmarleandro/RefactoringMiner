8b4e5111df5ff6668f5ec98f55ba8d2db1b71ae6
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:36:44 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,CompositeStatementObject,getVariables#,MoveMethod,OK

--- src/gr/uom/java/xmi/decomposition/CompositeStatementObject.java
+++ src/gr/uom/java/xmi/decomposition/CompositeStatementObject.java
@@ -17,7 +17,7 @@ import gr.uom.java.xmi.diff.CodeRange;
 public class CompositeStatementObject extends AbstractStatement {
 
 	private List<AbstractStatement> statementList;
-	private List<AbstractExpression> expressionList;
+	public List<AbstractExpression> expressionList;
 	private List<VariableDeclaration> variableDeclarations;
 	private LocationInfo locationInfo;
 
@@ -106,11 +106,7 @@ public class CompositeStatementObject extends AbstractStatement {
 
 	@Override
 	public List<String> getVariables() {
-		List<String> variables = new ArrayList<String>();
-		for(AbstractExpression expression : expressionList) {
-			variables.addAll(expression.getVariables());
-		}
-		return variables;
+		return locationInfo.getVariables(this);
 	}
 
 	@Override
