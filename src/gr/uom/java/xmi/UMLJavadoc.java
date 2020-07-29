67bc9adad21453715d8c126d51dd590d944b0746
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:03:35 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLOperation,toString#,MoveMethod,OK

--- src/gr/uom/java/xmi/UMLJavadoc.java
+++ src/gr/uom/java/xmi/UMLJavadoc.java
@@ -35,4 +35,33 @@ public class UMLJavadoc {
 		}
 		return false;
 	}
+
+	public String toString(UMLOperation umlOperation) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(umlOperation.visibility);
+		sb.append(" ");
+		if(umlOperation.isAbstract) {
+			sb.append("abstract");
+			sb.append(" ");
+		}
+		sb.append(umlOperation.name);
+		UMLParameter returnParameter = umlOperation.getReturnParameter();
+		List<UMLParameter> parameters = new ArrayList<UMLParameter>(umlOperation.parameters);
+		parameters.remove(returnParameter);
+		sb.append("(");
+		for(int i=0; i<parameters.size(); i++) {
+			UMLParameter parameter = parameters.get(i);
+			if(parameter.getKind().equals("in")) {
+				sb.append(parameter);
+				if(i < parameters.size()-1)
+					sb.append(", ");
+			}
+		}
+		sb.append(")");
+		if(returnParameter != null) {
+			sb.append(" : ");
+			sb.append(returnParameter.toString());
+		}
+		return sb.toString();
+	}
 }
