e7207084b18b6fbd96445288193504678fc1b328
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:43:01 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,TryStatementObject,addCatchClause#QCompositeStatementObject,MoveMethod,OK

--- src/gr/uom/java/xmi/decomposition/CompositeStatementObject.java
+++ src/gr/uom/java/xmi/decomposition/CompositeStatementObject.java
@@ -531,4 +531,8 @@ public class CompositeStatementObject extends AbstractStatement {
 		}
 		return null;
 	}
+
+	public void addCatchClause(TryStatementObject tryStatementObject) {
+		tryStatementObject.catchClauses.add(this);
+	}
 }
