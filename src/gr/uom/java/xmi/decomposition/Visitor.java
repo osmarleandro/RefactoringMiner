aadeda4acf67d84a153e491873b246e53c823c7f
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:25:10 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AnonymousClassDeclarationObject,getLambdas#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/Visitor.java
+++ src/gr/uom/java/xmi/decomposition/Visitor.java
@@ -265,7 +265,7 @@ public class Visitor extends ASTVisitor {
 			this.arguments.removeAll(anonymous.getArguments());
 			this.ternaryOperatorExpressions.removeAll(anonymous.getTernaryOperatorExpressions());
 			this.anonymousClassDeclarations.removeAll(anonymous.getAnonymousClassDeclarations());
-			this.lambdas.removeAll(anonymous.getLambdas());
+			this.lambdas.removeAll(anonymous.getLocationInfo().getLambdas(this));
 		}
 	}
 
@@ -783,7 +783,7 @@ public class Visitor extends ASTVisitor {
 		lambdas.add(lambda);
 		if(current.getUserObject() != null) {
 			AnonymousClassDeclarationObject anonymous = (AnonymousClassDeclarationObject)current.getUserObject();
-			anonymous.getLambdas().add(lambda);
+			anonymous.getLocationInfo().getLambdas(this).add(lambda);
 		}
 		return false;
 	}
