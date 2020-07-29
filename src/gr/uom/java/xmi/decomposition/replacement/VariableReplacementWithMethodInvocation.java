9790b33d96693ba6717896b978a31c08e720c8bc
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:46:38 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition.replacement,VariableReplacementWithMethodInvocation,getInvokedOperation#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/replacement/VariableReplacementWithMethodInvocation.java
+++ src/gr/uom/java/xmi/decomposition/replacement/VariableReplacementWithMethodInvocation.java
@@ -3,17 +3,13 @@ package gr.uom.java.xmi.decomposition.replacement;
 import gr.uom.java.xmi.decomposition.OperationInvocation;
 
 public class VariableReplacementWithMethodInvocation extends Replacement {
-	private OperationInvocation invokedOperation;
+	public OperationInvocation invokedOperation;
 	private Direction direction;
 	
 	public VariableReplacementWithMethodInvocation(String before, String after, OperationInvocation invocation, Direction direction) {
 		super(before, after, ReplacementType.VARIABLE_REPLACED_WITH_METHOD_INVOCATION);
 		this.invokedOperation = invocation;
 		this.direction = direction;
-	}
-
-	public OperationInvocation getInvokedOperation() {
-		return invokedOperation;
 	}
 
 	public Direction getDirection() {
