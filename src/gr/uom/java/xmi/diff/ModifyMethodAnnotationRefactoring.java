53ea0ff2f967ecfaaa9530112082554b5328727e
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:46:12 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ModifyMethodAnnotationRefactoring,getInvolvedClassesBeforeRefactoring#,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
@@ -78,9 +78,7 @@ public class ModifyMethodAnnotationRefactoring implements Refactoring {
 
 	@Override
 	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOperationBefore().getLocationInfo().getFilePath(), getOperationBefore().getClassName()));
-		return pairs;
+		return annotationAfter.getInvolvedClassesBeforeRefactoring(this);
 	}
 
 	@Override
