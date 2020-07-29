e0822d50d2206b325a392f3690399348a7a66f5d
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:00:13 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLOperation,setConstructor#Z,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/UMLOperation.java
+++ src/gr/uom/java/xmi/UMLOperation.java
@@ -28,7 +28,7 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
 	private boolean isAbstract;
 	private List<UMLParameter> parameters;
 	private String className;
-	private boolean isConstructor;
+	boolean isConstructor;
 	private boolean isFinal;
 	private boolean isStatic;
 	private boolean emptyBody;
@@ -89,10 +89,6 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
 
 	public boolean isConstructor() {
 		return isConstructor;
-	}
-
-	public void setConstructor(boolean isConstructor) {
-		this.isConstructor = isConstructor;
 	}
 
 	public boolean isFinal() {
