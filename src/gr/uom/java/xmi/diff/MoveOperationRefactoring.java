c8632e5be4f1fa8c198bb2bd93ac2051ccead3e1
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:35:15 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveOperationRefactoring,getInvolvedClassesBeforeRefactoring#,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/MoveOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveOperationRefactoring.java
@@ -98,9 +98,7 @@ public class MoveOperationRefactoring implements Refactoring {
 	}
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOriginalOperation().getLocationInfo().getFilePath(), getOriginalOperation().getClassName()));
-		return pairs;
+		return bodyMapper.getInvolvedClassesBeforeRefactoring(this);
 	}
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
