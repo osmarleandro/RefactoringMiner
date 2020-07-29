4af2de9a4f8d1ad233537b97e0557a93803ed1ad
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:02:36 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLOperation,isSetter#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/UMLJavadoc.java
+++ src/gr/uom/java/xmi/UMLJavadoc.java
@@ -3,6 +3,9 @@ package gr.uom.java.xmi;
 import java.util.ArrayList;
 import java.util.List;
 
+import gr.uom.java.xmi.decomposition.AbstractStatement;
+import gr.uom.java.xmi.decomposition.StatementObject;
+
 public class UMLJavadoc {
 	private List<UMLTagElement> tags;
 
@@ -31,6 +34,22 @@ public class UMLJavadoc {
 		for(UMLTagElement tag : tags) {
 			if(tag.containsIgnoreCase(s)) {
 				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isSetter(UMLOperation umlOperation) {
+		List<String> parameterNames = umlOperation.getParameterNameList();
+		if(umlOperation.getBody() != null && parameterNames.size() == 1) {
+			List<AbstractStatement> statements = umlOperation.getBody().getCompositeStatement().getStatements();
+			if(statements.size() == 1 && statements.get(0) instanceof StatementObject) {
+				StatementObject statement = (StatementObject)statements.get(0);
+				for(String variable : statement.getVariables()) {
+					if(statement.getString().equals(variable + "=" + parameterNames.get(0) + ";\n")) {
+						return true;
+					}
+				}
 			}
 		}
 		return false;
