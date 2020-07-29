c95a64b8f088ab315b69e68ba9adb1427ce1db90
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:42:03 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,TernaryOperatorExpression,getElseExpression#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractExpression.java
+++ src/gr/uom/java/xmi/decomposition/AbstractExpression.java
@@ -203,4 +203,8 @@ public class AbstractExpression extends AbstractCodeFragment {
 	public CodeRange codeRange() {
 		return locationInfo.codeRange();
 	}
+
+	public AbstractExpression getElseExpression(TernaryOperatorExpression ternaryOperatorExpression) {
+		return ternaryOperatorExpression.elseExpression;
+	}
 }
