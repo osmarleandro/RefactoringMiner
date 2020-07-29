67cd31e305157c32323f44a8499441e3acba064b
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:51:14 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,attributeMerged#QUMLAttribute;QUMLAttribute;QListQRefactoring,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
@@ -544,7 +544,7 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 						if((!originalClass.containsAttributeWithName(pattern.getAfter()) || cyclicRename(renameMap, pattern)) &&
 								(!nextClass.containsAttributeWithName(pattern.getBefore()) || cyclicRename(renameMap, pattern)) &&
 								!inconsistentAttributeRename(pattern, aliasedAttributesInOriginalClass, aliasedAttributesInNextClass) &&
-								!attributeMerged(a1, a2, refactorings) && !attributeSplit(a1, a2, refactorings)) {
+								!a1.attributeMerged(a2, refactorings) && !attributeSplit(a1, a2, refactorings)) {
 							UMLAttributeDiff attributeDiff = new UMLAttributeDiff(a1, a2, operationBodyMapperList);
 							Set<Refactoring> attributeDiffRefactorings = attributeDiff.getRefactorings(set);
 							if(!refactorings.containsAll(attributeDiffRefactorings)) {
@@ -796,18 +796,6 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 			}
 		}
 		return newRefactorings;
-	}
-
-	private boolean attributeMerged(UMLAttribute a1, UMLAttribute a2, List<Refactoring> refactorings) {
-		for(Refactoring refactoring : refactorings) {
-			if(refactoring instanceof MergeAttributeRefactoring) {
-				MergeAttributeRefactoring merge = (MergeAttributeRefactoring)refactoring;
-				if(merge.getMergedAttributes().contains(a1.getVariableDeclaration()) && merge.getNewAttribute().equals(a2.getVariableDeclaration())) {
-					return true;
-				}
-			}
-		}
-		return false;
 	}
 
 	private boolean attributeSplit(UMLAttribute a1, UMLAttribute a2, List<Refactoring> refactorings) {
