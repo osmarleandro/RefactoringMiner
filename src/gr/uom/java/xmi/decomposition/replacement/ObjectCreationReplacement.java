12b18ea7510075c8e692e5017da496aa50a53b27
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:47:00 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition.replacement,ObjectCreationReplacement,getCreatedObjectAfter#,MoveMethod,OK

--- src/gr/uom/java/xmi/decomposition/replacement/ObjectCreationReplacement.java
+++ src/gr/uom/java/xmi/decomposition/replacement/ObjectCreationReplacement.java
@@ -4,7 +4,7 @@ import gr.uom.java.xmi.decomposition.ObjectCreation;
 
 public class ObjectCreationReplacement extends Replacement {
 	private ObjectCreation createdObjectBefore;
-	private ObjectCreation createdObjectAfter;
+	public ObjectCreation createdObjectAfter;
 
 	public ObjectCreationReplacement(String before, String after,
 			ObjectCreation createdObjectBefore, ObjectCreation createdObjectAfter,
@@ -16,10 +16,6 @@ public class ObjectCreationReplacement extends Replacement {
 
 	public ObjectCreation getCreatedObjectBefore() {
 		return createdObjectBefore;
-	}
-
-	public ObjectCreation getCreatedObjectAfter() {
-		return createdObjectAfter;
 	}
 
 }
