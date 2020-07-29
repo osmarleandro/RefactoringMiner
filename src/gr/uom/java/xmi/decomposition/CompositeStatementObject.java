8da5b86245a628be4ba2368688f3cca87cbce8b0
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:20:52 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractStatement,setParent#QCompositeStatementObject,MoveMethod,OK

--- src/gr/uom/java/xmi/decomposition/CompositeStatementObject.java
+++ src/gr/uom/java/xmi/decomposition/CompositeStatementObject.java
@@ -33,7 +33,7 @@ public class CompositeStatementObject extends AbstractStatement {
 	public void addStatement(AbstractStatement statement) {
 		statement.setIndex(statementList.size());
 		statementList.add(statement);
-		statement.setParent(this);
+		setParent(statement);
 	}
 
 	public List<AbstractStatement> getStatements() {
@@ -530,5 +530,9 @@ public class CompositeStatementObject extends AbstractStatement {
 			}
 		}
 		return null;
+	}
+
+	public void setParent(AbstractStatement abstractStatement) {
+		abstractStatement.parent = this;
 	}
 }
