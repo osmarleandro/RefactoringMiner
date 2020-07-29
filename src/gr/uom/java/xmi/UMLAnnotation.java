2507e65deb9819da06a9b27c212a945260a25e19
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:04:07 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,AddMethodAnnotationRefactoring,equals#QObject,MoveMethod,OK

--- src/gr/uom/java/xmi/UMLAnnotation.java
+++ src/gr/uom/java/xmi/UMLAnnotation.java
@@ -13,6 +13,7 @@ import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
 
 import gr.uom.java.xmi.LocationInfo.CodeElementType;
 import gr.uom.java.xmi.decomposition.AbstractExpression;
+import gr.uom.java.xmi.diff.AddMethodAnnotationRefactoring;
 import gr.uom.java.xmi.diff.CodeRange;
 
 public class UMLAnnotation implements Serializable, LocationInfoProvider {
@@ -157,5 +158,31 @@ public class UMLAnnotation implements Serializable, LocationInfoProvider {
 		for (Map.Entry<String, AbstractExpression> entry : memberValuePairs.entrySet())
 			h += (entry.getKey() == null ? 0 : entry.getKey().hashCode()) ^ (entry.getValue() == null ? 0 : entry.getValue().getExpression().hashCode());
 		return h;
+	}
+
+	public boolean equals(AddMethodAnnotationRefactoring addMethodAnnotationRefactoring, Object obj) {
+		if (addMethodAnnotationRefactoring == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (addMethodAnnotationRefactoring.getClass() != obj.getClass())
+			return false;
+		AddMethodAnnotationRefactoring other = (AddMethodAnnotationRefactoring) obj;
+		if (this == null) {
+			if (other.annotation != null)
+				return false;
+		} else if (!equals(other.annotation))
+			return false;
+		if (addMethodAnnotationRefactoring.operationAfter == null) {
+			if (other.operationAfter != null)
+				return false;
+		} else if (!addMethodAnnotationRefactoring.operationAfter.equals(other.operationAfter))
+			return false;
+		if (addMethodAnnotationRefactoring.operationBefore == null) {
+			if (other.operationBefore != null)
+				return false;
+		} else if (!addMethodAnnotationRefactoring.operationBefore.equals(other.operationBefore))
+			return false;
+		return true;
 	}
 }
