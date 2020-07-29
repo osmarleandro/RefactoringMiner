c44f46a08ba3c43bcaea8e2529ea514e4041652e
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:42:21 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,LeafMapping,compareTo#QLeafMapping,MoveMethod,OK

--- src/gr/uom/java/xmi/decomposition/LeafMapping.java
+++ src/gr/uom/java/xmi/decomposition/LeafMapping.java
@@ -14,67 +14,6 @@ public class LeafMapping extends AbstractCodeMapping implements Comparable<LeafM
 		super(statement1, statement2, operation1, operation2);
 	}
 
-	@Override
-	public int compareTo(LeafMapping o) {
-		double distance1;
-		double distance2;
-		if(this.getFragment1().getString().equals(this.getFragment2().getString())) {
-			distance1 = 0;
-		}
-		else {
-			String s1 = this.getFragment1().getString().toLowerCase();
-			String s2 = this.getFragment2().getString().toLowerCase();
-			int distance = StringDistance.editDistance(s1, s2);
-			distance1 = (double)distance/(double)Math.max(s1.length(), s2.length());
-		}
-		
-		if(o.getFragment1().getString().equals(o.getFragment2().getString())) {
-			distance2 = 0;
-		}
-		else {
-			String s1 = o.getFragment1().getString().toLowerCase();
-			String s2 = o.getFragment2().getString().toLowerCase();
-			int distance = StringDistance.editDistance(s1, s2);
-			distance2 = (double)distance/(double)Math.max(s1.length(), s2.length());
-		}
-		
-		if(distance1 != distance2) {
-			if(this.isIdenticalWithExtractedVariable() && !o.isIdenticalWithExtractedVariable()) {
-				return -1;
-			}
-			else if(!this.isIdenticalWithExtractedVariable() && o.isIdenticalWithExtractedVariable()) {
-				return 1;
-			}
-			if(this.isIdenticalWithInlinedVariable() && !o.isIdenticalWithInlinedVariable()) {
-				return -1;
-			}
-			else if(!this.isIdenticalWithInlinedVariable() && o.isIdenticalWithInlinedVariable()) {
-				return 1;
-			}
-			return Double.compare(distance1, distance2);
-		}
-		else {
-			int depthDiff1 = Math.abs(this.getFragment1().getDepth() - this.getFragment2().getDepth());
-			int depthDiff2 = Math.abs(o.getFragment1().getDepth() - o.getFragment2().getDepth());
-
-			if(depthDiff1 != depthDiff2) {
-				return Integer.valueOf(depthDiff1).compareTo(Integer.valueOf(depthDiff2));
-			}
-			else {
-				int indexDiff1 = Math.abs(this.getFragment1().getIndex() - this.getFragment2().getIndex());
-				int indexDiff2 = Math.abs(o.getFragment1().getIndex() - o.getFragment2().getIndex());
-				if(indexDiff1 != indexDiff2) {
-					return Integer.valueOf(indexDiff1).compareTo(Integer.valueOf(indexDiff2));
-				}
-				else {
-					double parentEditDistance1 = this.parentEditDistance();
-					double parentEditDistance2 = o.parentEditDistance();
-					return Double.compare(parentEditDistance1, parentEditDistance2);
-				}
-			}
-		}
-	}
-
 	private double parentEditDistance() {
 		CompositeStatementObject parent1 = getFragment1().getParent();
 		while(parent1 != null && parent1.getLocationInfo().getCodeElementType().equals(CodeElementType.BLOCK)) {
@@ -114,5 +53,65 @@ public class LeafMapping extends AbstractCodeMapping implements Comparable<LeafM
 			return invocation1.callChainIntersection(invocation2);
 		}
 		return new LinkedHashSet<String>();
+	}
+
+	public int compareTo(LeafMapping leafMapping) {
+		double distance1;
+		double distance2;
+		if(leafMapping.getFragment1().getString().equals(leafMapping.getFragment2().getString())) {
+			distance1 = 0;
+		}
+		else {
+			String s1 = leafMapping.getFragment1().getString().toLowerCase();
+			String s2 = leafMapping.getFragment2().getString().toLowerCase();
+			int distance = StringDistance.editDistance(s1, s2);
+			distance1 = (double)distance/(double)Math.max(s1.length(), s2.length());
+		}
+		
+		if(getFragment1().getString().equals(getFragment2().getString())) {
+			distance2 = 0;
+		}
+		else {
+			String s1 = getFragment1().getString().toLowerCase();
+			String s2 = getFragment2().getString().toLowerCase();
+			int distance = StringDistance.editDistance(s1, s2);
+			distance2 = (double)distance/(double)Math.max(s1.length(), s2.length());
+		}
+		
+		if(distance1 != distance2) {
+			if(leafMapping.isIdenticalWithExtractedVariable() && !isIdenticalWithExtractedVariable()) {
+				return -1;
+			}
+			else if(!leafMapping.isIdenticalWithExtractedVariable() && isIdenticalWithExtractedVariable()) {
+				return 1;
+			}
+			if(leafMapping.isIdenticalWithInlinedVariable() && !isIdenticalWithInlinedVariable()) {
+				return -1;
+			}
+			else if(!leafMapping.isIdenticalWithInlinedVariable() && isIdenticalWithInlinedVariable()) {
+				return 1;
+			}
+			return Double.compare(distance1, distance2);
+		}
+		else {
+			int depthDiff1 = Math.abs(leafMapping.getFragment1().getDepth() - leafMapping.getFragment2().getDepth());
+			int depthDiff2 = Math.abs(getFragment1().getDepth() - getFragment2().getDepth());
+	
+			if(depthDiff1 != depthDiff2) {
+				return Integer.valueOf(depthDiff1).compareTo(Integer.valueOf(depthDiff2));
+			}
+			else {
+				int indexDiff1 = Math.abs(leafMapping.getFragment1().getIndex() - leafMapping.getFragment2().getIndex());
+				int indexDiff2 = Math.abs(getFragment1().getIndex() - getFragment2().getIndex());
+				if(indexDiff1 != indexDiff2) {
+					return Integer.valueOf(indexDiff1).compareTo(Integer.valueOf(indexDiff2));
+				}
+				else {
+					double parentEditDistance1 = leafMapping.parentEditDistance();
+					double parentEditDistance2 = parentEditDistance();
+					return Double.compare(parentEditDistance1, parentEditDistance2);
+				}
+			}
+		}
 	}
 }
