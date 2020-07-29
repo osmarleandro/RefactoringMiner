12def5e3f788ec5e3549bc4bbb81152506637116
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:35:39 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,CandidateExtractClassRefactoring,getRefactoring#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
@@ -1671,4 +1671,8 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 	public UMLModelDiff getModelDiff() {
 		return modelDiff;
 	}
+
+	public ExtractClassRefactoring getRefactoring(CandidateExtractClassRefactoring candidateExtractClassRefactoring) {
+		return candidateExtractClassRefactoring.refactoring;
+	}
 }
