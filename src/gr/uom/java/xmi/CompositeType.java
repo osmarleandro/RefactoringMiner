8460f8c64bc052b302edc32945ae84d156fc6707
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:12:28 -0300

    RefactoringMiner,gr.uom.java.xmi,CompositeType,toString#,MoveMethod,OK

--- src/gr/uom/java/xmi/CompositeType.java
+++ src/gr/uom/java/xmi/CompositeType.java
@@ -1,8 +1,8 @@
 package gr.uom.java.xmi;
 
 public class CompositeType extends UMLType {
-	private UMLType leftType;
-	private LeafType rightType;
+	UMLType leftType;
+	LeafType rightType;
 
 	public CompositeType(UMLType leftType, LeafType rightType) {
 		this.leftType = leftType;
@@ -50,7 +50,7 @@ public class CompositeType extends UMLType {
 
 	@Override
 	public String toString() {
-		return leftType.toString() + "." + rightType.toString();
+		return leftType.toString(this);
 	}
 
 	@Override
--- src/gr/uom/java/xmi/UMLType.java
+++ src/gr/uom/java/xmi/UMLType.java
@@ -169,6 +169,10 @@ public abstract class UMLType implements Serializable, LocationInfoProvider {
 		return normalized;
 	}
 
+	public String toString(CompositeType compositeType) {
+		return toString() + "." + compositeType.rightType.toString();
+	}
+
 	public static LeafType extractTypeObject(String qualifiedName) {
 		int arrayDimension = 0;
 		List<UMLType> typeArgumentDecomposition = new ArrayList<UMLType>();
