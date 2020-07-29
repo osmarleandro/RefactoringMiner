44037c6976c92f67e1663accc2106bdfed8b6bdb
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:39:49 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenameOperationRefactoring,getName#,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/RenameOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenameOperationRefactoring.java
@@ -61,7 +61,7 @@ public class RenameOperationRefactoring implements Refactoring {
 	}
 
 	public String getName() {
-		return this.getRefactoringType().getDisplayName();
+		return bodyMapper.getName(this);
 	}
 
 	public RefactoringType getRefactoringType() {
