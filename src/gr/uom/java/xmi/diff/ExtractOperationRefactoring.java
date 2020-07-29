8b77f325474abfe355804ba4c52b3923b0e883f2
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:58:18 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractOperationRefactoring,getReplacements#,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/ExtractOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractOperationRefactoring.java
@@ -23,7 +23,7 @@ public class ExtractOperationRefactoring implements Refactoring {
 	private UMLOperation sourceOperationBeforeExtraction;
 	private UMLOperation sourceOperationAfterExtraction;
 	private List<OperationInvocation> extractedOperationInvocations;
-	private Set<Replacement> replacements;
+	public Set<Replacement> replacements;
 	private Set<AbstractCodeFragment> extractedCodeFragmentsFromSourceOperation;
 	private Set<AbstractCodeFragment> extractedCodeFragmentsToExtractedOperation;
 	private UMLOperationBodyMapper bodyMapper;
@@ -101,10 +101,6 @@ public class ExtractOperationRefactoring implements Refactoring {
 
 	public List<OperationInvocation> getExtractedOperationInvocations() {
 		return extractedOperationInvocations;
-	}
-
-	public Set<Replacement> getReplacements() {
-		return replacements;
 	}
 
 	public Set<AbstractCodeFragment> getExtractedCodeFragmentsFromSourceOperation() {
