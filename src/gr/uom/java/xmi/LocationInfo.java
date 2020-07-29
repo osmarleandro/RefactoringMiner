ac266e95adc726520b2cad42b83f2bec300145a3
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:37:25 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,CompositeStatementObject,getPostfixExpressions#,MoveMethod,OK

--- src/gr/uom/java/xmi/LocationInfo.java
+++ src/gr/uom/java/xmi/LocationInfo.java
@@ -1,8 +1,13 @@
 package gr.uom.java.xmi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
+import gr.uom.java.xmi.decomposition.AbstractExpression;
+import gr.uom.java.xmi.decomposition.CompositeStatementObject;
 import gr.uom.java.xmi.diff.CodeRange;
 
 public class LocationInfo {
@@ -133,6 +138,14 @@ public class LocationInfo {
 		return true;
 	}
 	
+	public List<String> getPostfixExpressions(CompositeStatementObject compositeStatementObject) {
+		List<String> postfixExpressions = new ArrayList<String>();
+		for(AbstractExpression expression : compositeStatementObject.expressionList) {
+			postfixExpressions.addAll(expression.getPostfixExpressions());
+		}
+		return postfixExpressions;
+	}
+
 	public enum CodeElementType {
 		TYPE_DECLARATION,
 		METHOD_DECLARATION,
--- src/gr/uom/java/xmi/decomposition/CompositeStatementObject.java
+++ src/gr/uom/java/xmi/decomposition/CompositeStatementObject.java
@@ -17,7 +17,7 @@ import gr.uom.java.xmi.diff.CodeRange;
 public class CompositeStatementObject extends AbstractStatement {
 
 	private List<AbstractStatement> statementList;
-	private List<AbstractExpression> expressionList;
+	public List<AbstractExpression> expressionList;
 	private List<VariableDeclaration> variableDeclarations;
 	private LocationInfo locationInfo;
 
@@ -235,11 +235,7 @@ public class CompositeStatementObject extends AbstractStatement {
 
 	@Override
 	public List<String> getPostfixExpressions() {
-		List<String> postfixExpressions = new ArrayList<String>();
-		for(AbstractExpression expression : expressionList) {
-			postfixExpressions.addAll(expression.getPostfixExpressions());
-		}
-		return postfixExpressions;
+		return locationInfo.getPostfixExpressions(this);
 	}
 
 	@Override
