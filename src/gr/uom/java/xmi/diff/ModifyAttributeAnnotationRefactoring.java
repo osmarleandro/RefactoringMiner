68b4062ec7a2eaafed89125f58c230fa733f5d74
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:06:09 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ModifyAttributeAnnotationRefactoring,toString#,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/ModifyAttributeAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ModifyAttributeAnnotationRefactoring.java
@@ -13,10 +13,10 @@ import gr.uom.java.xmi.UMLAnnotation;
 import gr.uom.java.xmi.UMLAttribute;
 
 public class ModifyAttributeAnnotationRefactoring implements Refactoring {
-	private UMLAnnotation annotationBefore;
-	private UMLAnnotation annotationAfter;
+	public UMLAnnotation annotationBefore;
+	public UMLAnnotation annotationAfter;
 	private UMLAttribute attributeBefore;
-	private UMLAttribute attributeAfter;
+	public UMLAttribute attributeAfter;
 
 	public ModifyAttributeAnnotationRefactoring(UMLAnnotation annotationBefore, UMLAnnotation annotationAfter,
 			UMLAttribute attributeBefore, UMLAttribute attributeAfter) {
@@ -91,16 +91,7 @@ public class ModifyAttributeAnnotationRefactoring implements Refactoring {
 	}
 
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getName()).append("\t");
-		sb.append(annotationBefore);
-		sb.append(" to ");
-		sb.append(annotationAfter);
-		sb.append(" in attribute ");
-		sb.append(attributeAfter);
-		sb.append(" from class ");
-		sb.append(attributeAfter.getClassName());
-		return sb.toString();
+		return annotationAfter.toString(this);
 	}
 	@Override
 	public int hashCode() {
