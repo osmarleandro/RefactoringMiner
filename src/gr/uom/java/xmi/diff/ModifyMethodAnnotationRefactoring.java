8ff988968e2508c8b144bead8f8d2ef1261067f5
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:45:38 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ModifyMethodAnnotationRefactoring,getAnnotationBefore#,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
@@ -13,7 +13,7 @@ import gr.uom.java.xmi.UMLAnnotation;
 import gr.uom.java.xmi.UMLOperation;
 
 public class ModifyMethodAnnotationRefactoring implements Refactoring {
-	private UMLAnnotation annotationBefore;
+	public UMLAnnotation annotationBefore;
 	private UMLAnnotation annotationAfter;
 	private UMLOperation operationBefore;
 	private UMLOperation operationAfter;
@@ -24,10 +24,6 @@ public class ModifyMethodAnnotationRefactoring implements Refactoring {
 		this.annotationAfter = annotationAfter;
 		this.operationBefore = operationBefore;
 		this.operationAfter = operationAfter;
-	}
-
-	public UMLAnnotation getAnnotationBefore() {
-		return annotationBefore;
 	}
 
 	public UMLAnnotation getAnnotationAfter() {
