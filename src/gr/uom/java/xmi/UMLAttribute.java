5440548068f2c17dd2dd9d6c3034d9e5945768bf
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 20:42:41 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLAttributeDiff,getRefactorings#QSetQCandidateAttributeRefactoring,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/UMLAttribute.java
+++ src/gr/uom/java/xmi/UMLAttribute.java
@@ -1,12 +1,21 @@
 package gr.uom.java.xmi;
 
 import gr.uom.java.xmi.decomposition.VariableDeclaration;
+import gr.uom.java.xmi.decomposition.VariableReferenceExtractor;
+import gr.uom.java.xmi.diff.CandidateAttributeRefactoring;
+import gr.uom.java.xmi.diff.ChangeAttributeTypeRefactoring;
 import gr.uom.java.xmi.diff.CodeRange;
+import gr.uom.java.xmi.diff.RenameAttributeRefactoring;
 import gr.uom.java.xmi.diff.StringDistance;
+import gr.uom.java.xmi.diff.UMLAttributeDiff;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.refactoringminer.api.Refactoring;
 
 public class UMLAttribute implements Comparable<UMLAttribute>, Serializable, LocationInfoProvider, VariableDeclarationProvider {
 	private LocationInfo locationInfo;
@@ -170,5 +179,24 @@ public class UMLAttribute implements Comparable<UMLAttribute>, Serializable, Loc
 		int distance = StringDistance.editDistance(s1, s2);
 		double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
 		return normalized;
+	}
+
+	public Set<Refactoring> getRefactorings(UMLAttributeDiff umlAttributeDiff, Set<CandidateAttributeRefactoring> set) {
+		Set<Refactoring> refactorings = new LinkedHashSet<Refactoring>();
+		RenameAttributeRefactoring rename = null;
+		if(umlAttributeDiff.isRenamed()) {
+			rename = new RenameAttributeRefactoring(umlAttributeDiff.removedAttribute.getVariableDeclaration(), getVariableDeclaration(), umlAttributeDiff.removedAttribute.getClassName(), getClassName(), set);
+			refactorings.add(rename);
+		}
+		if(umlAttributeDiff.isTypeChanged() || umlAttributeDiff.isQualifiedTypeChanged()) {
+			ChangeAttributeTypeRefactoring ref = new ChangeAttributeTypeRefactoring(umlAttributeDiff.removedAttribute.getVariableDeclaration(), getVariableDeclaration(), umlAttributeDiff.removedAttribute.getClassName(), getClassName(),
+					VariableReferenceExtractor.findReferences(umlAttributeDiff.removedAttribute.getVariableDeclaration(), getVariableDeclaration(), umlAttributeDiff.operationBodyMapperList));
+			refactorings.add(ref);
+			if(rename != null) {
+				ref.addRelatedRefactoring(rename);
+			}
+		}
+		refactorings.addAll(umlAttributeDiff.getAnnotationRefactorings());
+		return refactorings;
 	}
 }
