f9bb1793246589ee4ec3d05ff89b46bcc49d2b01
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:55:33 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveClassRefactoring,getName#,MoveMethod,OK

--- src/gr/uom/java/xmi/diff/MoveClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveClassRefactoring.java
@@ -41,7 +41,7 @@ public class MoveClassRefactoring implements Refactoring {
 	}
 
 	public String getName() {
-		return this.getRefactoringType().getDisplayName();
+		return movedClass.getName(this);
 	}
 
 	public RefactoringType getRefactoringType() {
