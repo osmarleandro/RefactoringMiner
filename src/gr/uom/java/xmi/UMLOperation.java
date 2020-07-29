9ccdca0af9f613becd84f87cd43ae0f383189fc9
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 19:13:00 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLAbstractClass,containsOperationWithTheSameSignature#QUMLOperation,MoveMethod,OK

--- src/gr/uom/java/xmi/UMLOperation.java
+++ src/gr/uom/java/xmi/UMLOperation.java
@@ -833,4 +833,12 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
 		}
 		return null;
 	}
+
+	public boolean containsOperationWithTheSameSignature(UMLAbstractClass umlAbstractClass) {
+		for(UMLOperation originalOperation : umlAbstractClass.operations) {
+			if(originalOperation.equalSignature(this))
+				return true;
+		}
+		return false;
+	}
 }
