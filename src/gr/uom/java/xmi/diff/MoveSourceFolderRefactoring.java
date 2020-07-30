5cd4003723d096dd6c3b35079c1aa77a9a944dfc
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:57:50 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractAttributeRefactoring,hashCode#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/MoveSourceFolderRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveSourceFolderRefactoring.java
@@ -92,4 +92,10 @@ public class MoveSourceFolderRefactoring implements Refactoring {
 		}
 		return ranges;
 	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
