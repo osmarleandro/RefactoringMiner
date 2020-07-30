3f4cf29f6f2f750160bf999e4b94aa9a0738bf54
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 14:36:28 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLAttribute,setVariableDeclaration#QVariableDeclaration,PullUpMethod,OK

--- src/gr/uom/java/xmi/decomposition/LambdaExpressionObject.java
+++ src/gr/uom/java/xmi/decomposition/LambdaExpressionObject.java
@@ -41,4 +41,10 @@ public class LambdaExpressionObject implements LocationInfoProvider {
 	public CodeRange codeRange() {
 		return locationInfo.codeRange();
 	}
+
+	@Override
+	public void setVariableDeclaration(VariableDeclaration variableDeclaration) {
+		// TODO Auto-generated method stub
+		
+	}
 }
