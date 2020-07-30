e9dd4bd3b6bcb32a1d4cedd7b5e35a2529b5697e
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:06:59 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,VariableDeclaration,hashCode#,PullUpMethod,OK

--- src/gr/uom/java/xmi/decomposition/AnonymousClassDeclarationObject.java
+++ src/gr/uom/java/xmi/decomposition/AnonymousClassDeclarationObject.java
@@ -133,4 +133,10 @@ public class AnonymousClassDeclarationObject implements LocationInfoProvider {
 	public CodeRange codeRange() {
 		return locationInfo.codeRange();
 	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
