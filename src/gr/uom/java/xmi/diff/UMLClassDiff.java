25c9403fd036abd417b33a3411c33923226e0d62
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 12:11:42 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,findBestMapper#QTreeSetQUMLOperationBodyMapper,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLClassDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassDiff.java
@@ -3,6 +3,7 @@ package gr.uom.java.xmi.diff;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.TreeSet;
 
 import org.refactoringminer.api.RefactoringMinerTimedOutException;
 
@@ -11,6 +12,7 @@ import gr.uom.java.xmi.UMLAttribute;
 import gr.uom.java.xmi.UMLClass;
 import gr.uom.java.xmi.UMLOperation;
 import gr.uom.java.xmi.UMLType;
+import gr.uom.java.xmi.decomposition.OperationInvocation;
 import gr.uom.java.xmi.decomposition.UMLOperationBodyMapper;
 import gr.uom.java.xmi.decomposition.VariableReferenceExtractor;
 
@@ -194,5 +196,54 @@ public class UMLClassDiff extends UMLClassBaseDiff {
 
 	public boolean matches(UMLType type) {
 		return this.className.endsWith("." + type.getClassType());
+	}
+
+	private UMLOperationBodyMapper findBestMapper(TreeSet<UMLOperationBodyMapper> mapperSet) {
+		List<UMLOperationBodyMapper> mapperList = new ArrayList<UMLOperationBodyMapper>(mapperSet);
+		UMLOperationBodyMapper bestMapper = mapperSet.first();
+		UMLOperation bestMapperOperation1 = bestMapper.getOperation1();
+		UMLOperation bestMapperOperation2 = bestMapper.getOperation2();
+		if(bestMapperOperation1.equalReturnParameter(bestMapperOperation2) &&
+				bestMapperOperation1.getName().equals(bestMapperOperation2.getName()) &&
+				bestMapperOperation1.commonParameterTypes(bestMapperOperation2).size() > 0) {
+			return bestMapper;
+		}
+		for(int i=1; i<mapperList.size(); i++) {
+			UMLOperationBodyMapper mapper = mapperList.get(i);
+			UMLOperation operation2 = mapper.getOperation2();
+			List<OperationInvocation> operationInvocations2 = operation2.getAllOperationInvocations();
+			boolean anotherMapperCallsOperation2OfTheBestMapper = false;
+			for(OperationInvocation invocation : operationInvocations2) {
+				if(invocation.matchesOperation(bestMapper.getOperation2(), operation2.variableTypeMap(), modelDiff) && !invocation.matchesOperation(bestMapper.getOperation1(), operation2.variableTypeMap(), modelDiff) &&
+						!operationContainsMethodInvocationWithTheSameNameAndCommonArguments(invocation, removedOperations)) {
+					anotherMapperCallsOperation2OfTheBestMapper = true;
+					break;
+				}
+			}
+			UMLOperation operation1 = mapper.getOperation1();
+			List<OperationInvocation> operationInvocations1 = operation1.getAllOperationInvocations();
+			boolean anotherMapperCallsOperation1OfTheBestMapper = false;
+			for(OperationInvocation invocation : operationInvocations1) {
+				if(invocation.matchesOperation(bestMapper.getOperation1(), operation1.variableTypeMap(), modelDiff) && !invocation.matchesOperation(bestMapper.getOperation2(), operation1.variableTypeMap(), modelDiff) &&
+						!operationContainsMethodInvocationWithTheSameNameAndCommonArguments(invocation, addedOperations)) {
+					anotherMapperCallsOperation1OfTheBestMapper = true;
+					break;
+				}
+			}
+			boolean nextMapperMatchesConsistentRename = matchesConsistentMethodInvocationRename(mapper, consistentMethodInvocationRenames);
+			boolean bestMapperMismatchesConsistentRename = mismatchesConsistentMethodInvocationRename(bestMapper, consistentMethodInvocationRenames);
+			if(bestMapperMismatchesConsistentRename && nextMapperMatchesConsistentRename) {
+				bestMapper = mapper;
+				break;
+			}
+			if(anotherMapperCallsOperation2OfTheBestMapper || anotherMapperCallsOperation1OfTheBestMapper) {
+				bestMapper = mapper;
+				break;
+			}
+		}
+		if(mismatchesConsistentMethodInvocationRename(bestMapper, consistentMethodInvocationRenames)) {
+			return null;
+		}
+		return bestMapper;
 	}
 }
