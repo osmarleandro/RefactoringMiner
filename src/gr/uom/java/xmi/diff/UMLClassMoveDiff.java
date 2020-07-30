cf07bac262dbad65cf7dca1a30004faf592648af
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 12:07:22 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,nextClassImportsType#QString,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLClassMoveDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassMoveDiff.java
@@ -33,4 +33,8 @@ public class UMLClassMoveDiff extends UMLClassBaseDiff {
 		}
 		return false;
 	}
+
+	public boolean nextClassImportsType(String targetClass) {
+		return nextClass.importsType(targetClass);
+	}
 }
