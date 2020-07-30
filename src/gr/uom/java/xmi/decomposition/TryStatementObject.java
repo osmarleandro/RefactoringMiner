d1645d109c5ba05cc63b535707d310f6bbe8fa96
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:13:14 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,TryStatementObject,getCatchClauses#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/TryStatementObject.java
+++ src/gr/uom/java/xmi/decomposition/TryStatementObject.java
@@ -9,7 +9,7 @@ import org.eclipse.jdt.core.dom.Statement;
 import gr.uom.java.xmi.LocationInfo.CodeElementType;
 
 public class TryStatementObject extends CompositeStatementObject {
-	private List<CompositeStatementObject> catchClauses;
+	List<CompositeStatementObject> catchClauses;
 	private CompositeStatementObject finallyClause;
 
 	public TryStatementObject(CompilationUnit cu, String filePath, Statement statement, int depth) {
@@ -19,10 +19,6 @@ public class TryStatementObject extends CompositeStatementObject {
 
 	public void addCatchClause(CompositeStatementObject catchClause) {
 		catchClauses.add(catchClause);
-	}
-
-	public List<CompositeStatementObject> getCatchClauses() {
-		return catchClauses;
 	}
 
 	public void setFinallyClause(CompositeStatementObject finallyClause) {
