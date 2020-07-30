57e017fef6a24d3bcc5a11100d989e82f4aa75a3
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 14:48:07 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLType,isParameterized#,PullUpMethod,OK

--- src/gr/uom/java/xmi/UMLOperation.java
+++ src/gr/uom/java/xmi/UMLOperation.java
@@ -833,4 +833,10 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
 		}
 		return null;
 	}
+
+	@Override
+	public boolean isParameterized() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- src/gr/uom/java/xmi/UMLType.java
+++ src/gr/uom/java/xmi/UMLType.java
@@ -24,7 +24,7 @@ import gr.uom.java.xmi.diff.StringDistance;
 public abstract class UMLType implements Serializable, LocationInfoProvider {
 	private LocationInfo locationInfo;
 	private int arrayDimension;
-	private List<UMLType> typeArguments = new ArrayList<UMLType>();
+	List<UMLType> typeArguments = new ArrayList<UMLType>();
 	protected List<UMLAnnotation> annotations = new ArrayList<UMLAnnotation>();
 
 	public LocationInfo getLocationInfo() {
@@ -126,6 +126,7 @@ public abstract class UMLType implements Serializable, LocationInfoProvider {
 		return false;
 	}
 
+	@Override
 	public boolean isParameterized() {
 		return typeArguments.size() > 0;
 	}
