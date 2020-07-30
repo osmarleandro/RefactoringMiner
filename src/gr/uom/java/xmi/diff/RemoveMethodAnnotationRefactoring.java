7d9f44c325b4acefb147b24bff253da57e367fe0
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 16:02:49 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveAndRenameClassRefactoring,getOriginalClass#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
@@ -10,6 +10,7 @@ import org.refactoringminer.api.Refactoring;
 import org.refactoringminer.api.RefactoringType;
 
 import gr.uom.java.xmi.UMLAnnotation;
+import gr.uom.java.xmi.UMLClass;
 import gr.uom.java.xmi.UMLOperation;
 
 public class RemoveMethodAnnotationRefactoring implements Refactoring {
@@ -127,5 +128,11 @@ public class RemoveMethodAnnotationRefactoring implements Refactoring {
 		} else if (!operationBefore.equals(other.operationBefore))
 			return false;
 		return true;
+	}
+
+	@Override
+	public UMLClass getOriginalClass() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
