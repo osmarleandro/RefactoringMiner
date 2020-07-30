c79f626a591b1ec102b2a6aaa00ae3fddd5cb665
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:09:25 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,getArguments#,PullUpMethod,OK

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -52,6 +52,7 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	public abstract List<String> getArrayAccesses();
 	public abstract List<String> getPrefixExpressions();
 	public abstract List<String> getPostfixExpressions();
+	@Override
 	public abstract List<String> getArguments();
 	public abstract List<TernaryOperatorExpression> getTernaryOperatorExpressions();
 	public abstract List<LambdaExpressionObject> getLambdas();
