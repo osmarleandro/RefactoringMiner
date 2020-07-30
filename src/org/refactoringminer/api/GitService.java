b74255edf84c6a286926222a528bbd577e84f4ac
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 16:09:41 -0300

    RefactoringMiner,org.refactoringminer.util,GitServiceImpl,fileTreeDiff#QRepository;QRevCommit;QListQString;QListQString;QMapQStringQString,PullUpMethod,BUILD FAILED

--- src/org/refactoringminer/api/GitService.java
+++ src/org/refactoringminer/api/GitService.java
@@ -44,4 +44,6 @@ public interface GitService {
 	void fileTreeDiff(Repository repository, RevCommit currentCommit, List<String> filesBefore, List<String> filesCurrent, Map<String, String> renamedFilesHint) throws Exception;
 
 	Churn churn(Repository repository, RevCommit currentCommit) throws Exception;
+
+	void fileTreeDiff(Repository repository, RevCommit currentCommit, List<String> javaFilesBefore, List<String> javaFilesCurrent, Map<String, String> renamedFilesHint) throws Exception;
 }
--- src/org/refactoringminer/util/GitServiceImpl.java
+++ src/org/refactoringminer/util/GitServiceImpl.java
@@ -285,6 +285,7 @@ public class GitServiceImpl implements GitService {
 		}
 	}
 
+	@Override
 	public void fileTreeDiff(Repository repository, RevCommit currentCommit, List<String> javaFilesBefore, List<String> javaFilesCurrent, Map<String, String> renamedFilesHint) throws Exception {
         if (currentCommit.getParentCount() > 0) {
         	ObjectId oldTree = currentCommit.getParent(0).getTree();
@@ -321,7 +322,7 @@ public class GitServiceImpl implements GitService {
         }
 	}
 
-	private boolean isJavafile(String path) {
+	public boolean isJavafile(String path) {
 		return path.endsWith(".java");
 	}
 
