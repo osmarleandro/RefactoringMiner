e2cb6c68c72948bd9c2a0ff8930b54746b0938f8
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:40:08 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,PullUpAttributeRefactoring,toString#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MoveAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveAttributeRefactoring.java
@@ -128,4 +128,17 @@ public class MoveAttributeRefactoring implements Refactoring {
 				.setCodeElement(movedAttribute.toString()));
 		return ranges;
 	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(getName()).append("\t");
+		sb.append(getOriginalAttribute().toQualifiedString());
+		sb.append(" from class ");
+		sb.append(getSourceClassName());
+		sb.append(" to ");
+		sb.append(getMovedAttribute().toQualifiedString());
+		sb.append(" from class ");
+		sb.append(getTargetClassName());
+		return sb.toString();
+	}
 }
