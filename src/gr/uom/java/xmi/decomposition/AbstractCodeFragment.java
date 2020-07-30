445e47c51a92d6c74584f1eae168329115f30f20
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 12:00:11 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,getLambdas#,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -54,7 +54,6 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	public abstract List<String> getPostfixExpressions();
 	public abstract List<String> getArguments();
 	public abstract List<TernaryOperatorExpression> getTernaryOperatorExpressions();
-	public abstract List<LambdaExpressionObject> getLambdas();
 	public abstract VariableDeclaration searchVariableDeclaration(String variableName);
 	public abstract VariableDeclaration getVariableDeclaration(String variableName);
 	
