8aa78d92cd099a89457a6cc365dc9b0eca1d785f
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:05:23 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,CompositeStatementObject,getTernaryOperatorExpressions#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractStatement.java
+++ src/gr/uom/java/xmi/decomposition/AbstractStatement.java
@@ -1,5 +1,6 @@
 package gr.uom.java.xmi.decomposition;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractStatement extends AbstractCodeFragment {
@@ -30,4 +31,13 @@ public abstract class AbstractStatement extends AbstractCodeFragment {
 
     public abstract List<StatementObject> getLeaves();
     public abstract int statementCount();
+
+	@Override
+	public List<TernaryOperatorExpression> getTernaryOperatorExpressions() {
+		List<TernaryOperatorExpression> ternaryOperatorExpressions = new ArrayList<TernaryOperatorExpression>();
+		for(AbstractExpression expression : expressionList) {
+			ternaryOperatorExpressions.addAll(expression.getTernaryOperatorExpressions());
+		}
+		return ternaryOperatorExpressions;
+	}
 }
