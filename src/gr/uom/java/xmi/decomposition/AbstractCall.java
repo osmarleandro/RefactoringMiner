7c7e7edd0a9abdf5df7805d8ecd49863ffb020bf
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 11:52:07 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCall,makeReplacementForReturnedArgument#QString,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractCall.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCall.java
@@ -354,28 +354,16 @@ public abstract class AbstractCall implements LocationInfoProvider {
 		return argumentIntersectionSize;
 	}
 
-	private boolean argumentIsEqual(String statement) {
+	protected boolean argumentIsEqual(String statement) {
 		return statement.endsWith(";\n") && getArguments().size() == 1 &&
 				//length()-2 to remove ";\n" from the end of the statement
 				equalsIgnoringExtraParenthesis(getArguments().get(0), statement.substring(0, statement.length()-2));
 	}
 
-	private boolean argumentIsReturned(String statement) {
+	protected boolean argumentIsReturned(String statement) {
 		return statement.startsWith("return ") && getArguments().size() == 1 &&
 				//length()-2 to remove ";\n" from the end of the return statement, 7 to remove the prefix "return "
 				equalsIgnoringExtraParenthesis(getArguments().get(0), statement.substring(7, statement.length()-2));
-	}
-
-	public Replacement makeReplacementForReturnedArgument(String statement) {
-		if(argumentIsReturned(statement)) {
-			return new Replacement(getArguments().get(0), statement.substring(7, statement.length()-2),
-					ReplacementType.ARGUMENT_REPLACED_WITH_RETURN_EXPRESSION);
-		}
-		else if(argumentIsEqual(statement)) {
-			return new Replacement(getArguments().get(0), statement.substring(0, statement.length()-2),
-					ReplacementType.ARGUMENT_REPLACED_WITH_STATEMENT);
-		}
-		return null;
 	}
 
 	public Replacement makeReplacementForWrappedCall(String statement) {
