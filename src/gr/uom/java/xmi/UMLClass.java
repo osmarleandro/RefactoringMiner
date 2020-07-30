7d95e88e9357800c73b6d4bbf5951272a8331b1b
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-29 21:04:37 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractSuperclassRefactoring,getSubclassSet#,MoveMethod,BUILD FAILED

--- src/gr/uom/java/xmi/UMLClass.java
+++ src/gr/uom/java/xmi/UMLClass.java
@@ -1,10 +1,12 @@
 package gr.uom.java.xmi;
 
+import gr.uom.java.xmi.diff.ExtractSuperclassRefactoring;
 import gr.uom.java.xmi.diff.StringDistance;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
@@ -396,5 +398,13 @@ public class UMLClass extends UMLAbstractClass implements Comparable<UMLClass>, 
 			}
 		}
 		return new LinkedHashMap<String, Set<String>>();
+	}
+
+	public Set<String> getSubclassSet(ExtractSuperclassRefactoring extractSuperclassRefactoring) {
+		Set<String> subclassSet = new LinkedHashSet<String>();
+		for(UMLClass umlClass : extractSuperclassRefactoring.subclassSet) {
+			subclassSet.add(umlClass.getName());
+		}
+		return subclassSet;
 	}
 }
