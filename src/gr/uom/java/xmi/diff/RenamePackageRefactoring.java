02d546b32f091f878a16f500b4e7d12b0dbdb190
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 16:01:47 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenameVariableRefactoring,equals#QObject,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/RenamePackageRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenamePackageRefactoring.java
@@ -93,4 +93,10 @@ public class RenamePackageRefactoring implements Refactoring {
 		}
 		return ranges;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
