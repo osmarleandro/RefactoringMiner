bab4a8acc05a557ac88b4485caa07d6a4364bcfb
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:55:11 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractVariableRefactoring,getVariableDeclaration#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/RenameAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenameAttributeRefactoring.java
@@ -137,4 +137,10 @@ public class RenameAttributeRefactoring implements Refactoring {
 				.setCodeElement(renamedAttribute.toString()));
 		return ranges;
 	}
+
+	@Override
+	public VariableDeclaration getVariableDeclaration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/gr/uom/java/xmi/diff/SplitAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/SplitAttributeRefactoring.java
@@ -145,4 +145,10 @@ public class SplitAttributeRefactoring implements Refactoring {
 		}
 		return ranges;
 	}
+
+	@Override
+	public VariableDeclaration getVariableDeclaration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
