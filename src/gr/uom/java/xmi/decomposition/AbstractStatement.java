897568701f127bbb4d7d930101e8cb451f71c556
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:05:29 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,CompositeStatementObject,getCreationMap#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractStatement.java
+++ src/gr/uom/java/xmi/decomposition/AbstractStatement.java
@@ -1,6 +1,9 @@
 package gr.uom.java.xmi.decomposition;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractStatement extends AbstractCodeFragment {
 	private CompositeStatementObject parent;
@@ -30,4 +33,23 @@ public abstract class AbstractStatement extends AbstractCodeFragment {
 
     public abstract List<StatementObject> getLeaves();
     public abstract int statementCount();
+
+	@Override
+	public Map<String, List<ObjectCreation>> getCreationMap() {
+		Map<String, List<ObjectCreation>> map = new LinkedHashMap<String, List<ObjectCreation>>();
+		for(AbstractExpression expression : expressionList) {
+			Map<String, List<ObjectCreation>> expressionMap = expression.getCreationMap();
+			for(String key : expressionMap.keySet()) {
+				if(map.containsKey(key)) {
+					map.get(key).addAll(expressionMap.get(key));
+				}
+				else {
+					List<ObjectCreation> list = new ArrayList<ObjectCreation>();
+					list.addAll(expressionMap.get(key));
+					map.put(key, list);
+				}
+			}
+		}
+		return map;
+	}
 }
