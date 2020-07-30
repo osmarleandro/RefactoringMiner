bdfaa28ec376645aae2cc4b75ec4cede90693a32
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:08:35 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,getVariableDeclarations#,PullUpMethod,OK

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -39,6 +39,7 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	public abstract String getString();
 	public abstract List<String> getVariables();
 	public abstract List<String> getTypes();
+	@Override
 	public abstract List<VariableDeclaration> getVariableDeclarations();
 	public abstract Map<String, List<OperationInvocation>> getMethodInvocationMap();
 	public abstract List<AnonymousClassDeclarationObject> getAnonymousClassDeclarations();
