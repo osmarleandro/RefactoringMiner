81b6d584609626713e38ef606016864a70b4bb23
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 12:11:00 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,inconsistentAttributeRename#QReplacement;QMapQStringQSetQString;QMapQStringQSetQString,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLClassDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassDiff.java
@@ -3,6 +3,8 @@ package gr.uom.java.xmi.diff;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.refactoringminer.api.RefactoringMinerTimedOutException;
 
@@ -13,6 +15,7 @@ import gr.uom.java.xmi.UMLOperation;
 import gr.uom.java.xmi.UMLType;
 import gr.uom.java.xmi.decomposition.UMLOperationBodyMapper;
 import gr.uom.java.xmi.decomposition.VariableReferenceExtractor;
+import gr.uom.java.xmi.decomposition.replacement.Replacement;
 
 public class UMLClassDiff extends UMLClassBaseDiff {
 	
@@ -194,5 +197,47 @@ public class UMLClassDiff extends UMLClassBaseDiff {
 
 	public boolean matches(UMLType type) {
 		return this.className.endsWith("." + type.getClassType());
+	}
+
+	private boolean inconsistentAttributeRename(Replacement pattern, Map<String, Set<String>> aliasedAttributesInOriginalClass, Map<String, Set<String>> aliasedAttributesInNextClass) {
+		for(String key : aliasedAttributesInOriginalClass.keySet()) {
+			if(aliasedAttributesInOriginalClass.get(key).contains(pattern.getBefore())) {
+				return false;
+			}
+		}
+		for(String key : aliasedAttributesInNextClass.keySet()) {
+			if(aliasedAttributesInNextClass.get(key).contains(pattern.getAfter())) {
+				return false;
+			}
+		}
+		int counter = 0;
+		int allCases = 0;
+		for(UMLOperationBodyMapper mapper : this.operationBodyMapperList) {
+			List<String> allVariables1 = mapper.getOperation1().getAllVariables();
+			List<String> allVariables2 = mapper.getOperation2().getAllVariables();
+			for(UMLOperationBodyMapper nestedMapper : mapper.getChildMappers()) {
+				allVariables1.addAll(nestedMapper.getOperation1().getAllVariables());
+				allVariables2.addAll(nestedMapper.getOperation2().getAllVariables());
+			}
+			boolean variables1contains = (allVariables1.contains(pattern.getBefore()) &&
+					!mapper.getOperation1().getParameterNameList().contains(pattern.getBefore())) ||
+					allVariables1.contains("this."+pattern.getBefore());
+			boolean variables2Contains = (allVariables2.contains(pattern.getAfter()) &&
+					!mapper.getOperation2().getParameterNameList().contains(pattern.getAfter())) ||
+					allVariables2.contains("this."+pattern.getAfter());
+			if(variables1contains && !variables2Contains) {	
+				counter++;
+			}
+			if(variables2Contains && !variables1contains) {
+				counter++;
+			}
+			if(variables1contains || variables2Contains) {
+				allCases++;
+			}
+		}
+		double percentage = (double)counter/(double)allCases;
+		if(percentage > 0.5)
+			return true;
+		return false;
 	}
 }
