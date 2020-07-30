e824bb11141218c61589ef8f19ec49c01521b877
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 14:52:39 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLClass,setJavadoc#QUMLJavadoc,PullUpMethod,OK

--- src/gr/uom/java/xmi/UMLClass.java
+++ src/gr/uom/java/xmi/UMLClass.java
@@ -24,7 +24,7 @@ public class UMLClass extends UMLAbstractClass implements Comparable<UMLClass>, 
     private List<UMLAnonymousClass> anonymousClassList;
     private List<String> importedTypes;
     private List<UMLTypeParameter> typeParameters;
-    private UMLJavadoc javadoc;
+    UMLJavadoc javadoc;
     private List<UMLAnnotation> annotations;
     
     public UMLClass(String packageName, String name, LocationInfo locationInfo, boolean topLevel, List<String> importedTypes) {
@@ -188,6 +188,7 @@ public class UMLClass extends UMLAbstractClass implements Comparable<UMLClass>, 
 		return javadoc;
 	}
 
+	@Override
 	public void setJavadoc(UMLJavadoc javadoc) {
 		this.javadoc = javadoc;
 	}
