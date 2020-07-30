bf180e8a64a680bd09242ca4da1b1a796e69f611
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 11:59:29 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,getBooleanLiterals#,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -45,7 +45,6 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	public abstract List<String> getStringLiterals();
 	public abstract List<String> getNumberLiterals();
 	public abstract List<String> getNullLiterals();
-	public abstract List<String> getBooleanLiterals();
 	public abstract List<String> getTypeLiterals();
 	public abstract Map<String, List<ObjectCreation>> getCreationMap();
 	public abstract List<String> getInfixOperators();
