3e9f47afb98f371fefa49d6442152950e6922091
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:34:33 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RemoveMethodAnnotationRefactoring,getOperationAfter#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
@@ -15,7 +15,7 @@ import gr.uom.java.xmi.UMLOperation;
 public class RemoveMethodAnnotationRefactoring implements Refactoring {
 	private UMLAnnotation annotation;
 	private UMLOperation operationBefore;
-	private UMLOperation operationAfter;
+	public UMLOperation operationAfter;
 	
 	public RemoveMethodAnnotationRefactoring(UMLAnnotation annotation, UMLOperation operationBefore,
 			UMLOperation operationAfter) {
@@ -32,6 +32,7 @@ public class RemoveMethodAnnotationRefactoring implements Refactoring {
 		return operationBefore;
 	}
 
+	@Override
 	public UMLOperation getOperationAfter() {
 		return operationAfter;
 	}
