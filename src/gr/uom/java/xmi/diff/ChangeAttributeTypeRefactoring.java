5c389d51efc0fd01040a18f73cff9f6568c5b1fd
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:43:13 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeAttributeTypeRefactoring,leftSide#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/ChangeAttributeTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeAttributeTypeRefactoring.java
@@ -13,7 +13,7 @@ import gr.uom.java.xmi.decomposition.AbstractCodeMapping;
 import gr.uom.java.xmi.decomposition.VariableDeclaration;
 
 public class ChangeAttributeTypeRefactoring implements Refactoring {
-	private VariableDeclaration originalAttribute;
+	public VariableDeclaration originalAttribute;
 	private VariableDeclaration changedTypeAttribute;
 	private String classNameBefore;
 	private String classNameAfter;
