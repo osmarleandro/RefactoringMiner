c22651168594f8e256f684515379b83979d18e83
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:36:03 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,PullUpOperationRefactoring,getRefactoringType#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MoveOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveOperationRefactoring.java
@@ -126,4 +126,8 @@ public class MoveOperationRefactoring implements Refactoring {
 				.setCodeElement(movedOperation.toString()));
 		return ranges;
 	}
+
+	public RefactoringType getRefactoringType() {
+		return RefactoringType.PULL_UP_OPERATION;
+	}
 }
