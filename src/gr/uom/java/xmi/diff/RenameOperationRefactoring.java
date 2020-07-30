13f490bacc63c1d0349458e19a840f339c15e1c1
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:50:29 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenameOperationRefactoring,rightSide#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/RenameOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenameOperationRefactoring.java
@@ -15,7 +15,7 @@ import gr.uom.java.xmi.decomposition.replacement.Replacement;
 
 public class RenameOperationRefactoring implements Refactoring {
 	private UMLOperation originalOperation;
-	private UMLOperation renamedOperation;
+	public UMLOperation renamedOperation;
 	private Set<Replacement> replacements;
 	private UMLOperationBodyMapper bodyMapper;
 	
