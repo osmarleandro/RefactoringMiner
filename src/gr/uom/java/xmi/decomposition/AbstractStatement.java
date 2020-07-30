21c0557df745caf2b97c18ff22005c846873fc34
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:04:39 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,CompositeStatementObject,getMethodInvocationMap#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractStatement.java
+++ src/gr/uom/java/xmi/decomposition/AbstractStatement.java
@@ -1,6 +1,9 @@
 package gr.uom.java.xmi.decomposition;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractStatement extends AbstractCodeFragment {
 	private CompositeStatementObject parent;
@@ -30,4 +33,23 @@ public abstract class AbstractStatement extends AbstractCodeFragment {
 
     public abstract List<StatementObject> getLeaves();
     public abstract int statementCount();
+
+	@Override
+	public Map<String, List<OperationInvocation>> getMethodInvocationMap() {
+		Map<String, List<OperationInvocation>> map = new LinkedHashMap<String, List<OperationInvocation>>();
+		for(AbstractExpression expression : expressionList) {
+			Map<String, List<OperationInvocation>> expressionMap = expression.getMethodInvocationMap();
+			for(String key : expressionMap.keySet()) {
+				if(map.containsKey(key)) {
+					map.get(key).addAll(expressionMap.get(key));
+				}
+				else {
+					List<OperationInvocation> list = new ArrayList<OperationInvocation>();
+					list.addAll(expressionMap.get(key));
+					map.put(key, list);
+				}
+			}
+		}
+		return map;
+	}
 }
