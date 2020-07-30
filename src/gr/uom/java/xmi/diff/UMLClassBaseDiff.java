26324978d70c81a131b784bc749e31ec40e7eb4c
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 12:12:23 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,operationsBeforeAndAfterMatch#QUMLOperation;QUMLOperation,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
@@ -1486,50 +1486,6 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 		return false;
 	}
 
-	private boolean operationsBeforeAndAfterMatch(UMLOperation removedOperation, UMLOperation addedOperation) {
-		UMLOperation operationBefore1 = null;
-		UMLOperation operationAfter1 = null;
-		List<UMLOperation> originalClassOperations = originalClass.getOperations();
-		for(int i=0; i<originalClassOperations.size(); i++) {
-			UMLOperation current = originalClassOperations.get(i);
-			if(current.equals(removedOperation)) {
-				if(i>0) {
-					operationBefore1 = originalClassOperations.get(i-1);
-				}
-				if(i<originalClassOperations.size()-1) {
-					operationAfter1 = originalClassOperations.get(i+1);
-				}
-			}
-		}
-		
-		UMLOperation operationBefore2 = null;
-		UMLOperation operationAfter2 = null;
-		List<UMLOperation> nextClassOperations = nextClass.getOperations();
-		for(int i=0; i<nextClassOperations.size(); i++) {
-			UMLOperation current = nextClassOperations.get(i);
-			if(current.equals(addedOperation)) {
-				if(i>0) {
-					operationBefore2 = nextClassOperations.get(i-1);
-				}
-				if(i<nextClassOperations.size()-1) {
-					operationAfter2 = nextClassOperations.get(i+1);
-				}
-			}
-		}
-		
-		boolean operationsBeforeMatch = false;
-		if(operationBefore1 != null && operationBefore2 != null) {
-			operationsBeforeMatch = operationBefore1.equalParameterTypes(operationBefore2) && operationBefore1.getName().equals(operationBefore2.getName());
-		}
-		
-		boolean operationsAfterMatch = false;
-		if(operationAfter1 != null && operationAfter2 != null) {
-			operationsAfterMatch = operationAfter1.equalParameterTypes(operationAfter2) && operationAfter1.getName().equals(operationAfter2.getName());
-		}
-		
-		return operationsBeforeMatch || operationsAfterMatch;
-	}
-
 	private void checkForInlinedOperations() throws RefactoringMinerTimedOutException {
 		List<UMLOperation> operationsToBeRemoved = new ArrayList<UMLOperation>();
 		for(Iterator<UMLOperation> removedOperationIterator = removedOperations.iterator(); removedOperationIterator.hasNext();) {
--- src/gr/uom/java/xmi/diff/UMLClassMoveDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassMoveDiff.java
@@ -1,6 +1,9 @@
 package gr.uom.java.xmi.diff;
 
+import java.util.List;
+
 import gr.uom.java.xmi.UMLClass;
+import gr.uom.java.xmi.UMLOperation;
 
 public class UMLClassMoveDiff extends UMLClassBaseDiff {
 	
@@ -32,5 +35,49 @@ public class UMLClassMoveDiff extends UMLClassBaseDiff {
 			return this.originalClass.equals(classMoveDiff.originalClass) && this.nextClass.equals(classMoveDiff.nextClass);
 		}
 		return false;
+	}
+
+	private boolean operationsBeforeAndAfterMatch(UMLOperation removedOperation, UMLOperation addedOperation) {
+		UMLOperation operationBefore1 = null;
+		UMLOperation operationAfter1 = null;
+		List<UMLOperation> originalClassOperations = originalClass.getOperations();
+		for(int i=0; i<originalClassOperations.size(); i++) {
+			UMLOperation current = originalClassOperations.get(i);
+			if(current.equals(removedOperation)) {
+				if(i>0) {
+					operationBefore1 = originalClassOperations.get(i-1);
+				}
+				if(i<originalClassOperations.size()-1) {
+					operationAfter1 = originalClassOperations.get(i+1);
+				}
+			}
+		}
+		
+		UMLOperation operationBefore2 = null;
+		UMLOperation operationAfter2 = null;
+		List<UMLOperation> nextClassOperations = nextClass.getOperations();
+		for(int i=0; i<nextClassOperations.size(); i++) {
+			UMLOperation current = nextClassOperations.get(i);
+			if(current.equals(addedOperation)) {
+				if(i>0) {
+					operationBefore2 = nextClassOperations.get(i-1);
+				}
+				if(i<nextClassOperations.size()-1) {
+					operationAfter2 = nextClassOperations.get(i+1);
+				}
+			}
+		}
+		
+		boolean operationsBeforeMatch = false;
+		if(operationBefore1 != null && operationBefore2 != null) {
+			operationsBeforeMatch = operationBefore1.equalParameterTypes(operationBefore2) && operationBefore1.getName().equals(operationBefore2.getName());
+		}
+		
+		boolean operationsAfterMatch = false;
+		if(operationAfter1 != null && operationAfter2 != null) {
+			operationsAfterMatch = operationAfter1.equalParameterTypes(operationAfter2) && operationAfter1.getName().equals(operationAfter2.getName());
+		}
+		
+		return operationsBeforeMatch || operationsAfterMatch;
 	}
 }
