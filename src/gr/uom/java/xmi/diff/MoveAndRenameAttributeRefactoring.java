3cb7cfea9bfdf1feee57844c49b6e2f814f01b47
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:22:05 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveAndRenameAttributeRefactoring,getRefactoringType#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MoveAndRenameAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveAndRenameAttributeRefactoring.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.refactoringminer.api.RefactoringType;
-
 import gr.uom.java.xmi.UMLAttribute;
 
 public class MoveAndRenameAttributeRefactoring extends MoveAttributeRefactoring {
@@ -36,10 +34,6 @@ public class MoveAndRenameAttributeRefactoring extends MoveAttributeRefactoring 
 
 	public String getName() {
 		return this.getRefactoringType().getDisplayName();
-	}
-
-	public RefactoringType getRefactoringType() {
-		return RefactoringType.MOVE_RENAME_ATTRIBUTE;
 	}
 
 	@Override
