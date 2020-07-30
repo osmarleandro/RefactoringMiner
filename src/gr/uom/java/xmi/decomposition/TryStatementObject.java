0a38da3f1958c8c607ef6de2b7776852c0719798
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 11:58:04 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,CompositeStatementObject,aliasedAttributes#,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/TryStatementObject.java
+++ src/gr/uom/java/xmi/decomposition/TryStatementObject.java
@@ -1,7 +1,11 @@
 package gr.uom.java.xmi.decomposition;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Statement;
@@ -41,5 +45,37 @@ public class TryStatementObject extends CompositeStatementObject {
 			variableDeclarations.addAll(catchClause.getVariableDeclarations());
 		}
 		return variableDeclarations;
+	}
+
+	public Map<String, Set<String>> aliasedAttributes() {
+		Map<String, Set<String>> map = new LinkedHashMap<String, Set<String>>();
+		for(StatementObject statement : getLeaves()) {
+			String s = statement.getString();
+			if(s.startsWith("this.") && s.endsWith(";\n")) {
+				String firstLine = s.substring(0, s.indexOf("\n"));
+				if(firstLine.contains("=")) {
+					String attribute = s.substring(5, s.indexOf("="));
+					String value = s.substring(s.indexOf("=")+1, s.indexOf(";\n"));
+					if(map.containsKey(value)) {
+						map.get(value).add(attribute);
+					}
+					else {
+						Set<String> set = new LinkedHashSet<String>();
+						set.add(attribute);
+						map.put(value, set);
+					}
+				}
+			}
+		}
+		Set<String> keysToBeRemoved = new LinkedHashSet<String>();
+		for(String key : map.keySet()) {
+			if(map.get(key).size() <= 1) {
+				keysToBeRemoved.add(key);
+			}
+		}
+		for(String key : keysToBeRemoved) {
+			map.remove(key);
+		}
+		return map;
 	}
 }
