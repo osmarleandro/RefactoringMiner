244e9a15343d44579386e3f155443e8af04a337c
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 11:49:46 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLAbstractClass,hasCommonAttributesAndOperations#QUMLAbstractClass,PushDownMethod,OK

--- src/gr/uom/java/xmi/UMLAnonymousClass.java
+++ src/gr/uom/java/xmi/UMLAnonymousClass.java
@@ -1,6 +1,12 @@
 package gr.uom.java.xmi;
 
 import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.refactoringminer.util.PrefixSuffixUtils;
+
+import gr.uom.java.xmi.diff.RenamePattern;
 
 public class UMLAnonymousClass extends UMLAbstractClass implements Comparable<UMLAnonymousClass>, Serializable, LocationInfoProvider {
 	private String codePath;
@@ -58,5 +64,67 @@ public class UMLAnonymousClass extends UMLAbstractClass implements Comparable<UM
 
 	public boolean isInterface() {
 		return false;
+	}
+
+	public boolean hasCommonAttributesAndOperations(UMLAbstractClass umlClass) {
+		String commonPrefix = PrefixSuffixUtils.longestCommonPrefix(this.name, umlClass.name);
+		String commonSuffix = PrefixSuffixUtils.longestCommonSuffix(this.name, umlClass.name);
+		RenamePattern pattern = null;
+		if(!commonPrefix.isEmpty() && !commonSuffix.isEmpty()) {
+			int beginIndexS1 = this.name.indexOf(commonPrefix) + commonPrefix.length();
+			int endIndexS1 = this.name.lastIndexOf(commonSuffix);
+			String diff1 = beginIndexS1 > endIndexS1 ? "" :	this.name.substring(beginIndexS1, endIndexS1);
+			int beginIndexS2 = umlClass.name.indexOf(commonPrefix) + commonPrefix.length();
+			int endIndexS2 = umlClass.name.lastIndexOf(commonSuffix);
+			String diff2 = beginIndexS2 > endIndexS2 ? "" :	umlClass.name.substring(beginIndexS2, endIndexS2);
+			pattern = new RenamePattern(diff1, diff2);
+		}
+		Set<UMLOperation> commonOperations = new LinkedHashSet<UMLOperation>();
+		int totalOperations = 0;
+		for(UMLOperation operation : operations) {
+			if(!operation.isConstructor() && !operation.overridesObject()) {
+				totalOperations++;
+	    		if(umlClass.containsOperationWithTheSameSignatureIgnoringChangedTypes(operation) ||
+	    				(pattern != null && umlClass.containsOperationWithTheSameRenamePattern(operation, pattern.reverse()))) {
+	    			commonOperations.add(operation);
+	    		}
+			}
+		}
+		for(UMLOperation operation : umlClass.operations) {
+			if(!operation.isConstructor() && !operation.overridesObject()) {
+				totalOperations++;
+	    		if(this.containsOperationWithTheSameSignatureIgnoringChangedTypes(operation) ||
+	    				(pattern != null && this.containsOperationWithTheSameRenamePattern(operation, pattern))) {
+	    			commonOperations.add(operation);
+	    		}
+			}
+		}
+		Set<UMLAttribute> commonAttributes = new LinkedHashSet<UMLAttribute>();
+		int totalAttributes = 0;
+		for(UMLAttribute attribute : attributes) {
+			totalAttributes++;
+			if(umlClass.containsAttributeWithTheSameNameIgnoringChangedType(attribute) ||
+					(pattern != null && umlClass.containsAttributeWithTheSameRenamePattern(attribute, pattern.reverse()))) {
+				commonAttributes.add(attribute);
+			}
+		}
+		for(UMLAttribute attribute : umlClass.attributes) {
+			totalAttributes++;
+			if(this.containsAttributeWithTheSameNameIgnoringChangedType(attribute) ||
+					(pattern != null && this.containsAttributeWithTheSameRenamePattern(attribute, pattern))) {
+				commonAttributes.add(attribute);
+			}
+		}
+		if(this.isTestClass() && umlClass.isTestClass()) {
+			return commonOperations.size() > Math.floor(totalOperations/2.0) || commonOperations.containsAll(this.operations);
+		}
+		if(this.isSingleAbstractMethodInterface() && umlClass.isSingleAbstractMethodInterface()) {
+			return commonOperations.size() == totalOperations;
+		}
+		return (commonOperations.size() > Math.floor(totalOperations/2.0) && (commonAttributes.size() > 2 || totalAttributes == 0)) ||
+				(commonOperations.size() > Math.floor(totalOperations/3.0*2.0) && (commonAttributes.size() >= 2 || totalAttributes == 0)) ||
+				(commonAttributes.size() > Math.floor(totalAttributes/2.0) && (commonOperations.size() > 2 || totalOperations == 0)) ||
+				(commonOperations.size() == totalOperations && commonOperations.size() > 2 && this.attributes.size() == umlClass.attributes.size()) ||
+				(commonOperations.size() == totalOperations && commonOperations.size() > 2 && totalAttributes == 1);
 	}
 }
