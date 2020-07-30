875a4f2d804e78e33271c58059571b954f18330f
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:45:12 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,PushDownAttributeRefactoring,rightSide#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/PushDownAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/PushDownAttributeRefactoring.java
@@ -1,8 +1,5 @@
 package gr.uom.java.xmi.diff;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.refactoringminer.api.RefactoringType;
 
 import gr.uom.java.xmi.UMLAttribute;
@@ -28,14 +25,5 @@ public class PushDownAttributeRefactoring extends MoveAttributeRefactoring {
 
 	public RefactoringType getRefactoringType() {
 		return RefactoringType.PUSH_DOWN_ATTRIBUTE;
-	}
-
-	@Override
-	public List<CodeRange> rightSide() {
-		List<CodeRange> ranges = new ArrayList<CodeRange>();
-		ranges.add(movedAttribute.codeRange()
-				.setDescription("pushed down attribute declaration")
-				.setCodeElement(movedAttribute.toString()));
-		return ranges;
 	}
 }
