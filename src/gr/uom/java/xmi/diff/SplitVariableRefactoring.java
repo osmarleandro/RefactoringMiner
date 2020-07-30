de51873f18418eaba4faa5b1620d99ae6bc07086
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 16:05:39 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,SplitVariableRefactoring,rightSide#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/SplitVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/SplitVariableRefactoring.java
@@ -14,7 +14,7 @@ import gr.uom.java.xmi.decomposition.AbstractCodeMapping;
 import gr.uom.java.xmi.decomposition.VariableDeclaration;
 
 public class SplitVariableRefactoring implements Refactoring {
-	private Set<VariableDeclaration> splitVariables;
+	public Set<VariableDeclaration> splitVariables;
 	private VariableDeclaration oldVariable;
 	private UMLOperation operationBefore;
 	private UMLOperation operationAfter;
