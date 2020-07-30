dba7574ae242073ec144b6e6f747c4a937ddceab
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:04:25 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,CompositeStatementObject,toString#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractStatement.java
+++ src/gr/uom/java/xmi/decomposition/AbstractStatement.java
@@ -30,4 +30,18 @@ public abstract class AbstractStatement extends AbstractCodeFragment {
 
     public abstract List<StatementObject> getLeaves();
     public abstract int statementCount();
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(locationInfo.getCodeElementType().getName());
+		if(expressionList.size() > 0) {
+			sb.append("(");
+			for(int i=0; i<expressionList.size()-1; i++) {
+				sb.append(expressionList.get(i).toString()).append("; ");
+			}
+			sb.append(expressionList.get(expressionList.size()-1).toString());
+			sb.append(")");
+		}
+		return sb.toString();
+	}
 }
