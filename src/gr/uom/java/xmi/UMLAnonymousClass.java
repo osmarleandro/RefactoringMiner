ded27ce835d4dd2d36eea9b1274c3dd3ffba6b22
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 14:51:17 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLClass,addAnonymousClass#QUMLAnonymousClass,PullUpMethod,OK

--- src/gr/uom/java/xmi/UMLAnonymousClass.java
+++ src/gr/uom/java/xmi/UMLAnonymousClass.java
@@ -59,4 +59,10 @@ public class UMLAnonymousClass extends UMLAbstractClass implements Comparable<UM
 	public boolean isInterface() {
 		return false;
 	}
+
+	@Override
+	public void addAnonymousClass(UMLAnonymousClass anonymousClass) {
+		// TODO Auto-generated method stub
+		
+	}
 }
