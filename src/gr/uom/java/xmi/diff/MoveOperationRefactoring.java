c7c410c918d7e4813cd86af5c3f2650413d642f3
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 16:05:46 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,PushDownOperationRefactoring,rightSide#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MoveOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveOperationRefactoring.java
@@ -126,4 +126,13 @@ public class MoveOperationRefactoring implements Refactoring {
 				.setCodeElement(movedOperation.toString()));
 		return ranges;
 	}
+
+	@Override
+	public List<CodeRange> rightSide() {
+		List<CodeRange> ranges = new ArrayList<CodeRange>();
+		ranges.add(movedOperation.codeRange()
+				.setDescription("pushed down method declaration")
+				.setCodeElement(movedOperation.toString()));
+		return ranges;
+	}
 }
