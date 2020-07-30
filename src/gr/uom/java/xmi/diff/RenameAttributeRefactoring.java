54a8545e007abe26fa8506477f19bfd97312bf75
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:44:47 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenameAttributeRefactoring,rightSide#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/RenameAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenameAttributeRefactoring.java
@@ -13,7 +13,7 @@ import gr.uom.java.xmi.decomposition.VariableDeclaration;
 
 public class RenameAttributeRefactoring implements Refactoring {
 	private VariableDeclaration originalAttribute;
-	private VariableDeclaration renamedAttribute;
+	public VariableDeclaration renamedAttribute;
 	private Set<CandidateAttributeRefactoring> attributeRenames;
 	private String classNameBefore;
 	private String classNameAfter;
