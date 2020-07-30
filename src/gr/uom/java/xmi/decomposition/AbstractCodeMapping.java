0f000b188696da22c437bfb76eaad1f25757dcbd
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:12:51 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,LeafMapping,compareTo#QLeafMapping,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractCodeMapping.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeMapping.java
@@ -16,6 +16,7 @@ import gr.uom.java.xmi.decomposition.replacement.VariableReplacementWithMethodIn
 import gr.uom.java.xmi.diff.ExtractVariableRefactoring;
 import gr.uom.java.xmi.diff.InlineVariableRefactoring;
 import gr.uom.java.xmi.diff.RenameOperationRefactoring;
+import gr.uom.java.xmi.diff.StringDistance;
 import gr.uom.java.xmi.diff.UMLClassBaseDiff;
 
 public abstract class AbstractCodeMapping {
@@ -470,5 +471,66 @@ public abstract class AbstractCodeMapping {
 		} else if (!operation2.equals(other.operation2))
 			return false;
 		return true;
+	}
+
+	@Override
+	public int compareTo(LeafMapping o) {
+		double distance1;
+		double distance2;
+		if(this.getFragment1().getString().equals(this.getFragment2().getString())) {
+			distance1 = 0;
+		}
+		else {
+			String s1 = this.getFragment1().getString().toLowerCase();
+			String s2 = this.getFragment2().getString().toLowerCase();
+			int distance = StringDistance.editDistance(s1, s2);
+			distance1 = (double)distance/(double)Math.max(s1.length(), s2.length());
+		}
+		
+		if(o.getFragment1().getString().equals(o.getFragment2().getString())) {
+			distance2 = 0;
+		}
+		else {
+			String s1 = o.getFragment1().getString().toLowerCase();
+			String s2 = o.getFragment2().getString().toLowerCase();
+			int distance = StringDistance.editDistance(s1, s2);
+			distance2 = (double)distance/(double)Math.max(s1.length(), s2.length());
+		}
+		
+		if(distance1 != distance2) {
+			if(this.isIdenticalWithExtractedVariable() && !o.isIdenticalWithExtractedVariable()) {
+				return -1;
+			}
+			else if(!this.isIdenticalWithExtractedVariable() && o.isIdenticalWithExtractedVariable()) {
+				return 1;
+			}
+			if(this.isIdenticalWithInlinedVariable() && !o.isIdenticalWithInlinedVariable()) {
+				return -1;
+			}
+			else if(!this.isIdenticalWithInlinedVariable() && o.isIdenticalWithInlinedVariable()) {
+				return 1;
+			}
+			return Double.compare(distance1, distance2);
+		}
+		else {
+			int depthDiff1 = Math.abs(this.getFragment1().getDepth() - this.getFragment2().getDepth());
+			int depthDiff2 = Math.abs(o.getFragment1().getDepth() - o.getFragment2().getDepth());
+	
+			if(depthDiff1 != depthDiff2) {
+				return Integer.valueOf(depthDiff1).compareTo(Integer.valueOf(depthDiff2));
+			}
+			else {
+				int indexDiff1 = Math.abs(this.getFragment1().getIndex() - this.getFragment2().getIndex());
+				int indexDiff2 = Math.abs(o.getFragment1().getIndex() - o.getFragment2().getIndex());
+				if(indexDiff1 != indexDiff2) {
+					return Integer.valueOf(indexDiff1).compareTo(Integer.valueOf(indexDiff2));
+				}
+				else {
+					double parentEditDistance1 = this.parentEditDistance();
+					double parentEditDistance2 = o.parentEditDistance();
+					return Double.compare(parentEditDistance1, parentEditDistance2);
+				}
+			}
+		}
 	}
 }
