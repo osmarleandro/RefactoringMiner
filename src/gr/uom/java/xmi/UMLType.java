ce66ba1769efabe57ac42629f5711868a85c0c68
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 14:47:31 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLType,getArrayDimension#,PullUpMethod,OK

--- src/gr/uom/java/xmi/UMLType.java
+++ src/gr/uom/java/xmi/UMLType.java
@@ -23,7 +23,7 @@ import gr.uom.java.xmi.diff.StringDistance;
 
 public abstract class UMLType implements Serializable, LocationInfoProvider {
 	private LocationInfo locationInfo;
-	private int arrayDimension;
+	int arrayDimension;
 	private List<UMLType> typeArguments = new ArrayList<UMLType>();
 	protected List<UMLAnnotation> annotations = new ArrayList<UMLAnnotation>();
 
@@ -31,6 +31,7 @@ public abstract class UMLType implements Serializable, LocationInfoProvider {
 		return locationInfo;
 	}
 
+	@Override
 	public int getArrayDimension() {
 		return this.arrayDimension;
 	}
--- src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
+++ src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
@@ -256,4 +256,10 @@ public class VariableDeclaration implements LocationInfoProvider, VariableDeclar
 	public VariableDeclaration getVariableDeclaration() {
 		return this;
 	}
+
+	@Override
+	public int getArrayDimension() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
