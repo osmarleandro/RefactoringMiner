869024da66542b174aedcefd83da85868744ce3e
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:09:09 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,getInfixOperators#,PullUpMethod,OK

--- src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
+++ src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
@@ -256,4 +256,10 @@ public class VariableDeclaration implements LocationInfoProvider, VariableDeclar
 	public VariableDeclaration getVariableDeclaration() {
 		return this;
 	}
+
+	@Override
+	public List<String> getInfixOperators() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
