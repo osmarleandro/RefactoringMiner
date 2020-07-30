b1cfca8a1114ca238821fc6e050b7963da6311d5
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:53:05 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ModifyMethodAnnotationRefactoring,equals#QObject,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/MoveSourceFolderRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveSourceFolderRefactoring.java
@@ -92,4 +92,10 @@ public class MoveSourceFolderRefactoring implements Refactoring {
 		}
 		return ranges;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
