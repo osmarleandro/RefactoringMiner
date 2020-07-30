c4828de5de9533e9774678f65738a05d688adc4c
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:06:18 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,VariableDeclaration,getVariableName#,PullUpMethod,OK

--- src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
+++ src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
@@ -23,7 +23,7 @@ import gr.uom.java.xmi.VariableDeclarationProvider;
 import gr.uom.java.xmi.diff.CodeRange;
 
 public class VariableDeclaration implements LocationInfoProvider, VariableDeclarationProvider {
-	private String variableName;
+	public String variableName;
 	private AbstractExpression initializer;
 	private UMLType type;
 	private boolean varargsParameter;
@@ -99,6 +99,7 @@ public class VariableDeclaration implements LocationInfoProvider, VariableDeclar
 		this.varargsParameter = varargs;
 	}
 
+	@Override
 	public String getVariableName() {
 		return variableName;
 	}
