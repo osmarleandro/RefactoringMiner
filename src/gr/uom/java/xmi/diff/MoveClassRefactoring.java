04de59e3f8e6b27e63aa750bd1d29718e37b8480
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:18:18 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveClassRefactoring,getOriginalClass#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/MoveClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveClassRefactoring.java
@@ -13,7 +13,7 @@ import org.refactoringminer.api.RefactoringType;
 import org.refactoringminer.util.PrefixSuffixUtils;
 
 public class MoveClassRefactoring implements Refactoring {
-	private UMLClass originalClass;
+	public UMLClass originalClass;
 	private UMLClass movedClass;
 	
 	public MoveClassRefactoring(UMLClass originalClass,  UMLClass movedClass) {
@@ -56,6 +56,7 @@ public class MoveClassRefactoring implements Refactoring {
 		return movedClass.getName();
 	}
 
+	@Override
 	public UMLClass getOriginalClass() {
 		return originalClass;
 	}
