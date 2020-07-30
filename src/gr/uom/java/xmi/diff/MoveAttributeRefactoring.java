81df31892a0875b7f25886a9edd1b2e09b912f6a
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:40:15 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,PullUpAttributeRefactoring,rightSide#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/MoveAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveAttributeRefactoring.java
@@ -128,4 +128,13 @@ public class MoveAttributeRefactoring implements Refactoring {
 				.setCodeElement(movedAttribute.toString()));
 		return ranges;
 	}
+
+	@Override
+	public List<CodeRange> rightSide() {
+		List<CodeRange> ranges = new ArrayList<CodeRange>();
+		ranges.add(movedAttribute.codeRange()
+				.setDescription("pulled up attribute declaration")
+				.setCodeElement(movedAttribute.toString()));
+		return ranges;
+	}
 }
--- src/gr/uom/java/xmi/diff/PullUpAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/PullUpAttributeRefactoring.java
@@ -1,8 +1,5 @@
 package gr.uom.java.xmi.diff;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.refactoringminer.api.RefactoringType;
 
 import gr.uom.java.xmi.UMLAttribute;
@@ -28,14 +25,5 @@ public class PullUpAttributeRefactoring extends MoveAttributeRefactoring {
 
 	public RefactoringType getRefactoringType() {
 		return RefactoringType.PULL_UP_ATTRIBUTE;
-	}
-
-	@Override
-	public List<CodeRange> rightSide() {
-		List<CodeRange> ranges = new ArrayList<CodeRange>();
-		ranges.add(movedAttribute.codeRange()
-				.setDescription("pulled up attribute declaration")
-				.setCodeElement(movedAttribute.toString()));
-		return ranges;
 	}
 }
