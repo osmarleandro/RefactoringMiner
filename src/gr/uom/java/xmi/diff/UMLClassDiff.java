4a967e3990b6c07ba01c79ba589d26fa754e5ed7
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 12:07:34 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,nextClassAttributesOfType#QString,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLClassDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassDiff.java
@@ -195,4 +195,8 @@ public class UMLClassDiff extends UMLClassBaseDiff {
 	public boolean matches(UMLType type) {
 		return this.className.endsWith("." + type.getClassType());
 	}
+
+	public List<UMLAttribute> nextClassAttributesOfType(String targetClass) {
+		return nextClass.attributesOfType(targetClass);
+	}
 }
