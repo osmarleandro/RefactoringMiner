8906eb70252b8ccc68eb8dd17685dc6bbeff524a
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:49:53 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenameOperationRefactoring,getOriginalOperation#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
@@ -144,4 +144,10 @@ public class ModifyMethodAnnotationRefactoring implements Refactoring {
 			return false;
 		return true;
 	}
+
+	@Override
+	public UMLOperation getOriginalOperation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
