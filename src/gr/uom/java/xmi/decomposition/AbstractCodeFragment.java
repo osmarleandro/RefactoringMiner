12dcbd438dabfe7779a123a767c79a86852938e9
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 11:58:47 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,getString#,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -36,7 +36,6 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	}
 	
 	public abstract CompositeStatementObject getParent();
-	public abstract String getString();
 	public abstract List<String> getVariables();
 	public abstract List<String> getTypes();
 	public abstract List<VariableDeclaration> getVariableDeclarations();
