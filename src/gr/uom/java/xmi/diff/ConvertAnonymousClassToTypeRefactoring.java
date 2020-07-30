cf9338c75807306f493ecf51bfdac85070045c4b
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:23:08 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ConvertAnonymousClassToTypeRefactoring,getInvolvedClassesAfterRefactoring#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/ConvertAnonymousClassToTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ConvertAnonymousClassToTypeRefactoring.java
@@ -52,6 +52,7 @@ public class ConvertAnonymousClassToTypeRefactoring implements Refactoring {
 		return pairs;
 	}
 
+	@Override
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getAddedClass().getLocationInfo().getFilePath(), getAddedClass().getName()));
