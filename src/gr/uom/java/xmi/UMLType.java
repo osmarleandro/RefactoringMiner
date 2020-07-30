a1ceae2d7c6fc79278f53c7f885dd15cd23f8161
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 14:46:55 -0300

    RefactoringMiner,gr.uom.java.xmi,LeafType,equalsWithSubType#QUMLType,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/UMLType.java
+++ src/gr/uom/java/xmi/UMLType.java
@@ -169,6 +169,16 @@ public abstract class UMLType implements Serializable, LocationInfoProvider {
 		return normalized;
 	}
 
+	@Override
+	public boolean equalsWithSubType(UMLType type) {
+		if(this.getClass() == type.getClass()) {
+			if(firstOrLastCamelCaseTokenMatch(this.nonQualifiedClassType, ((LeafType)type).nonQualifiedClassType) && equalTypeArgumentsAndArrayDimensionForSubType(type)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public static LeafType extractTypeObject(String qualifiedName) {
 		int arrayDimension = 0;
 		List<UMLType> typeArgumentDecomposition = new ArrayList<UMLType>();
