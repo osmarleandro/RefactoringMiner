5b15a405e5ac74c047b9b1abd056677fe47b5a53
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 12:11:25 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,exactMappings#QUMLOperationBodyMapper,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
@@ -20,7 +20,6 @@ import gr.uom.java.xmi.UMLClass;
 import gr.uom.java.xmi.UMLOperation;
 import gr.uom.java.xmi.UMLType;
 import gr.uom.java.xmi.decomposition.AbstractCodeMapping;
-import gr.uom.java.xmi.decomposition.CompositeStatementObject;
 import gr.uom.java.xmi.decomposition.OperationInvocation;
 import gr.uom.java.xmi.decomposition.StatementObject;
 import gr.uom.java.xmi.decomposition.UMLOperationBodyMapper;
@@ -1151,100 +1150,6 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 				mapperSet.add(operationBodyMapper);
 			}
 		}
-	}
-
-	private boolean exactMappings(UMLOperationBodyMapper operationBodyMapper) {
-		if(allMappingsAreExactMatches(operationBodyMapper)) {
-			if(operationBodyMapper.nonMappedElementsT1() == 0 && operationBodyMapper.nonMappedElementsT2() == 0)
-				return true;
-			else if(operationBodyMapper.nonMappedElementsT1() > 0 && operationBodyMapper.getNonMappedInnerNodesT1().size() == 0 && operationBodyMapper.nonMappedElementsT2() == 0) {
-				int countableStatements = 0;
-				int parameterizedVariableDeclarationStatements = 0;
-				UMLOperation addedOperation = operationBodyMapper.getOperation2();
-				List<String> nonMappedLeavesT1 = new ArrayList<String>();
-				for(StatementObject statement : operationBodyMapper.getNonMappedLeavesT1()) {
-					if(statement.countableStatement()) {
-						nonMappedLeavesT1.add(statement.getString());
-						for(String parameterName : addedOperation.getParameterNameList()) {
-							if(statement.getVariableDeclaration(parameterName) != null) {
-								parameterizedVariableDeclarationStatements++;
-								break;
-							}
-						}
-						countableStatements++;
-					}
-				}
-				int nonMappedLeavesExactlyMatchedInTheBodyOfAddedOperation = 0;
-				for(UMLOperation operation : addedOperations) {
-					if(!operation.equals(addedOperation) && operation.getBody() != null) {
-						for(StatementObject statement : operation.getBody().getCompositeStatement().getLeaves()) {
-							if(nonMappedLeavesT1.contains(statement.getString())) {
-								nonMappedLeavesExactlyMatchedInTheBodyOfAddedOperation++;
-							}
-						}
-					}
-				}
-				return (countableStatements == parameterizedVariableDeclarationStatements || countableStatements == nonMappedLeavesExactlyMatchedInTheBodyOfAddedOperation + parameterizedVariableDeclarationStatements) && countableStatements > 0;
-			}
-			else if(operationBodyMapper.nonMappedElementsT1() == 0 && operationBodyMapper.nonMappedElementsT2() > 0 && operationBodyMapper.getNonMappedInnerNodesT2().size() == 0) {
-				int countableStatements = 0;
-				int parameterizedVariableDeclarationStatements = 0;
-				UMLOperation removedOperation = operationBodyMapper.getOperation1();
-				for(StatementObject statement : operationBodyMapper.getNonMappedLeavesT2()) {
-					if(statement.countableStatement()) {
-						for(String parameterName : removedOperation.getParameterNameList()) {
-							if(statement.getVariableDeclaration(parameterName) != null) {
-								parameterizedVariableDeclarationStatements++;
-								break;
-							}
-						}
-						countableStatements++;
-					}
-				}
-				return countableStatements == parameterizedVariableDeclarationStatements && countableStatements > 0;
-			}
-			else if((operationBodyMapper.nonMappedElementsT1() == 1 || operationBodyMapper.nonMappedElementsT2() == 1) &&
-					operationBodyMapper.getNonMappedInnerNodesT1().size() == 0 && operationBodyMapper.getNonMappedInnerNodesT2().size() == 0) {
-				StatementObject statementUsingParameterAsInvoker1 = null;
-				UMLOperation removedOperation = operationBodyMapper.getOperation1();
-				for(StatementObject statement : operationBodyMapper.getNonMappedLeavesT1()) {
-					if(statement.countableStatement()) {
-						for(String parameterName : removedOperation.getParameterNameList()) {
-							OperationInvocation invocation = statement.invocationCoveringEntireFragment();
-							if(invocation != null && invocation.getExpression() != null && invocation.getExpression().equals(parameterName)) {
-								statementUsingParameterAsInvoker1 = statement;
-								break;
-							}
-						}
-					}
-				}
-				StatementObject statementUsingParameterAsInvoker2 = null;
-				UMLOperation addedOperation = operationBodyMapper.getOperation2();
-				for(StatementObject statement : operationBodyMapper.getNonMappedLeavesT2()) {
-					if(statement.countableStatement()) {
-						for(String parameterName : addedOperation.getParameterNameList()) {
-							OperationInvocation invocation = statement.invocationCoveringEntireFragment();
-							if(invocation != null && invocation.getExpression() != null && invocation.getExpression().equals(parameterName)) {
-								statementUsingParameterAsInvoker2 = statement;
-								break;
-							}
-						}
-					}
-				}
-				if(statementUsingParameterAsInvoker1 != null && statementUsingParameterAsInvoker2 != null) {
-					for(AbstractCodeMapping mapping : operationBodyMapper.getMappings()) {
-						if(mapping.getFragment1() instanceof CompositeStatementObject && mapping.getFragment2() instanceof CompositeStatementObject) {
-							CompositeStatementObject parent1 = (CompositeStatementObject)mapping.getFragment1();
-							CompositeStatementObject parent2 = (CompositeStatementObject)mapping.getFragment2();
-							if(parent1.getLeaves().contains(statementUsingParameterAsInvoker1) && parent2.getLeaves().contains(statementUsingParameterAsInvoker2)) {
-								return true;
-							}
-						}
-					}
-				}
-			}
-		}
-		return false;
 	}
 
 	private boolean mappedElementsMoreThanNonMappedT1AndT2(int mappings, UMLOperationBodyMapper operationBodyMapper) {
