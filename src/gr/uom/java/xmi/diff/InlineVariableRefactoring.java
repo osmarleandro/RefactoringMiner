33f4d5fd2f204f59211de9573527fb22e7947683
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:34:13 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,InlineVariableRefactoring,leftSide#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/InlineVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/InlineVariableRefactoring.java
@@ -14,10 +14,10 @@ import gr.uom.java.xmi.decomposition.AbstractCodeMapping;
 import gr.uom.java.xmi.decomposition.VariableDeclaration;
 
 public class InlineVariableRefactoring implements Refactoring {
-	private VariableDeclaration variableDeclaration;
+	public VariableDeclaration variableDeclaration;
 	private UMLOperation operationBefore;
 	private UMLOperation operationAfter;
-	private Set<AbstractCodeMapping> references;
+	public Set<AbstractCodeMapping> references;
 
 	public InlineVariableRefactoring(VariableDeclaration variableDeclaration, UMLOperation operationBefore, UMLOperation operationAfter) {
 		this.variableDeclaration = variableDeclaration;
