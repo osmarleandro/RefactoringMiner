f409a64e1717ff24cec2349a96a2ff0d9b979c03
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 11:48:53 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLAbstractClass,addAttribute#QUMLAttribute,PushDownMethod,OK

--- src/gr/uom/java/xmi/UMLAnonymousClass.java
+++ src/gr/uom/java/xmi/UMLAnonymousClass.java
@@ -59,4 +59,8 @@ public class UMLAnonymousClass extends UMLAbstractClass implements Comparable<UM
 	public boolean isInterface() {
 		return false;
 	}
+
+	public void addAttribute(UMLAttribute attribute) {
+		this.attributes.add(attribute);
+	}
 }
