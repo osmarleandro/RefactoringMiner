870adacc20ce470d5ff5ea7090841a94412e717b
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 12:00:21 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,replaceParametersWithArguments#QMapQStringQString,PushDownMethod,OK

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -3,8 +3,6 @@ package gr.uom.java.xmi.decomposition;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import gr.uom.java.xmi.LocationInfo.CodeElementType;
 import gr.uom.java.xmi.LocationInfoProvider;
@@ -13,7 +11,7 @@ import gr.uom.java.xmi.decomposition.AbstractCall.StatementCoverageType;
 public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	private int depth;
 	private int index;
-	private String codeFragmentAfterReplacingParametersWithArguments;
+	protected String codeFragmentAfterReplacingParametersWithArguments;
 
 	public String getArgumentizedString() {
 		return codeFragmentAfterReplacingParametersWithArguments != null ? codeFragmentAfterReplacingParametersWithArguments : getString();
@@ -58,46 +56,7 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	public abstract VariableDeclaration searchVariableDeclaration(String variableName);
 	public abstract VariableDeclaration getVariableDeclaration(String variableName);
 	
-	public void replaceParametersWithArguments(Map<String, String> parameterToArgumentMap) {
-		String afterReplacements = getString();
-		for(String parameter : parameterToArgumentMap.keySet()) {
-			String argument = parameterToArgumentMap.get(parameter);
-			if(!parameter.equals(argument)) {
-				StringBuffer sb = new StringBuffer();
-				Pattern p = Pattern.compile(Pattern.quote(parameter));
-				Matcher m = p.matcher(afterReplacements);
-				while(m.find()) {
-					//check if the matched string is an argument
-					//previous character should be "(" or "," or " " or there is no previous character
-					int start = m.start();
-					boolean isArgument = false;
-					boolean isInsideStringLiteral = false;
-					if(start >= 1) {
-						String previousChar = afterReplacements.substring(start-1, start);
-						if(previousChar.equals("(") || previousChar.equals(",") || previousChar.equals(" ") || previousChar.equals("=")) {
-							isArgument = true;
-						}
-						String beforeMatch = afterReplacements.substring(0, start);
-						String afterMatch = afterReplacements.substring(start+parameter.length(), afterReplacements.length());
-						if(quoteBefore(beforeMatch) && quoteAfter(afterMatch)) {
-							isInsideStringLiteral = true;
-						}
-					}
-					else if(start == 0 && !afterReplacements.startsWith("return ")) {
-						isArgument = true;
-					}
-					if(isArgument && !isInsideStringLiteral) {
-						m.appendReplacement(sb, Matcher.quoteReplacement(argument));
-					}
-				}
-				m.appendTail(sb);
-				afterReplacements = sb.toString();
-			}
-		}
-		this.codeFragmentAfterReplacingParametersWithArguments = afterReplacements;
-	}
-
-	private static boolean quoteBefore(String beforeMatch) {
+	protected static boolean quoteBefore(String beforeMatch) {
 		if(beforeMatch.contains("\"")) {
 			if(beforeMatch.contains("+")) {
 				int indexOfQuote = beforeMatch.lastIndexOf("\"");
@@ -116,7 +75,7 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 		return false;
 	}
 
-	private static boolean quoteAfter(String afterMatch) {
+	protected static boolean quoteAfter(String afterMatch) {
 		if(afterMatch.contains("\"")) {
 			if(afterMatch.contains("+")) {
 				int indexOfQuote = afterMatch.indexOf("\"");
--- src/gr/uom/java/xmi/decomposition/AbstractStatement.java
+++ src/gr/uom/java/xmi/decomposition/AbstractStatement.java
@@ -1,6 +1,9 @@
 package gr.uom.java.xmi.decomposition;
 
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class AbstractStatement extends AbstractCodeFragment {
 	private CompositeStatementObject parent;
@@ -30,4 +33,43 @@ public abstract class AbstractStatement extends AbstractCodeFragment {
 
     public abstract List<StatementObject> getLeaves();
     public abstract int statementCount();
+
+	public void replaceParametersWithArguments(Map<String, String> parameterToArgumentMap) {
+		String afterReplacements = getString();
+		for(String parameter : parameterToArgumentMap.keySet()) {
+			String argument = parameterToArgumentMap.get(parameter);
+			if(!parameter.equals(argument)) {
+				StringBuffer sb = new StringBuffer();
+				Pattern p = Pattern.compile(Pattern.quote(parameter));
+				Matcher m = p.matcher(afterReplacements);
+				while(m.find()) {
+					//check if the matched string is an argument
+					//previous character should be "(" or "," or " " or there is no previous character
+					int start = m.start();
+					boolean isArgument = false;
+					boolean isInsideStringLiteral = false;
+					if(start >= 1) {
+						String previousChar = afterReplacements.substring(start-1, start);
+						if(previousChar.equals("(") || previousChar.equals(",") || previousChar.equals(" ") || previousChar.equals("=")) {
+							isArgument = true;
+						}
+						String beforeMatch = afterReplacements.substring(0, start);
+						String afterMatch = afterReplacements.substring(start+parameter.length(), afterReplacements.length());
+						if(quoteBefore(beforeMatch) && quoteAfter(afterMatch)) {
+							isInsideStringLiteral = true;
+						}
+					}
+					else if(start == 0 && !afterReplacements.startsWith("return ")) {
+						isArgument = true;
+					}
+					if(isArgument && !isInsideStringLiteral) {
+						m.appendReplacement(sb, Matcher.quoteReplacement(argument));
+					}
+				}
+				m.appendTail(sb);
+				afterReplacements = sb.toString();
+			}
+		}
+		this.codeFragmentAfterReplacingParametersWithArguments = afterReplacements;
+	}
 }
