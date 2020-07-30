9f7310f579048bf5643c36cd1ed591c2731627d5
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:08:31 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,getTypes#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -38,6 +38,7 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	public abstract CompositeStatementObject getParent();
 	public abstract String getString();
 	public abstract List<String> getVariables();
+	@Override
 	public abstract List<String> getTypes();
 	public abstract List<VariableDeclaration> getVariableDeclarations();
 	public abstract Map<String, List<OperationInvocation>> getMethodInvocationMap();
