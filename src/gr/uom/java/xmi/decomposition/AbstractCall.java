31af2f0f929c2cd192477a02f435efa70524826b
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 11:50:33 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCall,getName#,PushDownMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractCall.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCall.java
@@ -40,7 +40,6 @@ public abstract class AbstractCall implements LocationInfoProvider {
 	}
 
 	public abstract boolean identicalName(AbstractCall call);
-	public abstract String getName();
 	public abstract double normalizedNameDistance(AbstractCall call);
 	public abstract AbstractCall update(String oldExpression, String newExpression);
 
