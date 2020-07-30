ead17a597cf0efe547d01a73eb3d70de2b3a5f45
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:58:24 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassRenameDiff,samePackage#,PullUpMethod,OK

--- src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
@@ -1671,4 +1671,8 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 	public UMLModelDiff getModelDiff() {
 		return modelDiff;
 	}
+
+	public boolean samePackage() {
+		return originalClass.getPackageName().equals(nextClass.getPackageName());
+	}
 }
