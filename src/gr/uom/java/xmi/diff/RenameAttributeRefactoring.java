e54287012361929cd393a77d3cfe9db3081493cd
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:44:37 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenameAttributeRefactoring,getInvolvedClassesAfterRefactoring#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/RenameAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenameAttributeRefactoring.java
@@ -114,6 +114,7 @@ public class RenameAttributeRefactoring implements Refactoring {
 		return pairs;
 	}
 
+	@Override
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getRenamedAttribute().getLocationInfo().getFilePath(), getClassNameAfter()));
