ec6df82c5bc25722936c27e80a26b93fc1d8bde6
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:34:08 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,InlineVariableRefactoring,getInvolvedClassesAfterRefactoring#,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/diff/InlineVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/InlineVariableRefactoring.java
@@ -109,6 +109,7 @@ public class InlineVariableRefactoring implements Refactoring {
 		return pairs;
 	}
 
+	@Override
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getOperationAfter().getLocationInfo().getFilePath(), getOperationAfter().getClassName()));
