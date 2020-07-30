d7ef51c6ed26d28c6082368fa082b48a5f5e9883
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:06:14 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,CompositeStatementObject,loopWithVariables#QString;QString,PullUpMethod,BUILD FAILED

--- src/gr/uom/java/xmi/decomposition/AbstractStatement.java
+++ src/gr/uom/java/xmi/decomposition/AbstractStatement.java
@@ -2,6 +2,8 @@ package gr.uom.java.xmi.decomposition;
 
 import java.util.List;
 
+import gr.uom.java.xmi.LocationInfo.CodeElementType;
+
 public abstract class AbstractStatement extends AbstractCodeFragment {
 	private CompositeStatementObject parent;
 	
@@ -30,4 +32,50 @@ public abstract class AbstractStatement extends AbstractCodeFragment {
 
     public abstract List<StatementObject> getLeaves();
     public abstract int statementCount();
+
+	public CompositeStatementObject loopWithVariables(String currentElementName, String collectionName) {
+		for(CompositeStatementObject innerNode : getInnerNodes()) {
+			if(innerNode.getLocationInfo().getCodeElementType().equals(CodeElementType.ENHANCED_FOR_STATEMENT)) {
+				boolean currentElementNameMatched = false;
+				for(VariableDeclaration declaration : innerNode.getVariableDeclarations()) {
+					if(declaration.getVariableName().equals(currentElementName)) {
+						currentElementNameMatched = true;
+						break;
+					}
+				}
+				boolean collectionNameMatched = false;
+				for(AbstractExpression expression : innerNode.getExpressions()) {
+					if(expression.getVariables().contains(collectionName)) {
+						collectionNameMatched = true;
+						break;
+					}
+				}
+				if(currentElementNameMatched && collectionNameMatched) {
+					return innerNode;
+				}
+			}
+			else if(innerNode.getLocationInfo().getCodeElementType().equals(CodeElementType.FOR_STATEMENT) ||
+					innerNode.getLocationInfo().getCodeElementType().equals(CodeElementType.WHILE_STATEMENT)) {
+				boolean collectionNameMatched = false;
+				for(AbstractExpression expression : innerNode.getExpressions()) {
+					if(expression.getVariables().contains(collectionName)) {
+						collectionNameMatched = true;
+						break;
+					}
+				}
+				boolean currentElementNameMatched = false;
+				for(StatementObject statement : innerNode.getLeaves()) {
+					VariableDeclaration variableDeclaration = statement.getVariableDeclaration(currentElementName);
+					if(variableDeclaration != null && statement.getVariables().contains(collectionName)) {
+						currentElementNameMatched = true;
+						break;
+					}
+				}
+				if(currentElementNameMatched && collectionNameMatched) {
+					return innerNode;
+				}
+			}
+		}
+		return null;
+	}
 }
