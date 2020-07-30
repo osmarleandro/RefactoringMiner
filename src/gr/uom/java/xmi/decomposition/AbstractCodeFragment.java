ade1dccba1abe2962f4bfcb5f8265e4bb1591a1f
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 15:09:29 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCodeFragment,getTernaryOperatorExpressions#,PullUpMethod,OK

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -53,6 +53,7 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 	public abstract List<String> getPrefixExpressions();
 	public abstract List<String> getPostfixExpressions();
 	public abstract List<String> getArguments();
+	@Override
 	public abstract List<TernaryOperatorExpression> getTernaryOperatorExpressions();
 	public abstract List<LambdaExpressionObject> getLambdas();
 	public abstract VariableDeclaration searchVariableDeclaration(String variableName);
--- src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
+++ src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
@@ -256,4 +256,10 @@ public class VariableDeclaration implements LocationInfoProvider, VariableDeclar
 	public VariableDeclaration getVariableDeclaration() {
 		return this;
 	}
+
+	@Override
+	public List<TernaryOperatorExpression> getTernaryOperatorExpressions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
