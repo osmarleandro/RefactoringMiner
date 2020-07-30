8343fb0db2eb6132c46573fe029c698a55845ea4
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 14:48:40 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLType,equalsQualified#QUMLType,PullUpMethod,OK

--- src/gr/uom/java/xmi/UMLType.java
+++ src/gr/uom/java/xmi/UMLType.java
@@ -136,6 +136,7 @@ public abstract class UMLType implements Serializable, LocationInfoProvider {
 	public abstract String toQualifiedString();
 	public abstract String getClassType();
 
+	@Override
 	public boolean equalsQualified(UMLType type) {
 		if(this.getClass() == type.getClass()) {
 			return this.equals(type);
