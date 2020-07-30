af87434d58ffa7c359cb05babb2c0c050487d2a7
Osmar Leandro <osmarleandrods@gmail.com>
2020-07-30 14:49:15 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLAnnotation,getTypeName#,PullUpMethod,OK

--- src/gr/uom/java/xmi/UMLAnnotation.java
+++ src/gr/uom/java/xmi/UMLAnnotation.java
@@ -17,7 +17,7 @@ import gr.uom.java.xmi.diff.CodeRange;
 
 public class UMLAnnotation implements Serializable, LocationInfoProvider {
 	private LocationInfo locationInfo;
-	private String typeName;
+	String typeName;
 	private AbstractExpression value;
 	private Map<String, AbstractExpression> memberValuePairs = new LinkedHashMap<>();
 	
@@ -38,6 +38,7 @@ public class UMLAnnotation implements Serializable, LocationInfoProvider {
 		}
 	}
 
+	@Override
 	public String getTypeName() {
 		return typeName;
 	}
