8e8b47d292626977d4fdf1f7264183bb33922786
Osmar Leandro <osmarleandrods@gmail.com>
2020-09-07 17:32:10 -0300

    RefactoringMiner,org.refactoringminer.util,ExternalProcess,NA,RenameType,OK

--- src/org/refactoringminer/util/ExternalProcess_RENAMED.java
+++ src/org/refactoringminer/util/ExternalProcess_RENAMED.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-class ExternalProcess {
+class ExternalProcess_RENAMED {
 
 	public static String execute(File workingDir, String ... commandAndArgs) {
 		try {
