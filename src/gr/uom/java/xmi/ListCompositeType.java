b558c53d48c46b9041fe013b70b9691dff7e330e
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 04:12:55 -0300

    RefactoringMiner,gr.uom.java.xmi,ListCompositeType,hashCode#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/ListCompositeType.java
+++ src/gr/uom/java/xmi/ListCompositeType.java
@@ -28,15 +28,6 @@ public class ListCompositeType extends UMLType {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((kind == null) ? 0 : kind.hashCode());
-		result = prime * result + ((types == null) ? 0 : types.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
