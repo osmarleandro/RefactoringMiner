86ca79417a12b7c34f34d913635f34a9dad6e3b6
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:53:01 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,InlineVariableRefactoring,getOperationAfter#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/InlineVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/InlineVariableRefactoring.java
@@ -46,10 +46,6 @@ public class InlineVariableRefactoring implements Refactoring {
 		return operationBefore;
 	}
 
-	public UMLOperation getOperationAfter() {
-		return operationAfter;
-	}
-
 	public Set<AbstractCodeMapping> getReferences() {
 		return references;
 	}
@@ -111,7 +107,7 @@ public class InlineVariableRefactoring implements Refactoring {
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOperationAfter().getLocationInfo().getFilePath(), getOperationAfter().getClassName()));
+		pairs.add(new ImmutablePair<String, String>(operationAfter.getLocationInfo().getFilePath(), operationAfter.getClassName()));
 		return pairs;
 	}
 
