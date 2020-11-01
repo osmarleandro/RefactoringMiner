6cb5bedee7c103212201b7bc111e4e077c7c6832
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:51:49 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeReturnTypeRefactoring,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/ChangeReturnTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeReturnTypeRefactoring.java
@@ -71,17 +71,6 @@ public class ChangeReturnTypeRefactoring implements Refactoring {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((changedType == null) ? 0 : changedType.hashCode());
-		result = prime * result + ((operationAfter == null) ? 0 : operationAfter.hashCode());
-		result = prime * result + ((operationBefore == null) ? 0 : operationBefore.hashCode());
-		result = prime * result + ((originalType == null) ? 0 : originalType.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
