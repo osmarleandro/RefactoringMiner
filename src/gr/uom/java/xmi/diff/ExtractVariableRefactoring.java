222d9bc913fa1ae5a86772b15dac0fe0d9e73271
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 19:47:04 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractVariableRefactoring,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/ExtractVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractVariableRefactoring.java
@@ -73,15 +73,6 @@ public class ExtractVariableRefactoring implements Refactoring {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((operationAfter == null) ? 0 : operationAfter.hashCode());
-		result = prime * result + ((variableDeclaration == null) ? 0 : variableDeclaration.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
