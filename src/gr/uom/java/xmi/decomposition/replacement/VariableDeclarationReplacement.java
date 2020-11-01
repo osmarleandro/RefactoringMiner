47dee0321eb4b55b9d17e1561b7455b83d109a9a
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:23:56 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition.replacement,VariableDeclarationReplacement,getVariableNameReplacement#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/decomposition/replacement/VariableDeclarationReplacement.java
+++ src/gr/uom/java/xmi/decomposition/replacement/VariableDeclarationReplacement.java
@@ -35,10 +35,6 @@ public class VariableDeclarationReplacement extends Replacement {
 		return operation2;
 	}
 
-	public Replacement getVariableNameReplacement() {
-		return new Replacement(v1.getVariableName(), v2.getVariableName(), ReplacementType.VARIABLE_NAME);
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
