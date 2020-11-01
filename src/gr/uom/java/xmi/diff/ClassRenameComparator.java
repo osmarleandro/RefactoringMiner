2a22fb9413629f32c183f2a23d6f85b310a2acc4
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 13:14:11 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLClass,normalizedNameDistance#QUMLClass,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/ClassRenameComparator.java
+++ src/gr/uom/java/xmi/diff/ClassRenameComparator.java
@@ -2,12 +2,24 @@ package gr.uom.java.xmi.diff;
 
 import java.util.Comparator;
 
+import gr.uom.java.xmi.UMLClass;
+
 public class ClassRenameComparator implements Comparator<UMLClassRenameDiff> {
 
 	@Override
 	public int compare(UMLClassRenameDiff o1, UMLClassRenameDiff o2) {
-		double nameDistance1 = o1.getRenamedClass().normalizedNameDistance(o1.getOriginalClass());
-		double nameDistance2 = o2.getRenamedClass().normalizedNameDistance(o2.getOriginalClass());
+		UMLClass c = o1.getOriginalClass();
+		String s1 = o1.getRenamedClass().name.toLowerCase();
+		String s2 = c.name.toLowerCase();
+		int distance = StringDistance.editDistance(s1, s2);
+		double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
+		double nameDistance1 = normalized;
+		UMLClass c1 = o2.getOriginalClass();
+		String s1 = o2.getRenamedClass().name.toLowerCase();
+		String s2 = c1.name.toLowerCase();
+		int distance = StringDistance.editDistance(s1, s2);
+		double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
+		double nameDistance2 = normalized;
 		
 		if(nameDistance1 != nameDistance2) {
 			return Double.compare(nameDistance1, nameDistance2);
