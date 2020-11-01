0c81c92ad2e32180c4105c9652770fd5cf7e19d1
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 10:15:14 -0300

    RefactoringMiner,gr.uom.java.xmi,AnonymousClassDeclarationVisitor,visit#QAnonymousClassDeclaration,InlineMethod,OK,false

--- src/gr/uom/java/xmi/AnonymousClassDeclarationVisitor.java
+++ src/gr/uom/java/xmi/AnonymousClassDeclarationVisitor.java
@@ -9,11 +9,6 @@ import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
 public class AnonymousClassDeclarationVisitor extends ASTVisitor {
 
 	private Set<AnonymousClassDeclaration> anonymousClassDeclarations = new LinkedHashSet<AnonymousClassDeclaration>();
-	public boolean visit(AnonymousClassDeclaration node) {
-		anonymousClassDeclarations.add(node);
-		return super.visit(node);
-	}
-
 	public Set<AnonymousClassDeclaration> getAnonymousClassDeclarations() {
 		return anonymousClassDeclarations;
 	}
