a74d748d76a66a9a6b374ea10a679e7c6e7afbe6
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 19:16:53 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeAttributeTypeRefactoring,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/ChangeAttributeTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeAttributeTypeRefactoring.java
@@ -80,17 +80,6 @@ public class ChangeAttributeTypeRefactoring implements Refactoring {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((changedTypeAttribute == null) ? 0 : changedTypeAttribute.hashCode());
-		result = prime * result + ((classNameAfter == null) ? 0 : classNameAfter.hashCode());
-		result = prime * result + ((classNameBefore == null) ? 0 : classNameBefore.hashCode());
-		result = prime * result + ((originalAttribute == null) ? 0 : originalAttribute.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
