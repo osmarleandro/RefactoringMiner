13cbf8559f51ba889271f301b7b9b63bcbab9c0b
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 19:36:40 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractClassRefactoring,getInvolvedClassesBeforeRefactoring#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/ExtractClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractClassRefactoring.java
@@ -68,12 +68,6 @@ public class ExtractClassRefactoring implements Refactoring {
 		return attributeOfExtractedClassTypeInOriginalClass;
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOriginalClass().getLocationInfo().getFilePath(), getOriginalClass().getName()));
-		return pairs;
-	}
-
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getExtractedClass().getLocationInfo().getFilePath(), getExtractedClass().getName()));
