d13bac8dd6dbe29e6ea163620779e66f42751e16
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 02:12:21 -0300

    RefactoringMiner,gr.uom.java.xmi,LocationInfo,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/LocationInfo.java
+++ src/gr/uom/java/xmi/LocationInfo.java
@@ -88,21 +88,6 @@ public class LocationInfo {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + endColumn;
-		result = prime * result + endLine;
-		result = prime * result + endOffset;
-		result = prime * result + ((filePath == null) ? 0 : filePath.hashCode());
-		result = prime * result + length;
-		result = prime * result + startColumn;
-		result = prime * result + startLine;
-		result = prime * result + startOffset;
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
