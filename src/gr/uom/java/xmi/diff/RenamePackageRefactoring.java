e3c4ec60243ccae7229569f43acd6788eb63bac4
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:33:48 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenamePackageRefactoring,getInvolvedClassesBeforeRefactoring#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/RenamePackageRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenamePackageRefactoring.java
@@ -56,14 +56,6 @@ public class RenamePackageRefactoring implements Refactoring {
 		return sb.toString();
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		for(MoveClassRefactoring ref : moveClassRefactorings) {
-			pairs.add(new ImmutablePair<String, String>(ref.getOriginalClass().getLocationInfo().getFilePath(), ref.getOriginalClassName()));
-		}
-		return pairs;
-	}
-
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		for(MoveClassRefactoring ref : moveClassRefactorings) {
