556978889d48a2ee05a4f3dcfc77e958b2bcddcf
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:47:18 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,AddAttributeAnnotationRefactoring,getAnnotation#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/AddAttributeAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/AddAttributeAnnotationRefactoring.java
@@ -24,10 +24,6 @@ public class AddAttributeAnnotationRefactoring implements Refactoring {
 		this.attributeAfter = attributeAfter;
 	}
 
-	public UMLAnnotation getAnnotation() {
-		return annotation;
-	}
-
 	public UMLAttribute getAttributeBefore() {
 		return attributeBefore;
 	}
