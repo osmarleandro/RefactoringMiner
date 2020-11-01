6b68ea52ebf501492d099851a9ff0d71a260fe4a
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 03:59:38 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLTypeParameter,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/UMLTypeParameter.java
+++ src/gr/uom/java/xmi/UMLTypeParameter.java
@@ -35,15 +35,6 @@ public class UMLTypeParameter {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((typeBounds == null) ? 0 : typeBounds.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
