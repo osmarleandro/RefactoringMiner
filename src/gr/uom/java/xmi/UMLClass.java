18210ab021183eb9fbe06d08928a7fd63fec5333
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 13:16:59 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLClass,normalizedPackageNameDistance#QUMLClass,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/UMLClass.java
+++ src/gr/uom/java/xmi/UMLClass.java
@@ -317,14 +317,6 @@ public class UMLClass extends UMLAbstractClass implements Comparable<UMLClass>, 
 		return normalized;
 	}
 
-	public double normalizedPackageNameDistance(UMLClass c) {
-		String s1 = packageName.toLowerCase();
-		String s2 = c.packageName.toLowerCase();
-		int distance = StringDistance.editDistance(s1, s2);
-		double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
-		return normalized;
-	}
-	
 	public double normalizedSourceFolderDistance(UMLClass c) {
 		String s1 = sourceFolder.toLowerCase();
 		String s2 = c.sourceFolder.toLowerCase();
--- src/gr/uom/java/xmi/diff/ClassRenameComparator.java
+++ src/gr/uom/java/xmi/diff/ClassRenameComparator.java
@@ -2,6 +2,8 @@ package gr.uom.java.xmi.diff;
 
 import java.util.Comparator;
 
+import gr.uom.java.xmi.UMLClass;
+
 public class ClassRenameComparator implements Comparator<UMLClassRenameDiff> {
 
 	@Override
@@ -13,8 +15,18 @@ public class ClassRenameComparator implements Comparator<UMLClassRenameDiff> {
 			return Double.compare(nameDistance1, nameDistance2);
 		}
 		else {
-			double packageDistance1 = o1.getRenamedClass().normalizedPackageNameDistance(o1.getOriginalClass());
-			double packageDistance2 = o2.getRenamedClass().normalizedPackageNameDistance(o2.getOriginalClass());
+			UMLClass c = o1.getOriginalClass();
+			String s1 = o1.getRenamedClass().packageName.toLowerCase();
+			String s2 = c.packageName.toLowerCase();
+			int distance = StringDistance.editDistance(s1, s2);
+			double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
+			double packageDistance1 = normalized;
+			UMLClass c1 = o2.getOriginalClass();
+			String s1 = o2.getRenamedClass().packageName.toLowerCase();
+			String s2 = c1.packageName.toLowerCase();
+			int distance = StringDistance.editDistance(s1, s2);
+			double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
+			double packageDistance2 = normalized;
 			return Double.compare(packageDistance1, packageDistance2);
 		}
 	}
