69f3bf8e7f289fb02612c1a10db180985cfe7958
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:32:31 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveClassRefactoring,getInvolvedClassesAfterRefactoring#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/MoveClassRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveClassRefactoring.java
@@ -70,12 +70,6 @@ public class MoveClassRefactoring implements Refactoring {
 		return pairs;
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getMovedClass().getLocationInfo().getFilePath(), getMovedClass().getName()));
-		return pairs;
-	}
-
 	@Override
 	public List<CodeRange> leftSide() {
 		List<CodeRange> ranges = new ArrayList<CodeRange>();
