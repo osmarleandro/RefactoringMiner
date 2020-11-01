b4be6574e02925432518cc94c757667cbe1f3a3c
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 20:12:13 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractAttributeRefactoring,getNextClass#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/ExtractAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractAttributeRefactoring.java
@@ -66,10 +66,6 @@ public class ExtractAttributeRefactoring implements Refactoring {
 		return originalClass;
 	}
 
-	public UMLClass getNextClass() {
-		return nextClass;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -103,7 +99,7 @@ public class ExtractAttributeRefactoring implements Refactoring {
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getNextClass().getLocationInfo().getFilePath(), getNextClass().getName()));
+		pairs.add(new ImmutablePair<String, String>(nextClass.getLocationInfo().getFilePath(), nextClass.getName()));
 		return pairs;
 	}
 
