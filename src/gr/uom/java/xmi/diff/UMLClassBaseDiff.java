2218c749c56cab4e9b1fe10344604174b7c6dfa8
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:28:53 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,getRemovedAnonymousClasses#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
@@ -368,10 +368,6 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 		return addedAnonymousClasses;
 	}
 
-	public List<UMLAnonymousClass> getRemovedAnonymousClasses() {
-		return removedAnonymousClasses;
-	}
-
 	public Set<CandidateAttributeRefactoring> getCandidateAttributeRenames() {
 		return candidateAttributeRenames;
 	}
