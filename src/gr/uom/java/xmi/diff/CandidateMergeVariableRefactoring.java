5c58a9005083151de4a64960dd118db3d6867f4c
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 20:53:06 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,CandidateMergeVariableRefactoring,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/CandidateMergeVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/CandidateMergeVariableRefactoring.java
@@ -73,17 +73,6 @@ public class CandidateMergeVariableRefactoring {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((mergedVariables == null) ? 0 : mergedVariables.hashCode());
-		result = prime * result + ((newVariable == null) ? 0 : newVariable.hashCode());
-		result = prime * result + ((operationAfter == null) ? 0 : operationAfter.hashCode());
-		result = prime * result + ((operationBefore == null) ? 0 : operationBefore.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
