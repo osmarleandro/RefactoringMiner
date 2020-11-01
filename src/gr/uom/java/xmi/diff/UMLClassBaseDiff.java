683d2a20eb6448d4f05d3a7e65bf11bbd2b64027
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:31:15 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLClassBaseDiff,compareTo#QUMLClassBaseDiff,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
@@ -1651,10 +1651,6 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 		return sb.toString();
 	}
 
-	public int compareTo(UMLClassBaseDiff other) {
-		return this.originalClass.getName().compareTo(other.originalClass.getName());
-	}
-
 	public boolean containsExtractOperationRefactoring(UMLOperation sourceOperationBeforeExtraction, UMLOperation extractedOperation) {
 		for(Refactoring ref : refactorings) {
 			if(ref instanceof ExtractOperationRefactoring) {
