30ce34184939275851daf4036cdb5829446f6648
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:55:02 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RemoveMethodAnnotationRefactoring,getAnnotation#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
@@ -24,10 +24,6 @@ public class RemoveMethodAnnotationRefactoring implements Refactoring {
 		this.operationAfter = operationAfter;
 	}
 
-	public UMLAnnotation getAnnotation() {
-		return annotation;
-	}
-
 	public UMLOperation getOperationBefore() {
 		return operationBefore;
 	}
