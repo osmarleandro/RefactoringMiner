5057e2ef591a998937d84a184e2b8d4866eb2c3b
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:55:45 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RemoveMethodAnnotationRefactoring,getInvolvedClassesAfterRefactoring#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
@@ -74,13 +74,6 @@ public class RemoveMethodAnnotationRefactoring implements Refactoring {
 		return pairs;
 	}
 
-	@Override
-	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOperationAfter().getLocationInfo().getFilePath(), getOperationAfter().getClassName()));
-		return pairs;
-	}
-
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName()).append("\t");
