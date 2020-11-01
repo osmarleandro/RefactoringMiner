acd4165628e6758f1e338225b20df12c0bc94baa
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:47:35 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,AddAttributeAnnotationRefactoring,leftSide#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/AddAttributeAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/AddAttributeAnnotationRefactoring.java
@@ -37,15 +37,6 @@ public class AddAttributeAnnotationRefactoring implements Refactoring {
 	}
 
 	@Override
-	public List<CodeRange> leftSide() {
-		List<CodeRange> ranges = new ArrayList<CodeRange>();
-		ranges.add(attributeBefore.codeRange()
-				.setDescription("original attribute declaration")
-				.setCodeElement(attributeBefore.toString()));
-		return ranges;
-	}
-
-	@Override
 	public List<CodeRange> rightSide() {
 		List<CodeRange> ranges = new ArrayList<CodeRange>();
 		ranges.add(annotation.codeRange()
