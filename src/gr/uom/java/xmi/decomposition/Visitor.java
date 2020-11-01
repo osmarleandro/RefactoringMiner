15b96ce48121c90950c2201d9ceb29370db91431
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:16:07 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,Visitor,visit#QQualifiedType,InlineMethod,OK,false

--- src/gr/uom/java/xmi/decomposition/Visitor.java
+++ src/gr/uom/java/xmi/decomposition/Visitor.java
@@ -465,15 +465,6 @@ public class Visitor extends ASTVisitor {
 		return false;
 	}
 	
-	public boolean visit(QualifiedType node) {
-		types.add(node.toString());
-		if(current.getUserObject() != null) {
-			AnonymousClassDeclarationObject anonymous = (AnonymousClassDeclarationObject)current.getUserObject();
-			anonymous.getTypes().add(node.toString());
-		}
-		return false;
-	}
-	
 	public boolean visit(PrimitiveType node) {
 		types.add(node.toString());
 		if(current.getUserObject() != null) {
