a34bf1522191c680fb482ca9564288e67d963879
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:46:50 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,SplitAttributeRefactoring,equals#QObject,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/SplitAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/SplitAttributeRefactoring.java
@@ -79,38 +79,6 @@ public class SplitAttributeRefactoring implements Refactoring {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		SplitAttributeRefactoring other = (SplitAttributeRefactoring) obj;
-		if (classNameAfter == null) {
-			if (other.classNameAfter != null)
-				return false;
-		} else if (!classNameAfter.equals(other.classNameAfter))
-			return false;
-		if (classNameBefore == null) {
-			if (other.classNameBefore != null)
-				return false;
-		} else if (!classNameBefore.equals(other.classNameBefore))
-			return false;
-		if (oldAttribute == null) {
-			if (other.oldAttribute != null)
-				return false;
-		} else if (!oldAttribute.equals(other.oldAttribute))
-			return false;
-		if (splitAttributes == null) {
-			if (other.splitAttributes != null)
-				return false;
-		} else if (!splitAttributes.equals(other.splitAttributes))
-			return false;
-		return true;
-	}
-
-	@Override
 	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getOldAttribute().getLocationInfo().getFilePath(), getClassNameBefore()));
