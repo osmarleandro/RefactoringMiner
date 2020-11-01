8a9f0b2b5e8f7ed130ad9a64fe124f6bec87d060
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:34:25 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractOperationRefactoring,toString#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/ExtractOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractOperationRefactoring.java
@@ -59,21 +59,6 @@ public class ExtractOperationRefactoring implements Refactoring {
 		}
 	}
 
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getName()).append("\t");
-		sb.append(extractedOperation);
-		sb.append(" extracted from ");
-		sb.append(sourceOperationBeforeExtraction);
-		sb.append(" in class ");
-		sb.append(getClassName());
-		if(getRefactoringType().equals(RefactoringType.EXTRACT_AND_MOVE_OPERATION)) {
-			sb.append(" & moved to class ");
-			sb.append(extractedOperation.getClassName());
-		}
-		return sb.toString();
-	}
-
 	private String getClassName() {
 		if(getRefactoringType().equals(RefactoringType.EXTRACT_AND_MOVE_OPERATION)) {
 			return getSourceOperationBeforeExtraction().getClassName();
