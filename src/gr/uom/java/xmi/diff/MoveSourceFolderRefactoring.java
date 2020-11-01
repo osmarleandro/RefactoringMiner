11d34119d5bdf85fa6b6b11035f3e703c7da69b9
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 20:19:27 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveSourceFolderRefactoring,toString#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/MoveSourceFolderRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveSourceFolderRefactoring.java
@@ -36,17 +36,6 @@ public class MoveSourceFolderRefactoring implements Refactoring {
 		return pattern;
 	}
 
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getName()).append("\t");
-		String originalPath = pattern.getBefore().endsWith("/") ? pattern.getBefore().substring(0, pattern.getBefore().length()-1) : pattern.getBefore();
-		sb.append(originalPath);
-		sb.append(" to ");
-		String movedPath = pattern.getAfter().endsWith("/") ? pattern.getAfter().substring(0, pattern.getAfter().length()-1) : pattern.getAfter();
-		sb.append(movedPath);
-		return sb.toString();
-	}
-
 	public String getName() {
 		return this.getRefactoringType().getDisplayName();
 	}
