40d1aa7b5b17eb1319d6de835ab51e590f1045a6
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 19:02:41 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MergeVariableRefactoring,leftSide#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/MergeVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/MergeVariableRefactoring.java
@@ -136,17 +136,6 @@ public class MergeVariableRefactoring implements Refactoring {
 	}
 
 	@Override
-	public List<CodeRange> leftSide() {
-		List<CodeRange> ranges = new ArrayList<CodeRange>();
-		for(VariableDeclaration mergedVariable : mergedVariables) {
-			ranges.add(mergedVariable.codeRange()
-					.setDescription("merged variable declaration")
-					.setCodeElement(mergedVariable.toString()));
-		}
-		return ranges;
-	}
-
-	@Override
 	public List<CodeRange> rightSide() {
 		List<CodeRange> ranges = new ArrayList<CodeRange>();
 		ranges.add(newVariable.codeRange()
