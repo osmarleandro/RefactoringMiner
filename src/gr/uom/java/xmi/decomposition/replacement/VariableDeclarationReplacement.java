026c7f0ba4367d16b7547facc25cfff6e7c7593b
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:24:04 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition.replacement,VariableDeclarationReplacement,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/decomposition/replacement/VariableDeclarationReplacement.java
+++ src/gr/uom/java/xmi/decomposition/replacement/VariableDeclarationReplacement.java
@@ -40,15 +40,6 @@ public class VariableDeclarationReplacement extends Replacement {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((v1 == null) ? 0 : v1.hashCode());
-		result = prime * result + ((v2 == null) ? 0 : v2.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
