1db56ea7ca063f3b075a4ae657360344597d2126
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 13:19:44 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLClass,normalizedSourceFolderDistance#QUMLClass,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/ClassMoveComparator.java
+++ src/gr/uom/java/xmi/diff/ClassMoveComparator.java
@@ -2,12 +2,24 @@ package gr.uom.java.xmi.diff;
 
 import java.util.Comparator;
 
+import gr.uom.java.xmi.UMLClass;
+
 public class ClassMoveComparator implements Comparator<UMLClassMoveDiff> {
 
 	@Override
 	public int compare(UMLClassMoveDiff o1, UMLClassMoveDiff o2) {
-		double sourceFolderDistance1 = o1.getMovedClass().normalizedSourceFolderDistance(o1.getOriginalClass());
-		double sourceFolderDistance2 = o2.getMovedClass().normalizedSourceFolderDistance(o2.getOriginalClass());
+		UMLClass c = o1.getOriginalClass();
+		String s1 = o1.getMovedClass().sourceFolder.toLowerCase();
+		String s2 = c.sourceFolder.toLowerCase();
+		int distance = StringDistance.editDistance(s1, s2);
+		double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
+		double sourceFolderDistance1 = normalized;
+		UMLClass c1 = o2.getOriginalClass();
+		String s1 = o2.getMovedClass().sourceFolder.toLowerCase();
+		String s2 = c1.sourceFolder.toLowerCase();
+		int distance = StringDistance.editDistance(s1, s2);
+		double normalized = (double)distance/(double)Math.max(s1.length(), s2.length());
+		double sourceFolderDistance2 = normalized;
 		return Double.compare(sourceFolderDistance1, sourceFolderDistance2);
 	}
 }
