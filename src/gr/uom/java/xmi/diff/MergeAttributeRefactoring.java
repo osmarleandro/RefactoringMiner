f520b31d18c76181f79455d815576367be5cfa04
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 20:16:48 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MergeAttributeRefactoring,getInvolvedClassesBeforeRefactoring#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/MergeAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/MergeAttributeRefactoring.java
@@ -108,14 +108,6 @@ public class MergeAttributeRefactoring implements Refactoring {
 		return true;
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		for(VariableDeclaration mergedAttribute : this.mergedAttributes) {
-			pairs.add(new ImmutablePair<String, String>(mergedAttribute.getLocationInfo().getFilePath(), getClassNameBefore()));
-		}
-		return pairs;
-	}
-
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getNewAttribute().getLocationInfo().getFilePath(), getClassNameAfter()));
