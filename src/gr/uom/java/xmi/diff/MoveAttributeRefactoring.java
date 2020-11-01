749c1e7b97a1ec7b946c356cd4a6cf268e58b58d
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 19:26:09 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveAttributeRefactoring,getOriginalAttribute#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/MoveAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveAttributeRefactoring.java
@@ -24,7 +24,7 @@ public class MoveAttributeRefactoring implements Refactoring {
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName()).append("\t");
-		sb.append(getOriginalAttribute().toQualifiedString());
+		sb.append(originalAttribute.toQualifiedString());
 		sb.append(" from class ");
 		sb.append(getSourceClassName());
 		sb.append(" to ");
@@ -40,10 +40,6 @@ public class MoveAttributeRefactoring implements Refactoring {
 
 	public RefactoringType getRefactoringType() {
 		return RefactoringType.MOVE_ATTRIBUTE;
-	}
-
-	public UMLAttribute getOriginalAttribute() {
-		return originalAttribute;
 	}
 
 	public UMLAttribute getMovedAttribute() {
@@ -101,7 +97,7 @@ public class MoveAttributeRefactoring implements Refactoring {
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOriginalAttribute().getLocationInfo().getFilePath(), getOriginalAttribute().getClassName()));
+		pairs.add(new ImmutablePair<String, String>(originalAttribute.getLocationInfo().getFilePath(), originalAttribute.getClassName()));
 		return pairs;
 	}
 
