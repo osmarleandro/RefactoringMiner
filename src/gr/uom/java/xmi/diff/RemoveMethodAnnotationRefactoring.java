44677b2152535dc30559f3e9d4cfce95894d52a8
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:56:14 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RemoveMethodAnnotationRefactoring,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
@@ -93,16 +93,6 @@ public class RemoveMethodAnnotationRefactoring implements Refactoring {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((annotation == null) ? 0 : annotation.hashCode());
-		result = prime * result + ((operationAfter == null) ? 0 : operationAfter.hashCode());
-		result = prime * result + ((operationBefore == null) ? 0 : operationBefore.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
