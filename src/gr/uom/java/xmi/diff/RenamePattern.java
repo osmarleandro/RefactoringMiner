d551ecfde55cb9f0fbc179acf102fcae36391824
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:39:23 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenamePattern,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/RenamePattern.java
+++ src/gr/uom/java/xmi/diff/RenamePattern.java
@@ -31,14 +31,6 @@ public class RenamePattern {
     	}
     	return false;
 	}
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((after == null) ? 0 : after.hashCode());
-		result = prime * result + ((before == null) ? 0 : before.hashCode());
-		return result;
-	}
-	
 	public RenamePattern reverse() {
 		return new RenamePattern(after, before);
 	}
