6f9fb995bba3e8ed6ce8cd86644ff5b0daa92e4a
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 20:23:35 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,MoveSourceFolderRefactoring,getInvolvedClassesAfterRefactoring#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/MoveSourceFolderRefactoring.java
+++ src/gr/uom/java/xmi/diff/MoveSourceFolderRefactoring.java
@@ -63,14 +63,6 @@ public class MoveSourceFolderRefactoring implements Refactoring {
 		return pairs;
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		for(MovedClassToAnotherSourceFolder ref : movedClassesToAnotherSourceFolder) {
-			pairs.add(new ImmutablePair<String, String>(ref.getMovedClass().getLocationInfo().getFilePath(), ref.getMovedClassName()));
-		}
-		return pairs;
-	}
-
 	@Override
 	public List<CodeRange> leftSide() {
 		List<CodeRange> ranges = new ArrayList<CodeRange>();
