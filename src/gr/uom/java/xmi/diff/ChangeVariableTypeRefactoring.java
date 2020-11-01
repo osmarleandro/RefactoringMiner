9150a524abbb20b7ccf7e30373cfd626cadbc52b
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 19:40:34 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeVariableTypeRefactoring,getInvolvedClassesBeforeRefactoring#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/ChangeVariableTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeVariableTypeRefactoring.java
@@ -125,12 +125,6 @@ public class ChangeVariableTypeRefactoring implements Refactoring {
 		return true;
 	}
 
-	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
-		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOperationBefore().getLocationInfo().getFilePath(), getOperationBefore().getClassName()));
-		return pairs;
-	}
-
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
 		pairs.add(new ImmutablePair<String, String>(getOperationAfter().getLocationInfo().getFilePath(), getOperationAfter().getClassName()));
