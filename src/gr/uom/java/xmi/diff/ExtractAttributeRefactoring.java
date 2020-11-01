5aa8a74dd706bf3c3ec4cb69617ae3083220701f
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 20:13:57 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractAttributeRefactoring,hashCode#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/ExtractAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ExtractAttributeRefactoring.java
@@ -71,14 +71,6 @@ public class ExtractAttributeRefactoring implements Refactoring {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((attributeDeclaration == null) ? 0 : attributeDeclaration.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
