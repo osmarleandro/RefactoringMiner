0f63cd3806ac0e0fa025f288a0569c8062018858
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:21:23 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition.replacement,ConsistentReplacementDetector,inconsistentRenames#QSetQT;QT,InlineMethod,OK,false

--- src/gr/uom/java/xmi/decomposition/replacement/ConsistentReplacementDetector.java
+++ src/gr/uom/java/xmi/decomposition/replacement/ConsistentReplacementDetector.java
@@ -6,26 +6,21 @@ import java.util.Set;
 
 public class ConsistentReplacementDetector {
 
-	private static <T extends Replacement> Set<T> inconsistentRenames(
-			Set<T> currentRenames, T newRename) {
-		Set<T> inconsistentRenames = new LinkedHashSet<T>();
-		for(T rename : currentRenames) {
-			if(rename.getBefore().equals(newRename.getBefore()) && !rename.getAfter().equals(newRename.getAfter())) {
-				inconsistentRenames.add(rename);
-			}
-			else if(!rename.getBefore().equals(newRename.getBefore()) && rename.getAfter().equals(newRename.getAfter())) {
-				inconsistentRenames.add(rename);
-			}
-		}
-		return inconsistentRenames;
-	}
-
 	public static <T extends Replacement> void updateRenames(
 			Set<T> allConsistentRenames,
 			Set<T> allInconsistentRenames,
 			Set<T> renames) {
 		for(T newRename : renames) {
-			Set<T> inconsistentRenames = inconsistentRenames(allConsistentRenames, newRename);
+			Set<T> inconsistentRenames1 = new LinkedHashSet<T>();
+			for(T rename : allConsistentRenames) {
+				if(rename.getBefore().equals(newRename.getBefore()) && !rename.getAfter().equals(newRename.getAfter())) {
+					inconsistentRenames1.add(rename);
+				}
+				else if(!rename.getBefore().equals(newRename.getBefore()) && rename.getAfter().equals(newRename.getAfter())) {
+					inconsistentRenames1.add(rename);
+				}
+			}
+			Set<T> inconsistentRenames = inconsistentRenames1;
 			if(inconsistentRenames.isEmpty()) {
 				allConsistentRenames.add(newRename);
 			}
@@ -44,7 +39,16 @@ public class ConsistentReplacementDetector {
 			Map<String, Set<String>> aliasedAttributesInOriginalClass,
 			Map<String, Set<String>> aliasedAttributesInNextClass) {
 		for(T newRename : renames) {
-			Set<T> inconsistentRenames = inconsistentRenames(allConsistentRenames, newRename);
+			Set<T> inconsistentRenames1 = new LinkedHashSet<T>();
+			for(T rename : allConsistentRenames) {
+				if(rename.getBefore().equals(newRename.getBefore()) && !rename.getAfter().equals(newRename.getAfter())) {
+					inconsistentRenames1.add(rename);
+				}
+				else if(!rename.getBefore().equals(newRename.getBefore()) && rename.getAfter().equals(newRename.getAfter())) {
+					inconsistentRenames1.add(rename);
+				}
+			}
+			Set<T> inconsistentRenames = inconsistentRenames1;
 			filter(inconsistentRenames, aliasedAttributesInOriginalClass, aliasedAttributesInNextClass);
 			if(inconsistentRenames.isEmpty()) {
 				allConsistentRenames.add(newRename);
