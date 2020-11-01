f80b9f232fe1c7ae1a7785b0909543f136ff32a2
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 19:16:11 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeAttributeTypeRefactoring,getClassNameAfter#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/ChangeAttributeTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeAttributeTypeRefactoring.java
@@ -50,10 +50,6 @@ public class ChangeAttributeTypeRefactoring implements Refactoring {
 		return classNameBefore;
 	}
 
-	public String getClassNameAfter() {
-		return classNameAfter;
-	}
-
 	public Set<AbstractCodeMapping> getAttributeReferences() {
 		return attributeReferences;
 	}
@@ -132,7 +128,7 @@ public class ChangeAttributeTypeRefactoring implements Refactoring {
 	@Override
 	public Set<ImmutablePair<String, String>> getInvolvedClassesAfterRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getChangedTypeAttribute().getLocationInfo().getFilePath(), getClassNameAfter()));
+		pairs.add(new ImmutablePair<String, String>(getChangedTypeAttribute().getLocationInfo().getFilePath(), classNameAfter));
 		return pairs;
 	}
 
