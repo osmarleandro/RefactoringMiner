52fc57bdedc166fd6b11579fb0b96f771029d512
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:57:06 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,PullUpOperationRefactoring,rightSide#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/PullUpOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/PullUpOperationRefactoring.java
@@ -21,13 +21,4 @@ public class PullUpOperationRefactoring extends MoveOperationRefactoring {
 	public RefactoringType getRefactoringType() {
 		return RefactoringType.PULL_UP_OPERATION;
 	}
-
-	@Override
-	public List<CodeRange> rightSide() {
-		List<CodeRange> ranges = new ArrayList<CodeRange>();
-		ranges.add(movedOperation.codeRange()
-				.setDescription("pulled up method declaration")
-				.setCodeElement(movedOperation.toString()));
-		return ranges;
-	}
 }
