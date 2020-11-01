2273d7e5dbbbea827596c9955da8261dc18a9625
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 18:40:01 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ConvertAnonymousClassToTypeRefactoring,toString#,InlineMethod,OK,false

--- src/gr/uom/java/xmi/diff/ConvertAnonymousClassToTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ConvertAnonymousClassToTypeRefactoring.java
@@ -29,15 +29,6 @@ public class ConvertAnonymousClassToTypeRefactoring implements Refactoring {
 		return addedClass;
 	}
 
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getName()).append("\t");
-		sb.append(anonymousClass);
-		sb.append(" was converted to ");
-		sb.append(addedClass);
-		return sb.toString();
-	}
-
 	public String getName() {
 		return this.getRefactoringType().getDisplayName();
 	}
