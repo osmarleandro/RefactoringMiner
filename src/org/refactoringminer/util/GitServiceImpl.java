d864b1ea8a0b6e91271888af7dd1b3712c808fb3
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 21:11:19 -0300

    RefactoringMiner,org.refactoringminer.util,GitServiceImpl,cloneIfNotExists#QString;QString,InlineMethod,BUILD FAILED,false

--- src/org/refactoringminer/util/GitServiceImpl.java
+++ src/org/refactoringminer/util/GitServiceImpl.java
@@ -45,61 +45,6 @@ public class GitServiceImpl implements GitService {
 	DefaultCommitsFilter commitsFilter = new DefaultCommitsFilter();
 	
 	@Override
-	public Repository cloneIfNotExists(String projectPath, String cloneUrl/*, String branch*/) throws Exception {
-		File folder = new File(projectPath);
-		Repository repository;
-		if (folder.exists()) {
-			RepositoryBuilder builder = new RepositoryBuilder();
-			repository = builder
-					.setGitDir(new File(folder, ".git"))
-					.readEnvironment()
-					.findGitDir()
-					.build();
-			
-			//logger.info("Project {} is already cloned, current branch is {}", cloneUrl, repository.getBranch());
-			
-		} else {
-			logger.info("Cloning {} ...", cloneUrl);
-			Git git = Git.cloneRepository()
-					.setDirectory(folder)
-					.setURI(cloneUrl)
-					.setCloneAllBranches(true)
-					.call();
-			repository = git.getRepository();
-			//logger.info("Done cloning {}, current branch is {}", cloneUrl, repository.getBranch());
-		}
-
-//		if (branch != null && !repository.getBranch().equals(branch)) {
-//			Git git = new Git(repository);
-//			
-//			String localBranch = "refs/heads/" + branch;
-//			List<Ref> refs = git.branchList().call();
-//			boolean branchExists = false;
-//			for (Ref ref : refs) {
-//				if (ref.getName().equals(localBranch)) {
-//					branchExists = true;
-//				}
-//			}
-//			
-//			if (branchExists) {
-//				git.checkout()
-//					.setName(branch)
-//					.call();
-//			} else {
-//				git.checkout()
-//					.setCreateBranch(true)
-//					.setName(branch)
-//					.setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK)
-//					.setStartPoint("origin/" + branch)
-//					.call();
-//			}
-//			
-//			logger.info("Project {} switched to {}", cloneUrl, repository.getBranch());
-//		}
-		return repository;
-	}
-
-	@Override
 	public Repository openRepository(String repositoryPath) throws Exception {
 	    File folder = new File(repositoryPath);
 	    Repository repository;
