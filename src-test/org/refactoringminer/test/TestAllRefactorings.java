2e3fb248f132eeb603d846c2aaf2bab8c47d6b77
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 21:12:34 -0300

    RefactoringMiner,org.refactoringminer.test,TestAllRefactorings,testAllRefactorings#,InlineMethod,OK,false

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -7,11 +7,4 @@ import org.refactoringminer.test.RefactoringPopulator.Systems;
 import org.junit.Test;
 
 public class TestAllRefactorings {
-
-	@Test
-	public void testAllRefactorings() throws Exception {
-		TestBuilder test = new TestBuilder(new GitHistoryRefactoringMinerImpl(), "tmp1", Refactorings.All.getValue());
-		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectations();
-	}
 }
