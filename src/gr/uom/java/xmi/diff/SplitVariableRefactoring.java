c1783b73493af5130875124162e793210f56e04e
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 21:04:11 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,SplitVariableRefactoring,getOperationBefore#,InlineMethod,BUILD FAILED,false

--- src/gr/uom/java/xmi/diff/SplitVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/SplitVariableRefactoring.java
@@ -37,10 +37,6 @@ public class SplitVariableRefactoring implements Refactoring {
 		return oldVariable;
 	}
 
-	public UMLOperation getOperationBefore() {
-		return operationBefore;
-	}
-
 	public UMLOperation getOperationAfter() {
 		return operationAfter;
 	}
@@ -73,7 +69,7 @@ public class SplitVariableRefactoring implements Refactoring {
 	@Override
 	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
-		pairs.add(new ImmutablePair<String, String>(getOperationBefore().getLocationInfo().getFilePath(), getOperationBefore().getClassName()));
+		pairs.add(new ImmutablePair<String, String>(operationBefore.getLocationInfo().getFilePath(), operationBefore.getClassName()));
 		return pairs;
 	}
 
