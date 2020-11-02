9235ab70d145e809c4ea3e8f9da8c88846e77a2d
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 21:17:20 -0300

    RefactoringMiner,org.refactoringminer.utils,ResultComparator,collectRmResult#QGitHistoryRefactoringMiner;QRefactoringSet,InlineMethod,OK,false

--- src-test/org/refactoringminer/utils/ResultComparator.java
+++ src-test/org/refactoringminer/utils/ResultComparator.java
@@ -349,14 +349,6 @@ public class ResultComparator {
         }
     }
 
-    public static RefactoringSet[] collectRmResult(GitHistoryRefactoringMiner rm, RefactoringSet[] oracle) {
-        RefactoringSet[] result = new RefactoringSet[oracle.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = collectRmResult(rm, oracle[i].getProject(), oracle[i].getRevision());
-        }
-        return result;
-    }
-
     public boolean isGroupRefactorings() {
         return groupRefactorings;
     }
