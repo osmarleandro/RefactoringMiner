8e1f457d587aaabf620f11da6f42ec0a6267cd62
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 21:13:33 -0300

    RefactoringMiner,org.refactoringminer.utils,RefactoringRelationshipGroup,getMainEntity#,InlineMethod,OK,false

--- src-test/org/refactoringminer/utils/RefactoringRelationshipGroup.java
+++ src-test/org/refactoringminer/utils/RefactoringRelationshipGroup.java
@@ -14,7 +14,7 @@ public class RefactoringRelationshipGroup {
   }
 
   public RefactoringType addRefactoringRelationship(RefactoringRelationship r) {
-    if (r.getRefactoringType().equals(this.getRefactoringType()) && r.getMainEntity().equals(this.getMainEntity())) {
+    if (r.getRefactoringType().equals(this.getRefactoringType()) && r.getMainEntity().equals(this.refactoringRelationships.get(0).getMainEntity())) {
       refactoringRelationships.add(r);
     }
     throw new IllegalArgumentException(String.format("refactoring relatiships are note from the same group: [] []", r, refactoringRelationships.get(0)));
@@ -24,15 +24,11 @@ public class RefactoringRelationshipGroup {
     return refactoringRelationships.get(0).getRefactoringType();
   }
 
-  public String getMainEntity() {
-    return refactoringRelationships.get(0).getMainEntity();
-  }
-
   @Override
   public boolean equals(Object obj) {
     if (obj instanceof RefactoringRelationshipGroup) {
       RefactoringRelationshipGroup other = (RefactoringRelationshipGroup) obj;
-      return other.getRefactoringType().equals(this.getRefactoringType()) && other.getMainEntity().equals(this.getMainEntity());
+      return other.getRefactoringType().equals(this.getRefactoringType()) && other.refactoringRelationships.get(0).getMainEntity().equals(this.refactoringRelationships.get(0).getMainEntity());
     }
     return false;
   }
@@ -41,7 +37,7 @@ public class RefactoringRelationshipGroup {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + getMainEntity().hashCode();
+    result = prime * result + refactoringRelationships.get(0).getMainEntity().hashCode();
     result = prime * result + getRefactoringType().hashCode();
     return result;
   }
