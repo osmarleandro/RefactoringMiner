6cdd2c7d96f16676eea56bdde0d730d6213d7dfa
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-01 21:18:37 -0300

    RefactoringMiner,org.refactoringminer.utils,RefactoringCollector,handle#QString;QListQRefactoring,InlineMethod,OK,false

--- src-test/org/refactoringminer/utils/RefactoringCollector.java
+++ src-test/org/refactoringminer/utils/RefactoringCollector.java
@@ -21,41 +21,6 @@ public class RefactoringCollector extends RefactoringHandler {
     rs = new RefactoringSet(cloneUrl, commitId);
   }
   @Override
-  public void handle(String commitId, List<Refactoring> refactorings) {
-    for (Refactoring r : refactorings) {
-      if (r instanceof MoveClassRefactoring) {
-        MoveClassRefactoring ref = (MoveClassRefactoring) r;
-        rs.add(new RefactoringRelationship(r.getRefactoringType(), ref.getOriginalClassName(), ref.getMovedClassName()));
-      } else if (r instanceof RenameClassRefactoring) {
-        RenameClassRefactoring ref = (RenameClassRefactoring) r;
-        rs.add(new RefactoringRelationship(r.getRefactoringType(), ref.getOriginalClassName(), ref.getRenamedClassName()));
-      } else if (r instanceof ExtractSuperclassRefactoring) {
-        ExtractSuperclassRefactoring ref = (ExtractSuperclassRefactoring) r;
-        for (String subclass : ref.getSubclassSet()) {
-          rs.add(new RefactoringRelationship(r.getRefactoringType(), subclass, ref.getExtractedClass().getName()));
-        }
-      } else if (r instanceof MoveOperationRefactoring) {
-        MoveOperationRefactoring ref = (MoveOperationRefactoring) r;
-        rs.add(new RefactoringRelationship(r.getRefactoringType(), ref.getOriginalOperation().getKey(), ref.getMovedOperation().getKey()));
-      } else if (r instanceof RenameOperationRefactoring) {
-        RenameOperationRefactoring ref = (RenameOperationRefactoring) r;
-        rs.add(new RefactoringRelationship(r.getRefactoringType(), ref.getOriginalOperation().getKey(), ref.getRenamedOperation().getKey()));
-      } else if (r instanceof ExtractOperationRefactoring) {
-        ExtractOperationRefactoring ref = (ExtractOperationRefactoring) r;
-        rs.add(new RefactoringRelationship(r.getRefactoringType(), ref.getSourceOperationBeforeExtraction().getKey(), ref.getExtractedOperation().getKey()));
-      } else if (r instanceof InlineOperationRefactoring) {
-        InlineOperationRefactoring ref = (InlineOperationRefactoring) r;
-        rs.add(new RefactoringRelationship(r.getRefactoringType(), ref.getInlinedOperation().getKey(), ref.getTargetOperationAfterInline().getKey()));
-      } else if (r instanceof MoveAttributeRefactoring) {
-        MoveAttributeRefactoring ref = (MoveAttributeRefactoring) r;
-        String attrName = ref.getMovedAttribute().getName();
-        rs.add(new RefactoringRelationship(r.getRefactoringType(), ref.getSourceClassName() + "#" + attrName, ref.getTargetClassName() + "#" + attrName));
-      } else {
-        throw new RuntimeException("refactoring not supported");
-      }
-    }
-  }
-  @Override
   public void handleException(String commitId, Exception e) {
     this.ex = e;
   }
