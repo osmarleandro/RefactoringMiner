2e2fd18c055e44cf3ea4c899c2f58081425ba07d
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:20:14 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,VariableDeclaration,hashCode#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
+++ src/gr/uom/java/xmi/decomposition/VariableDeclaration.java
@@ -142,9 +142,14 @@ public class VariableDeclaration implements LocationInfoProvider, VariableDeclar
 	@Override
 	public int hashCode() {
 		final int prime = 31;
+		int result = extracted(prime);
+		result = prime * result + ((variableName == null) ? 0 : variableName.hashCode());
+		return result;
+	}
+
+	private int extracted(final int prime) {
 		int result = 1;
 		result = prime * result + ((scope == null) ? 0 : scope.hashCode());
-		result = prime * result + ((variableName == null) ? 0 : variableName.hashCode());
 		return result;
 	}
 
