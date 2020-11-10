3482b157c25dc2b1341ca51b96b822eb890cdf54
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:56:47 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ModifyMethodAnnotationRefactoring,toString#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/ModifyMethodAnnotationRefactoring.java
@@ -91,8 +91,7 @@ public class ModifyMethodAnnotationRefactoring implements Refactoring {
 	}
 
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getName()).append("\t");
+		StringBuilder sb = extracted();
 		sb.append(annotationBefore);
 		sb.append(" to ");
 		sb.append(annotationAfter);
@@ -101,6 +100,12 @@ public class ModifyMethodAnnotationRefactoring implements Refactoring {
 		sb.append(" from class ");
 		sb.append(operationAfter.getClassName());
 		return sb.toString();
+	}
+
+	private StringBuilder extracted() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(getName()).append("\t");
+		return sb;
 	}
 	@Override
 	public int hashCode() {
