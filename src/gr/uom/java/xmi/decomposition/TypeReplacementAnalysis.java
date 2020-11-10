a4bc1095721e33d1cea53654a006a5c428106895
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:20:49 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,TypeReplacementAnalysis,findTypeChanges#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/decomposition/TypeReplacementAnalysis.java
+++ src/gr/uom/java/xmi/decomposition/TypeReplacementAnalysis.java
@@ -26,26 +26,32 @@ public class TypeReplacementAnalysis {
 		for(AbstractCodeMapping mapping : mappings) {
 			AbstractCodeFragment fragment1 = mapping.getFragment1();
 			AbstractCodeFragment fragment2 = mapping.getFragment2();
-			for(Replacement replacement : mapping.getReplacements()) {
-				if(replacement.getType().equals(ReplacementType.TYPE)) {
-					List<VariableDeclaration> declarations1 = fragment1.getVariableDeclarations();
-					List<VariableDeclaration> declarations2 = fragment2.getVariableDeclarations();
-					for(VariableDeclaration declaration1 : declarations1) {
-						for(VariableDeclaration declaration2 : declarations2) {
-							if(declaration1.getVariableName().equals(declaration2.getVariableName()) &&
-									(!declaration1.getType().equals(declaration2.getType()) || !declaration1.getType().equalsQualified(declaration2.getType())) &&
-									!containsVariableDeclarationWithSameNameAndType(declaration1, declarations2)) {
-								ChangeVariableTypeRefactoring ref = new ChangeVariableTypeRefactoring(declaration1, declaration2, mapping.getOperation1(), mapping.getOperation2(),
-										VariableReferenceExtractor.findReferences(declaration1, declaration2, mappings));
-								changedTypes.add(ref);
-								break;
-							}
+			for(Replacement replacement : mapping.getReplacements())
+				extracted(mapping, fragment1, fragment2, replacement);
+			if(fragment1.getLocationInfo().getCodeElementType().equals(CodeElementType.ENHANCED_FOR_STATEMENT) &&
+					fragment2.getLocationInfo().getCodeElementType().equals(CodeElementType.ENHANCED_FOR_STATEMENT)) {
+				List<VariableDeclaration> declarations1 = fragment1.getVariableDeclarations();
+				List<VariableDeclaration> declarations2 = fragment2.getVariableDeclarations();
+				for(VariableDeclaration declaration1 : declarations1) {
+					for(VariableDeclaration declaration2 : declarations2) {
+						if(declaration1.getVariableName().equals(declaration2.getVariableName()) &&
+								(!declaration1.getType().equals(declaration2.getType()) || !declaration1.getType().equalsQualified(declaration2.getType())) &&
+								!containsVariableDeclarationWithSameNameAndType(declaration1, declarations2)) {
+							ChangeVariableTypeRefactoring ref = new ChangeVariableTypeRefactoring(declaration1, declaration2, mapping.getOperation1(), mapping.getOperation2(),
+									VariableReferenceExtractor.findReferences(declaration1, declaration2, mappings));
+							changedTypes.add(ref);
+							break;
 						}
 					}
 				}
 			}
-			if(fragment1.getLocationInfo().getCodeElementType().equals(CodeElementType.ENHANCED_FOR_STATEMENT) &&
-					fragment2.getLocationInfo().getCodeElementType().equals(CodeElementType.ENHANCED_FOR_STATEMENT)) {
+		}
+	}
+
+	private void extracted(AbstractCodeMapping mapping, AbstractCodeFragment fragment1, AbstractCodeFragment fragment2,
+			Replacement replacement) {
+		{
+			if(replacement.getType().equals(ReplacementType.TYPE)) {
 				List<VariableDeclaration> declarations1 = fragment1.getVariableDeclarations();
 				List<VariableDeclaration> declarations2 = fragment2.getVariableDeclarations();
 				for(VariableDeclaration declaration1 : declarations1) {
