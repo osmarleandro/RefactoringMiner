82cf73f4f60e48f7a108da44aa8b96f58cd81508
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:59:45 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RenameVariableRefactoring,equals#QObject,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/RenameVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/RenameVariableRefactoring.java
@@ -96,6 +96,10 @@ public class RenameVariableRefactoring implements Refactoring {
 			return true;
 		if (obj == null)
 			return false;
+		return extracted(obj);
+	}
+
+	private boolean extracted(Object obj) {
 		if (getClass() != obj.getClass())
 			return false;
 		RenameVariableRefactoring other = (RenameVariableRefactoring) obj;
