db2f89e4733245895c9bd2a271c962d4e026a9d4
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 04:02:25 -0300

    RefactoringMiner,org.refactoringminer,RefactoringMinerHttpServer,queryToMap#QString,ExtractMethod,OK,false

--- src/org/refactoringminer/RefactoringMinerHttpServer.java
+++ src/org/refactoringminer/RefactoringMinerHttpServer.java
@@ -76,7 +76,13 @@ public class RefactoringMinerHttpServer {
 
 	private static Map<String, String> queryToMap(String query) {
 		Map<String, String> result = new HashMap<>();
-		for (String param : query.split("&")) {
+		for (String param : query.split("&"))
+			extracted(result, param);
+		return result;
+	}
+
+	private static void extracted(Map<String, String> result, String param) {
+		{
 			String[] entry = param.split("=");
 			if (entry.length > 1) {
 				result.put(entry[0], entry[1]);
@@ -85,7 +91,6 @@ public class RefactoringMinerHttpServer {
 				result.put(entry[0], "");
 			}
 		}
-		return result;
 	}
 
 	private static String JSON(String gitURL, String currentCommitId, List<Refactoring> refactoringsAtRevision) {
