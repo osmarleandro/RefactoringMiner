87dbc823dae454cf03d5ad0fdcfc04905650caeb
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:42:55 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLRealizationDiff,toString#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/UMLRealizationDiff.java
+++ src/gr/uom/java/xmi/diff/UMLRealizationDiff.java
@@ -20,14 +20,19 @@ public class UMLRealizationDiff implements Comparable<UMLRealizationDiff> {
 	}
 
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		if(parentChanged || childChanged)
-			sb.append("realization ").append(removedRealization).append(":").append("\n");
+		StringBuilder sb = extracted();
 		if(childChanged)
 			sb.append("\t").append("child changed from " + removedRealization.getClient() + " to " + addedRealization.getClient()).append("\n");
 		if(parentChanged)
 			sb.append("\t").append("parent changed from " + removedRealization.getSupplier() + " to " + addedRealization.getSupplier()).append("\n");
 		return sb.toString();
+	}
+
+	private StringBuilder extracted() {
+		StringBuilder sb = new StringBuilder();
+		if(parentChanged || childChanged)
+			sb.append("realization ").append(removedRealization).append(":").append("\n");
+		return sb;
 	}
 
 	public int compareTo(UMLRealizationDiff generalizationDiff) {
