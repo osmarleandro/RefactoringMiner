87408b458f06d5487e458880bc1cd9c151ce162c
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:41:22 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeReturnTypeRefactoring,equals#QObject,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/ChangeReturnTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeReturnTypeRefactoring.java
@@ -87,6 +87,10 @@ public class ChangeReturnTypeRefactoring implements Refactoring {
 			return true;
 		if (obj == null)
 			return false;
+		return extracted(obj);
+	}
+
+	private boolean extracted(Object obj) {
 		if (getClass() != obj.getClass())
 			return false;
 		ChangeReturnTypeRefactoring other = (ChangeReturnTypeRefactoring) obj;
