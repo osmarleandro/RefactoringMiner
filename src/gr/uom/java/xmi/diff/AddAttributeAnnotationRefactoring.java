98ad29cc9b2f934b1d381a278dd5da18d4f4ead4
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:40:48 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,AddAttributeAnnotationRefactoring,equals#QObject,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/AddAttributeAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/AddAttributeAnnotationRefactoring.java
@@ -108,6 +108,10 @@ public class AddAttributeAnnotationRefactoring implements Refactoring {
 			return true;
 		if (obj == null)
 			return false;
+		return extracted(obj);
+	}
+
+	private boolean extracted(Object obj) {
 		if (getClass() != obj.getClass())
 			return false;
 		AddAttributeAnnotationRefactoring other = (AddAttributeAnnotationRefactoring) obj;
