edeb38ad5f50c88f01b8219c6eac2a0a86385ba9
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:48:46 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLModelDiff,inferRefactoringsFromMatchingMappers#QListQUMLOperationBodyMapper;QUMLOperationDiff;QSetQRefactoring,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/UMLModelDiff.java
+++ src/gr/uom/java/xmi/diff/UMLModelDiff.java
@@ -1569,31 +1569,9 @@ public class UMLModelDiff {
    private void inferRefactoringsFromMatchingMappers(List<UMLOperationBodyMapper> mappers, UMLOperationDiff operationSignatureDiff, Set<Refactoring> refactorings) {
 	   for(UMLOperationBodyMapper mapper : mappers) {
 		   for(Refactoring refactoring : mapper.getRefactoringsAfterPostProcessing()) {
-			   if(refactoring instanceof RenameVariableRefactoring) {
-				   RenameVariableRefactoring rename = (RenameVariableRefactoring)refactoring;
-				   UMLParameter matchingRemovedParameter = null;
-				   for(UMLParameter parameter : operationSignatureDiff.getRemovedParameters()) {
-					   if(parameter.getName().equals(rename.getOriginalVariable().getVariableName()) &&
-							   parameter.getType().equals(rename.getOriginalVariable().getType())) {
-						   matchingRemovedParameter = parameter;
-						   break;
-					   }
-				   }
-				   UMLParameter matchingAddedParameter = null;
-				   for(UMLParameter parameter : operationSignatureDiff.getAddedParameters()) {
-					   if(parameter.getName().equals(rename.getRenamedVariable().getVariableName()) &&
-							   parameter.getType().equals(rename.getRenamedVariable().getType())) {
-						   matchingAddedParameter = parameter;
-						   break;
-					   }
-				   }
-				   if(matchingRemovedParameter != null && matchingAddedParameter != null) {
-					   RenameVariableRefactoring newRename = new RenameVariableRefactoring(matchingRemovedParameter.getVariableDeclaration(), matchingAddedParameter.getVariableDeclaration(),
-							   operationSignatureDiff.getRemovedOperation(), operationSignatureDiff.getAddedOperation(), new LinkedHashSet<AbstractCodeMapping>());
-					   refactorings.add(newRename);
-				   }
-			   }
-			   else if(refactoring instanceof ChangeVariableTypeRefactoring) {
+			   if(refactoring instanceof RenameVariableRefactoring)
+				extracted(operationSignatureDiff, refactorings, refactoring);
+			else if(refactoring instanceof ChangeVariableTypeRefactoring) {
 				   ChangeVariableTypeRefactoring changeType = (ChangeVariableTypeRefactoring)refactoring;
 				   UMLParameter matchingRemovedParameter = null;
 				   for(UMLParameter parameter : operationSignatureDiff.getRemovedParameters()) {
@@ -1620,6 +1598,34 @@ public class UMLModelDiff {
 		   }
 	   }
    }
+
+private void extracted(UMLOperationDiff operationSignatureDiff, Set<Refactoring> refactorings,
+		Refactoring refactoring) {
+	{
+		   RenameVariableRefactoring rename = (RenameVariableRefactoring)refactoring;
+		   UMLParameter matchingRemovedParameter = null;
+		   for(UMLParameter parameter : operationSignatureDiff.getRemovedParameters()) {
+			   if(parameter.getName().equals(rename.getOriginalVariable().getVariableName()) &&
+					   parameter.getType().equals(rename.getOriginalVariable().getType())) {
+				   matchingRemovedParameter = parameter;
+				   break;
+			   }
+		   }
+		   UMLParameter matchingAddedParameter = null;
+		   for(UMLParameter parameter : operationSignatureDiff.getAddedParameters()) {
+			   if(parameter.getName().equals(rename.getRenamedVariable().getVariableName()) &&
+					   parameter.getType().equals(rename.getRenamedVariable().getType())) {
+				   matchingAddedParameter = parameter;
+				   break;
+			   }
+		   }
+		   if(matchingRemovedParameter != null && matchingAddedParameter != null) {
+			   RenameVariableRefactoring newRename = new RenameVariableRefactoring(matchingRemovedParameter.getVariableDeclaration(), matchingAddedParameter.getVariableDeclaration(),
+					   operationSignatureDiff.getRemovedOperation(), operationSignatureDiff.getAddedOperation(), new LinkedHashSet<AbstractCodeMapping>());
+			   refactorings.add(newRename);
+		   }
+	   }
+}
 
    private List<UMLOperationBodyMapper> findMappersWithMatchingSignatures(UMLOperation operation1, UMLOperation operation2) {
 	   List<UMLOperationBodyMapper> mappers = new ArrayList<UMLOperationBodyMapper>();
