8336812561366b1887d42c2ac7ce52223a38f728
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:49:12 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,UMLModelDiff,moveAndInlineMatchCondition#QUMLOperationBodyMapper;QUMLOperationBodyMapper,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/UMLModelDiff.java
+++ src/gr/uom/java/xmi/diff/UMLModelDiff.java
@@ -1776,12 +1776,8 @@ public class UMLModelDiff {
 			}
 		}
 		int delegateStatements = 0;
-		for(StatementObject statement : operationBodyMapper.getNonMappedLeavesT1()) {
-			OperationInvocation invocation = statement.invocationCoveringEntireFragment();
-			if(invocation != null && invocation.matchesOperation(operationBodyMapper.getOperation1())) {
-				delegateStatements++;
-			}
-		}
+		for(StatementObject statement : operationBodyMapper.getNonMappedLeavesT1())
+			delegateStatements = extracted(operationBodyMapper, delegateStatements, statement);
 		int mappings = operationBodyMapper.mappingsWithoutBlocks();
 		int nonMappedElementsT1 = operationBodyMapper.nonMappedElementsT1()-delegateStatements;
 		List<AbstractCodeMapping> exactMatchList = operationBodyMapper.getExactMatches();
@@ -1789,6 +1785,17 @@ public class UMLModelDiff {
 		return mappings > 0 && (mappings > nonMappedElementsT1 ||
 				(exactMatches == 1 && !exactMatchList.get(0).getFragment1().throwsNewException() && nonMappedElementsT1-exactMatches < 10) ||
 				(exactMatches > 1 && nonMappedElementsT1-exactMatches < 20));
+	}
+
+	private int extracted(UMLOperationBodyMapper operationBodyMapper, int delegateStatements,
+			StatementObject statement) {
+		{
+			OperationInvocation invocation = statement.invocationCoveringEntireFragment();
+			if(invocation != null && invocation.matchesOperation(operationBodyMapper.getOperation1())) {
+				delegateStatements++;
+			}
+		}
+		return delegateStatements;
 	}
 
 	private boolean invocationMatchesWithAddedOperation(OperationInvocation removedOperationInvocation, Map<String, UMLType> variableTypeMap, List<OperationInvocation> operationInvocationsInNewMethod) {
