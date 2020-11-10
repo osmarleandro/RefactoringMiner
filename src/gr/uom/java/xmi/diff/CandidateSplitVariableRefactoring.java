3c1a31e6eba0d9ebfb767f0c1619877250dab9d4
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:37:54 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,CandidateSplitVariableRefactoring,hashCode#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/CandidateSplitVariableRefactoring.java
+++ src/gr/uom/java/xmi/diff/CandidateSplitVariableRefactoring.java
@@ -75,11 +75,16 @@ public class CandidateSplitVariableRefactoring {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((oldVariable == null) ? 0 : oldVariable.hashCode());
+		int result = extracted(prime);
 		result = prime * result + ((operationAfter == null) ? 0 : operationAfter.hashCode());
 		result = prime * result + ((operationBefore == null) ? 0 : operationBefore.hashCode());
 		result = prime * result + ((splitVariables == null) ? 0 : splitVariables.hashCode());
+		return result;
+	}
+
+	private int extracted(final int prime) {
+		int result = 1;
+		result = prime * result + ((oldVariable == null) ? 0 : oldVariable.hashCode());
 		return result;
 	}
 
