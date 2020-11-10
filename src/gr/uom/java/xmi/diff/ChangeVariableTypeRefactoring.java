041a80b874e6b9c40200a2e3591f21ca4f66ca19
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:57:15 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ChangeVariableTypeRefactoring,hashCode#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/ChangeVariableTypeRefactoring.java
+++ src/gr/uom/java/xmi/diff/ChangeVariableTypeRefactoring.java
@@ -85,11 +85,16 @@ public class ChangeVariableTypeRefactoring implements Refactoring {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((changedTypeVariable == null) ? 0 : changedTypeVariable.hashCode());
+		int result = extracted(prime);
 		result = prime * result + ((operationAfter == null) ? 0 : operationAfter.hashCode());
 		result = prime * result + ((operationBefore == null) ? 0 : operationBefore.hashCode());
 		result = prime * result + ((originalVariable == null) ? 0 : originalVariable.hashCode());
+		return result;
+	}
+
+	private int extracted(final int prime) {
+		int result = 1;
+		result = prime * result + ((changedTypeVariable == null) ? 0 : changedTypeVariable.hashCode());
 		return result;
 	}
 
