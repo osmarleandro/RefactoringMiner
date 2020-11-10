736cc7754a018bdaa781e451b5d147b2b3f1f299
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:52:07 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,ExtractOperationDetection,extractMatchCondition#QUMLOperationBodyMapper;QListQAbstractCodeMapping,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/ExtractOperationDetection.java
+++ src/gr/uom/java/xmi/diff/ExtractOperationDetection.java
@@ -237,14 +237,8 @@ public class ExtractOperationDetection {
 		boolean throwsNewExceptionExactMatch = false;
 		if(exactMatchList.size() == 1) {
 			AbstractCodeMapping mapping = exactMatchList.get(0);
-			if(mapping.getFragment1() instanceof StatementObject && mapping.getFragment2() instanceof StatementObject) {
-				StatementObject statement1 = (StatementObject)mapping.getFragment1();
-				StatementObject statement2 = (StatementObject)mapping.getFragment2();
-				if(statement1.getParent().getString().startsWith("catch(") &&
-						statement2.getParent().getString().startsWith("catch(")) {
-					exceptionHandlingExactMatch = true;
-				}
-			}
+			if(mapping.getFragment1() instanceof StatementObject && mapping.getFragment2() instanceof StatementObject)
+				exceptionHandlingExactMatch = extracted(exceptionHandlingExactMatch, mapping);
 			if(mapping.getFragment1().throwsNewException() && mapping.getFragment2().throwsNewException()) {
 				throwsNewExceptionExactMatch = true;
 			}
@@ -257,6 +251,18 @@ public class ExtractOperationDetection {
 				(!exceptionHandlingExactMatch && exactMatches > 1 && additionalExactMatches.size() < exactMatches && nonMappedElementsT2-exactMatches < 20) ||
 				(mappings == 1 && mappings > operationBodyMapper.nonMappedLeafElementsT2())) ||
 				argumentExtractedWithDefaultReturnAdded(operationBodyMapper);
+	}
+
+	private boolean extracted(boolean exceptionHandlingExactMatch, AbstractCodeMapping mapping) {
+		{
+			StatementObject statement1 = (StatementObject)mapping.getFragment1();
+			StatementObject statement2 = (StatementObject)mapping.getFragment2();
+			if(statement1.getParent().getString().startsWith("catch(") &&
+					statement2.getParent().getString().startsWith("catch(")) {
+				exceptionHandlingExactMatch = true;
+			}
+		}
+		return exceptionHandlingExactMatch;
 	}
 
 	private boolean argumentExtractedWithDefaultReturnAdded(UMLOperationBodyMapper operationBodyMapper) {
