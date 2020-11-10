88c21071468c427ac2e4e8a9943b4ca8173da827
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 01:50:46 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition,AbstractCall,allArgumentsReplaced#QAbstractCall;QSetQReplacement;QMapQStringQString,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/decomposition/AbstractCall.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCall.java
@@ -192,7 +192,15 @@ public abstract class AbstractCall implements LocationInfoProvider {
 		int replacedArguments = 0;
 		List<String> arguments1 = getArguments();
 		List<String> arguments2 = call.getArguments();
-		if(arguments1.size() == arguments2.size()) {
+		if(arguments1.size() == arguments2.size())
+			replacedArguments = extracted(replacements, parameterToArgumentMap, replacedArguments, arguments1,
+					arguments2);
+		return replacedArguments > 0 && replacedArguments == arguments1.size();
+	}
+
+	private int extracted(Set<Replacement> replacements, Map<String, String> parameterToArgumentMap,
+			int replacedArguments, List<String> arguments1, List<String> arguments2) {
+		{
 			for(int i=0; i<arguments1.size(); i++) {
 				String argument1 = arguments1.get(i);
 				String argument2 = arguments2.get(i);
@@ -205,7 +213,7 @@ public abstract class AbstractCall implements LocationInfoProvider {
 				}
 			}
 		}
-		return replacedArguments > 0 && replacedArguments == arguments1.size();
+		return replacedArguments;
 	}
 
 	public boolean renamedWithIdenticalExpressionAndArguments(AbstractCall call, Set<Replacement> replacements, double distance) {
