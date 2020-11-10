591f3ca5cc82bda97a119c89091fd4771b7e8787
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:32:18 -0300

    RefactoringMiner,gr.uom.java.xmi.decomposition.replacement,VariableDeclarationReplacement,hashCode#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/decomposition/replacement/VariableDeclarationReplacement.java
+++ src/gr/uom/java/xmi/decomposition/replacement/VariableDeclarationReplacement.java
@@ -42,9 +42,14 @@ public class VariableDeclarationReplacement extends Replacement {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
+		int result = extracted(prime);
+		result = prime * result + ((v2 == null) ? 0 : v2.hashCode());
+		return result;
+	}
+
+	private int extracted(final int prime) {
 		int result = super.hashCode();
 		result = prime * result + ((v1 == null) ? 0 : v1.hashCode());
-		result = prime * result + ((v2 == null) ? 0 : v2.hashCode());
 		return result;
 	}
 
