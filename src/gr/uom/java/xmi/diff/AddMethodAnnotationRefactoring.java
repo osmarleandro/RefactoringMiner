920d106f3cb08b9e8c810cd60662d8824c30e6ec
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:38:39 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,AddMethodAnnotationRefactoring,toString#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/AddMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/AddMethodAnnotationRefactoring.java
@@ -82,14 +82,19 @@ public class AddMethodAnnotationRefactoring implements Refactoring {
 	}
 
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getName()).append("\t");
+		StringBuilder sb = extracted();
 		sb.append(annotation);
 		sb.append(" in method ");
 		sb.append(operationAfter);
 		sb.append(" from class ");
 		sb.append(operationAfter.getClassName());
 		return sb.toString();
+	}
+
+	private StringBuilder extracted() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(getName()).append("\t");
+		return sb;
 	}
 
 	@Override
