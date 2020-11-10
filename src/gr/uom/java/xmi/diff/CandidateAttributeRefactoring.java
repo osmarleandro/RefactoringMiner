dd66ff861e95aa18d9eba9ebf06c95205459d767
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:56:12 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,CandidateAttributeRefactoring,equals#QObject,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/CandidateAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/CandidateAttributeRefactoring.java
@@ -116,6 +116,10 @@ public class CandidateAttributeRefactoring {
 			return true;
 		if (obj == null)
 			return false;
+		return extracted(obj);
+	}
+
+	private boolean extracted(Object obj) {
 		if (getClass() != obj.getClass())
 			return false;
 		CandidateAttributeRefactoring other = (CandidateAttributeRefactoring) obj;
