86234335e0a7b71b43f99fefcba72681ce22b6f9
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 01:07:49 -0300

    RefactoringMiner,gr.uom.java.xmi,WildcardType,hashCode#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/WildcardType.java
+++ src/gr/uom/java/xmi/WildcardType.java
@@ -12,9 +12,14 @@ public class WildcardType extends UMLType {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
+		int result = extracted(prime);
+		result = prime * result + (upperBound ? 1231 : 1237);
+		return result;
+	}
+
+	private int extracted(final int prime) {
 		int result = 1;
 		result = prime * result + ((bound == null) ? 0 : bound.hashCode());
-		result = prime * result + (upperBound ? 1231 : 1237);
 		return result;
 	}
 
