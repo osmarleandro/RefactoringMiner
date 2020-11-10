248318155296fab691c2565a37bf6c0030385c48
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 01:11:29 -0300

    RefactoringMiner,gr.uom.java.xmi,UMLParameter,hashCode#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/UMLParameter.java
+++ src/gr/uom/java/xmi/UMLParameter.java
@@ -84,10 +84,15 @@ public class UMLParameter implements Serializable, VariableDeclarationProvider {
 
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((kind == null) ? 0 : kind.hashCode());
+		int result = extracted(prime);
 		result = prime * result + ((type == null) ? 0 : type.hashCode());
 		result = prime * result + (varargs ? 1231 : 1237);
+		return result;
+	}
+
+	private int extracted(final int prime) {
+		int result = 1;
+		result = prime * result + ((kind == null) ? 0 : kind.hashCode());
 		return result;
 	}
 
