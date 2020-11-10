c7c4659ce296513de723afc427d5a47266540b09
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:42:21 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,RemoveMethodAnnotationRefactoring,equals#QObject,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
+++ src/gr/uom/java/xmi/diff/RemoveMethodAnnotationRefactoring.java
@@ -108,6 +108,10 @@ public class RemoveMethodAnnotationRefactoring implements Refactoring {
 			return true;
 		if (obj == null)
 			return false;
+		return extracted(obj);
+	}
+
+	private boolean extracted(Object obj) {
 		if (getClass() != obj.getClass())
 			return false;
 		RemoveMethodAnnotationRefactoring other = (RemoveMethodAnnotationRefactoring) obj;
