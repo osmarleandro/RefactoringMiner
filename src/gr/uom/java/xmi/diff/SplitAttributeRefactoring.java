beed829efab5a75f047899b60a2c13766f34c916
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-10 03:40:12 -0300

    RefactoringMiner,gr.uom.java.xmi.diff,SplitAttributeRefactoring,hashCode#,ExtractMethod,OK,false

--- src/gr/uom/java/xmi/diff/SplitAttributeRefactoring.java
+++ src/gr/uom/java/xmi/diff/SplitAttributeRefactoring.java
@@ -70,11 +70,16 @@ public class SplitAttributeRefactoring implements Refactoring {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((classNameAfter == null) ? 0 : classNameAfter.hashCode());
+		int result = extracted(prime);
 		result = prime * result + ((classNameBefore == null) ? 0 : classNameBefore.hashCode());
 		result = prime * result + ((oldAttribute == null) ? 0 : oldAttribute.hashCode());
 		result = prime * result + ((splitAttributes == null) ? 0 : splitAttributes.hashCode());
+		return result;
+	}
+
+	private int extracted(final int prime) {
+		int result = 1;
+		result = prime * result + ((classNameAfter == null) ? 0 : classNameAfter.hashCode());
 		return result;
 	}
 
