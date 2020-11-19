ccf19ff0f3ff4e7878b7681a3147536288391cb1
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-19 00:58:38 -0300

    RefactoringMiner,org.refactoringminer.api,CodeRangeProvider,NA,ExtractInterface,OK,NA

--- src/org/refactoringminer/api/CodeRangeProvider.java
+++ src/org/refactoringminer/api/CodeRangeProvider.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import gr.uom.java.xmi.diff.CodeRange;
 
-public interface CodeRangeProvider {
+public interface CodeRangeProvider extends ICodeRangeProvider {
 	List<CodeRange> leftSide();
 	List<CodeRange> rightSide();
 }
